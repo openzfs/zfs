70eadc19583d4c90f59bd1a78350657aa3f4afc9
behlendo <behlendo@7e1ea52c-4ff2-0310-8f11-9dd32ca42a1c>
2008-02-27 21:56:51 +0000

    OK, it builds... and the modules load...  now for some more cleanup to remove the remaining vestages of the time it lives with the ZFS code.
    git-svn-id: https://outreach.scidac.gov/svn/spl/trunk@13 7e1ea52c-4ff2-0310-8f11-9dd32ca42a1c

--- modules/spl/linux-generic.c
+++ modules/spl/linux-generic.c
@@ -1,4 +1,5 @@
 #include "linux-generic.h"
+#include "config.h"
 
 /*
  * Generic support
@@ -6,3 +7,21 @@
 
 int p0 = 0;
 EXPORT_SYMBOL(p0);
+
+static int __init spl_init(void)
+{
+        printk(KERN_INFO "spl: Loaded Solaris Porting Layer v%s\n", VERSION);
+	return 0;
+}
+
+static void spl_fini(void)
+{
+	return;
+}
+
+module_init(spl_init);
+module_exit(spl_fini);
+
+MODULE_AUTHOR("Lawrence Livermore National Labs");
+MODULE_DESCRIPTION("Solaris Porting Layer");
+MODULE_LICENSE("GPL");
