e4009e98c75612fcd75a1f142dfc37fea4b5ac4a
behlendo <behlendo@7e1ea52c-4ff2-0310-8f11-9dd32ca42a1c>
2008-02-27 20:55:24 +0000

    Quiet libtool
    git-svn-id: https://outreach.scidac.gov/svn/spl/trunk@12 7e1ea52c-4ff2-0310-8f11-9dd32ca42a1c

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 
 find . -type d -name .deps | xargs rm -rf
 rm -rf config.guess config.sub ltmain.sh
-libtoolize
+libtoolize --automake
 aclocal 2>/dev/null &&
 autoheader &&
 automake --add-missing --include-deps # 2>/dev/null &&
