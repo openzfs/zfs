8ddd0ee4154e7e785f07400c113c7465906c97b9
behlendo <behlendo@7e1ea52c-4ff2-0310-8f11-9dd32ca42a1c>
2008-03-13 20:41:29 +0000

    Add two more missing headers
    git-svn-id: https://outreach.scidac.gov/svn/spl/trunk@43 7e1ea52c-4ff2-0310-8f11-9dd32ca42a1c

---
+++ include/sys/resource.h
@@ -0,0 +1,6 @@
+#ifndef _SPL_RESOURCE_H
+#define _SPL_RESOURCE_H
+
+#include <linux/resource.h>
+
+#endif /* SPL_RESOURCE_H */
---
+++ include/sys/stat.h
@@ -0,0 +1,6 @@
+#ifndef _SPL_STAT_H
+#define _SPL_STAT_H
+
+#include <linux/stat.h>
+
+#endif /* SPL_STAT_H */
