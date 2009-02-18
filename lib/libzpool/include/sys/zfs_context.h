d164b2093561a9771db07346e6fffc9ca19427a2
Brian Behlendorf <behlendorf1@llnl.gov>
2009-02-18 12:51:31 -0800

    Rebase master to b108

--- lib/libzpool/include/sys/zfs_context.h
+++ lib/libzpool/include/sys/zfs_context.h
@@ -19,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -490,7 +490,7 @@ typedef struct callb_cpr {
 /*
  * Hostname information
  */
-extern char hw_serial[];
+extern char hw_serial[];	/* for userland-emulated hostid access */
 extern int ddi_strtoul(const char *str, char **nptr, int base,
     unsigned long *result);
 
