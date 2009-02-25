eb7c7f44e8b7630f027cf92e0490062428f7fced
Ricardo M. Correia <Ricardo.M.Correia@Sun.COM>
2009-02-25 21:06:15 +0000

    Changed ptob()/btop() mult/div into bit shifts. Added necessary include for PAGE_SHIFT.

--- include/sys/param.h
+++ include/sys/param.h
@@ -1,8 +1,10 @@
 #ifndef _SPL_PARAM_H
 #define _SPL_PARAM_H
 
+#include <asm/page.h>
+
 /* Pages to bytes and back */
-#define ptob(pages)			(pages * PAGE_SIZE)
-#define btop(bytes)			(bytes / PAGE_SIZE)
+#define ptob(pages)			(pages << PAGE_SHIFT)
+#define btop(bytes)			(bytes >> PAGE_SHIFT)
 
 #endif /* SPL_PARAM_H */
