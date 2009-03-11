84b63f7730ba0d0cd50cd701bf5a5a87f897c2ce
Brian Behlendorf <behlendorf1@llnl.gov>
2009-03-11 12:13:44 -0700

    Remove all #pragma idents from libspl.

--- lib/libspl/list.c
+++ lib/libspl/list.c
@@ -23,8 +23,6 @@
  * Use is subject to license terms.
  */
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
-
 /*
  * Generic doubly-linked list implementation
  */
--- lib/libspl/strnlen.c
+++ lib/libspl/strnlen.c
@@ -24,8 +24,6 @@
  * All rights reserved.  Use is subject to license terms.
  */
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
-
 #include <string.h>
 #include <sys/types.h>
 
