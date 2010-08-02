52c23142d4b2c0de86fe749eccbd3ae2727192db
Brian Behlendorf <behlendorf1@llnl.gov>
2010-08-02 15:22:11 -0700

    Revert "Inline zio_execute() to reduce stack use"
    This reverts commit 411dd65af1a3f931cee72adc6b9817187ae112cf.
    
    gcc version 4.1.2 does not like having differing prototypes
    for zio_execute, one version in the .c with inline and one
    version in the .h without.  Thus I'm reverting this change
    and we'll see how critical this particular stack reduction is.

--- module/zfs/zio.c
+++ module/zfs/zio.c
@@ -1119,8 +1119,7 @@ zio_interrupt(zio_t *zio)
  */
 static zio_pipe_stage_t *zio_pipeline[];
 
-__attribute__((always_inline))
-inline void
+void
 zio_execute(zio_t *zio)
 {
 	zio->io_executor = curthread;
