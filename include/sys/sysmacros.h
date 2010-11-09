a50cede3880ca0f7f7f10acc01bebd8617fc48ad
Brian Behlendorf <behlendorf1@llnl.gov>
2010-11-09 10:34:10 -0800

    Linux 2.6.36 compat, wrap RLIM64_INFINITY
    As of linux-2.6.36 RLIM64_INFINITY is defined in linux/resource.h.
    This is handled by conditionally defining RLIM64_INFINITY in the
    SPL only when the kernel does not provide it.

--- include/sys/sysmacros.h
+++ include/sys/sysmacros.h
@@ -83,8 +83,9 @@
 #define _NOTE(x)
 #define is_system_labeled()		0
 
-
-#define RLIM64_INFINITY			RLIM_INFINITY
+#ifndef RLIM64_INFINITY
+#define RLIM64_INFINITY			(~0ULL)
+#endif
 
 /* 0..MAX_PRIO-1:		Process priority
  * 0..MAX_RT_PRIO-1:		RT priority tasks
