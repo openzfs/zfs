69d8c8ea431f638e0bbab0e3fcfc3260a6d58490
Zachary Bedell <zac@thebedells.org>
2011-07-02 23:23:34 -0400

    Use initqueue hook to delay bootfs parsing until zfs modules are loaded

--- dracut/90zfs/parse-zfs.sh
+++ dracut/90zfs/parse-zfs.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
-# This seems to not be ready in time for some reason...
-ZFS_DEV=/dev/zfs
-if [ ! -c ${ZFS_DEV} ]; then
-  modprobe zfs
-  
-  COUNT=0
-  DELAY=5
-  while [ ! -e ${ZFS_DEV} ]; do
-		if [ ${COUNT} -gt ${DELAY} ]; then
-		  echo "ZFS: Module didn't create /dev/zfs.  This isn't likely to end well..."
-			break
-		fi
+. /lib/dracut-lib.sh
 
-		let COUNT=${COUNT}+1
-	  sleep 1
-	done
+# Let the command line override our host id.
+spl_hostid=`getarg spl_hostid=`
+if [ "${spl_hostid}" != "" ] ; then
+  info "ZFS: Using hostid from command line: ${spl_hostid}"
+  echo "${spl_hostid}" > /etc/hostid
+elif [ -f /etc/hostid ] ; then
+  info "ZFS: Using hostid from /etc/hostid: `cat /etc/hostid`"
+else
+  warn "ZFS: No hostid found on kernel command line or /etc/hostid.  ZFS pools may not import correctly."
 fi
 
 case "$root" in
+  ""|zfs|zfs:)
+    # We'll take root unset, root=zfs, or root=zfs:
+    # No root set, so we want to read the bootfs attribute.  We can't do that until udev settles,
+    # so we'll set dummy values and hope for the best later on.
+    root="-auto-"
+    
+    rootfs="zfs"
+    rootok=1
+
+    info "ZFS: Enabling autodetection of bootfs after udev settles."
+    ;;
+    
   ZFS\=*|zfs:*|zfs:FILESYSTEM\=*|FILESYSTEM\=*)
-    # root is explicit ZFS root. Try to import the pool.
+    # root is explicit ZFS root.  Parse it now.
     # We can handle a root=... param in any of the following formats:
     # root=ZFS=rpool/ROOT
     # root=zfs:rpool/ROOT
@@ -28,53 +35,16 @@ case "$root" in
     # root=FILESYSTEM=rpool/ROOT
     
     # Strip down to just the pool/fs
-    zfsbootfs="${root#zfs:}"
-    zfsbootfs="${zfsbootfs#FILESYSTEM=}"
-    zfsbootfs="${zfsbootfs#ZFS=}"
+    root="${root#zfs:}"
+    root="${root#FILESYSTEM=}"
+    root="${root#ZFS=}"
     
-    pool="${zfsbootfs%%/*}"
-    zpool list -H $pool > /dev/null
-    if [ "$?" -eq "1" ] ; then
-      # pool wasn't imported automatically by the kernel module, so try it manually.
-      zpool import -N $pool || { echo "ZFS: Unable to import root pool '${pool}'." ; continue ; }
-    fi
     rootfs="zfs"
     rootok=1
-    root="${zfsbootfs}"
-    echo "ZFS: Using ${zfsbootfs} as root."
+    
+    info "ZFS: Set ${root} as bootfs."
     ;;
-  "")
-    # No root set, so try to find it from bootfs attribute.
-    echo "ZFS: Attempting to detect root from imported ZFS pools."
-
-    # Might be imported by the kernel module, so try searching before we import anything.
-    zfsbootfs=`zpool list -H -o bootfs | sed 'q'`
-    if [ "x$zfsbootfs" = 'x' ] ; then
-      # Not there, so we need to import everything.
-      echo "ZFS: Attempting to import additional pools."
-      zpool import -N -a
-      zfsbootfs=`zpool list -H -o bootfs | sed 'q'`
-    fi
-    if [ "x$zfsbootfs" = 'x' ] ; then
-      pool=""
-      echo "ZFS: No bootfs attribute found in importable pools."
-    else
-      rootfs="zfs"
-      rootok=1
-      root="$zfsbootfs"
-      pool="${root%%/*}"
-      echo "ZFS: Using ${zfsbootfs} as root."
-    fi
-
-    # Export everything but our root again.
-    # FIXME: Ideally, we shouldn't export it unless we imported it -- IE anything brought
-    # in by the kernel module should stay imported.
-    zpool list -H | while read fs rest ; do
-      if [ "$pool" != "$fs" ] ; then
-        zpool export "$fs"
-      fi
-    done
-    ;;
-    # We leave the rpool imported since exporting it can cause things to mount
-    # badly later on.
 esac
+
+# Call back later to import the pools and possible parse bootfs
+/sbin/initqueue --unique /sbin/zfs-udev-settled.sh
