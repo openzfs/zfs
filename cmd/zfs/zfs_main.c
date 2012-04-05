aa6bb243183258e5c2ce5d7f40c8a6e0afe31a04
P.SCH <p88@yahoo.com>
2012-04-05 16:03:21 +0300

    ZFS Snapshot Alias

--- cmd/zfs/zfs_main.c
+++ cmd/zfs/zfs_main.c
@@ -131,6 +131,7 @@ typedef enum {
 	HELP_SET,
 	HELP_SHARE,
 	HELP_SNAPSHOT,
+	HELP_SNAP,
 	HELP_UNMOUNT,
 	HELP_UNSHARE,
 	HELP_ALLOW,
@@ -163,6 +164,7 @@ static zfs_command_t command_table[] = {
 	{ "destroy",	zfs_do_destroy,		HELP_DESTROY		},
 	{ NULL },
 	{ "snapshot",	zfs_do_snapshot,	HELP_SNAPSHOT		},
+	{ "snap",	zfs_do_snapshot,	HELP_SNAP		},
 	{ "rollback",	zfs_do_rollback,	HELP_ROLLBACK		},
 	{ "clone",	zfs_do_clone,		HELP_CLONE		},
 	{ "promote",	zfs_do_promote,		HELP_PROMOTE		},
@@ -254,7 +256,10 @@ get_usage(zfs_help_t idx)
 	case HELP_SHARE:
 		return (gettext("\tshare <-a | filesystem>\n"));
 	case HELP_SNAPSHOT:
-		return (gettext("\tsnapshot [-r] [-o property=value] ... "
+		return (gettext("\tsnapshot|snap [-r] [-o property=value] ... "
+		    "<filesystem@snapname|volume@snapname>\n"));
+	case HELP_SNAP:
+		return (gettext("\tsnap|snapshot [-r] [-o property=value] ... "
 		    "<filesystem@snapname|volume@snapname>\n"));
 	case HELP_UNMOUNT:
 		return (gettext("\tunmount [-f] "
