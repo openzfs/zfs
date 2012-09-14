openzfs/zfs

commit 2b2861362f7dd09cc3167df8fddb6e2cb475018a

Clear PG_writeback after zil_commit() for sync I/O
When writing via ->writepage() the writeback bit was always cleared
as part of the txg commit callback.  However, when the I/O is also
being written synchronsously to the zil we can immediately clear this
bit.  There is no need to wait for the subsequent TXG sync since the
data is already safe on stable storage.

This has been observed to reduce the msync(2) delay from up to 5
seconds down 10s of miliseconds.  One workload which is expected
to benefit from this are the intermittent samba hands described
in issue #700.

Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
Closes #700
Closes #907

module/zfs/zfs_vnops.c
@@ -3797,6 +3797,7 @@ zfs_putpage(struct inode *ip, struct page *pp, struct writeback_control *wbc)
 	uint64_t	mtime[2], ctime[2];
 	sa_bulk_attr_t	bulk[3];
 	int		cnt = 0;
+	int		sync;
 
 
 	ASSERT(PageLocked(pp));
@@ -3833,7 +3834,10 @@ zfs_putpage(struct inode *ip, struct page *pp, struct writeback_control *wbc)
 
 	tx = dmu_tx_create(zsb->z_os);
 
-	dmu_tx_callback_register(tx, zfs_putpage_commit_cb, pp);
+	sync = ((zsb->z_os->os_sync == ZFS_SYNC_ALWAYS) ||
+	        (wbc->sync_mode == WB_SYNC_ALL));
+	if (!sync)
+		dmu_tx_callback_register(tx, zfs_putpage_commit_cb, pp);
 
 	dmu_tx_hold_write(tx, zp->z_id, pgoff, pglen);
 
@@ -3862,9 +3866,10 @@ zfs_putpage(struct inode *ip, struct page *pp, struct writeback_control *wbc)
 	dmu_tx_commit(tx);
 	ASSERT3S(err, ==, 0);
 
-	if ((zsb->z_os->os_sync == ZFS_SYNC_ALWAYS) ||
-	    (wbc->sync_mode == WB_SYNC_ALL))
+	if (sync) {
 		zil_commit(zsb->z_log, zp->z_id);
+		zfs_putpage_commit_cb(pp, err);
+	}
 
 	return (err);
 }

commit 8312c6df55c1fb2ad3536fc3bc7ae93c429b85aa

Clear PG_writeback for sync I/O error case
Commit 2b2861362f7dd09cc3167df8fddb6e2cb475018a accidentally
introduced this issue by only conditionally registering the
commit callback in the async case.

The error handing code for the dmu_tx_assign() failure case
relied on there always being a registered commit callback to
clear the PG_writeback bit.  Since that is no longer strictly
true for the synchronous case we must explicitly invoke the
callback.

Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
Closes #961

module/zfs/zfs_vnops.c
@@ -3848,7 +3848,16 @@ zfs_putpage(struct inode *ip, struct page *pp, struct writeback_control *wbc)
 		if (err == ERESTART)
 			dmu_tx_wait(tx);
 
+		/* Will call all registered commit callbacks */
 		dmu_tx_abort(tx);
+
+		/*
+		 * For the synchronous case the commit callback must be
+		 * explicitly called because there is no registered callback.
+		 */
+		if (sync)
+			zfs_putpage_commit_cb(pp, ECANCELED);
+
 		return (err);
 	}
 