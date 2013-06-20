4b8d921b37da18557a1eb70118ac3c0b84917e7c
Matthew Ahrens <mahrens@delphix.com>
2013-06-20 14:43:17 -0800

    Illumos #3829
    3829 fix for 3740 changed behavior of zfs destroy/hold/release ioctl
    Reviewed by: Matt Amdur <matt.amdur@delphix.com>
    Reviewed by: Christopher Siden <christopher.siden@delphix.com>
    Approved by: Richard Lowe <richlowe@richlowe.net>
    Signed-off-by: Richard Yao <ryao@gentoo.org>
    
    References:
    
    https://www.illumos.org/issues/3829
    illumos/illumos-gate@bb6e70758d0c30c09f148026d6e686e21cfc8d18

--- module/zfs/dsl_userhold.c
+++ module/zfs/dsl_userhold.c
@@ -126,10 +126,6 @@ dsl_dataset_user_hold_check(void *arg, dmu_tx_t *tx)
 		}
 	}
 
-	/* Return ENOENT if no holds would be created. */
-	if (nvlist_empty(dduha->dduha_chkholds))
-		return (SET_ERROR(ENOENT));
-
 	return (0);
 }
 
@@ -467,10 +463,6 @@ dsl_dataset_user_release_check(void *arg, dmu_tx_t *tx)
 				return (error);
 		}
 	}
-
-	/* Return ENOENT if none of the holds existed. */
-	if (nvlist_empty(ddura->ddura_chkholds))
-		return (SET_ERROR(ENOENT));
 
 	return (0);
 }
