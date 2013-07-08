2149df5397c73f7a421c5b1b1bb2dfaabe03ebf2
Dmitry Khasanov <pik4ez@gmail.com>
2013-07-08 18:18:07 +0700

    Added zpool_pool_state_to_name function.

--- include/libzfs.h
+++ include/libzfs.h
@@ -212,6 +212,7 @@ extern void zpool_close(zpool_handle_t *);
 extern const char *zpool_get_name(zpool_handle_t *);
 extern int zpool_get_state(zpool_handle_t *);
 extern char *zpool_state_to_name(vdev_state_t, vdev_aux_t);
+extern const char *zpool_pool_state_to_name(pool_state_t);
 extern void zpool_free_handles(libzfs_handle_t *);
 
 /*
