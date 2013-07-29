6389d42205f56083b7658b2c67f117a244f13e52
Steven Hartland <smh@freebsd.org>
2013-07-29 11:36:31 -0800

    Illumos #3909
    3909 "zfs send -D" does not work
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Approved by: Christopher Siden <christopher.siden@delphix.com>
    
    References:
      https://www.illumos.org/issues/3909
      illumos/illumos-gate@36f7455d36b60be70d7aae5959fa19e71954678e
    
    Ported-by: Richard Yao <ryao@gentoo.org>
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Issue #1775

--- lib/libzfs/libzfs_sendrecv.c
+++ lib/libzfs/libzfs_sendrecv.c
@@ -1572,8 +1572,8 @@ zfs_send(zfs_handle_t *zhp, const char *fromsnap, const char *tosnap,
 	if (tid != 0) {
 		if (err != 0)
 			(void) pthread_cancel(tid);
-		(void) pthread_join(tid, NULL);
 		(void) close(pipefd[0]);
+		(void) pthread_join(tid, NULL);
 	}
 
 	if (sdd.cleanup_fd != -1) {
@@ -1609,8 +1609,8 @@ err_out:
 		VERIFY(0 == close(sdd.cleanup_fd));
 	if (tid != 0) {
 		(void) pthread_cancel(tid);
-		(void) pthread_join(tid, NULL);
 		(void) close(pipefd[0]);
+		(void) pthread_join(tid, NULL);
 	}
 	return (err);
 }
