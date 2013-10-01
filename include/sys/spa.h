03f43841cbdffb5dc831e3142be3cd413f079331
Brian Behlendorf <behlendorf1@llnl.gov>
2013-10-01 09:50:50 -0700

    Add visibility in to txg sync behavior
    This change is an attempt to add visibility in to how txgs are being
    formed on a system, in real time. To do this, a list was added to the
    in memory SPA data structure for a pool, with each element on the list
    corresponding to txg. These entries are then exported through the kstat
    interface, which can then be interpreted in userspace.
    
    For each txg, the following information is exported:
    
     * Unique txg number (uint64_t)
     * The time the txd was born (hrtime_t)
       (*not* wall clock time; relative to the other entries on the list)
     * The current txg state ((O)pen/(Q)uiescing/(S)yncing/(C)ommitted)
     * The number of reserved bytes for the txg (uint64_t)
     * The number of bytes read during the txg (uint64_t)
     * The number of bytes written during the txg (uint64_t)
     * The number of read operations during the txg (uint64_t)
     * The number of write operations during the txg (uint64_t)
     * The time the txg was closed (hrtime_t)
     * The time the txg was quiesced (hrtime_t)
     * The time the txg was synced (hrtime_t)
    
    Note that while the raw kstat now stores relative hrtimes for the
    open, quiesce, and sync times.  Those relative times are used to
    calculate how long each state took and these deltas and printed by
    output handlers.
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- include/sys/spa.h
+++ include/sys/spa.h
@@ -548,12 +548,26 @@ typedef struct spa_stats_history {
 
 typedef struct spa_stats {
 	spa_stats_history_t	read_history;
+	spa_stats_history_t	txg_history;
 } spa_stats_t;
+
+typedef enum txg_state {
+	TXG_STATE_BIRTH		= 0,
+	TXG_STATE_OPEN		= 1,
+	TXG_STATE_QUIESCED	= 2,
+	TXG_STATE_SYNCED	= 3,
+	TXG_STATE_COMMITTED	= 4,
+} txg_state_t;
 
 extern void spa_stats_init(spa_t *spa);
 extern void spa_stats_destroy(spa_t *spa);
 extern void spa_read_history_add(spa_t *spa, const zbookmark_t *zb,
     uint32_t aflags);
+extern void spa_txg_history_add(spa_t *spa, uint64_t txg);
+extern int spa_txg_history_set(spa_t *spa,  uint64_t txg,
+    txg_state_t completed_state, hrtime_t completed_time);
+extern int spa_txg_history_set_io(spa_t *spa,  uint64_t txg, uint64_t nread,
+    uint64_t nwritten, uint64_t reads, uint64_t writes, uint64_t nreserved);
 
 /* Pool configuration locks */
 extern int spa_config_tryenter(spa_t *spa, int locks, void *tag, krw_t rw);
