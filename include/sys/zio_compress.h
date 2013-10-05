f638b3ee3dd09e2e4857f47d452ba567e9539ed9
Richard Yao <ryao@gentoo.org>
2013-10-05 17:55:24 -0400

    Linux 3.11 compat: Rename LZ4 symbols
    Linus Torvalds merged LZ4 into Linux 3.11. This causes a conflict
    whenever CONFIG_LZ4_DECOMPRESS=y or CONFIG_LZ4_COMPRESS=y are set in the
    kernel's .config. We rename the symbols to avoid the conflict.
    
    Signed-off-by: Richard Yao <ryao@gentoo.org>

--- include/sys/zio_compress.h
+++ include/sys/zio_compress.h
@@ -74,9 +74,9 @@ extern size_t zle_compress(void *src, void *dst, size_t s_len, size_t d_len,
     int level);
 extern int zle_decompress(void *src, void *dst, size_t s_len, size_t d_len,
     int level);
-extern size_t lz4_compress(void *src, void *dst, size_t s_len, size_t d_len,
+extern size_t lz4_compress_zfs(void *src, void *dst, size_t s_len, size_t d_len,
     int level);
-extern int lz4_decompress(void *src, void *dst, size_t s_len, size_t d_len,
+extern int lz4_decompress_zfs(void *src, void *dst, size_t s_len, size_t d_len,
     int level);
 
 /*
