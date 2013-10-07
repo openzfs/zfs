8034f3c0dc9eee938adb5e23994151bdce1b1b7e
Richard Yao <ryao@gentoo.org>
2013-10-07 06:53:58 -0400

    Add missing copyright notices from Illumos
    This resolves merge conflicts when merging Illumos #3588 and Illumos #4047.
    
    Signed-off-by: Richard Yao <ryao@gentoo.org>

--- include/sys/dmu.h
+++ include/sys/dmu.h
@@ -21,6 +21,7 @@
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2012 by Delphix. All rights reserved.
+ * Copyright 2011 Nexenta Systems, Inc. All rights reserved.
  * Copyright (c) 2012, Joyent, Inc. All rights reserved.
  */
 
