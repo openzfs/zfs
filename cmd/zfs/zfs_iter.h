c07415ef51029ae1a382a60a6f42bc184d753ed6
Gregor Kopka <mailfrom-github.com@kopka.net>
2013-11-19 16:41:37 +0100

    Add -p (parsable) to zfs list Ported from illumos feature #2583 Fixes issue https://github.com/zfsonlinux/zfs/issues/937

--- cmd/zfs/zfs_iter.h
+++ cmd/zfs/zfs_iter.h
@@ -44,6 +44,7 @@ typedef struct zfs_sort_column {
 #define	ZFS_ITER_DEPTH_LIMIT	   (1 << 3)
 #define	ZFS_ITER_RECVD_PROPS	   (1 << 4)
 #define	ZFS_ITER_SIMPLE		   (1 << 5)
+#define	ZFS_ITER_LITERAL_PROPS	   (1 << 6)
 
 int zfs_for_each(int, char **, int options, zfs_type_t,
     zfs_sort_column_t *, zprop_list_t **, int, zfs_iter_f, void *);
