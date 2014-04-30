5132a2d0806ade11e0a5f61d7f249e283fd8ca0b
ilovezfs <ilovezfs@icloud.com>
2014-04-29 22:47:14 -0700

    Fill in mountpoint buffer before using it in errors
    zfs_is_mountable() fills in the mountpoint buffer, so, as in
    upstream, it needs to have been called before the mountpoint
    buffer can be used in error messages.
    
    In particular,
    
    	return (zfs_error_fmt(hdl, EZFS_MOUNTFAILED,
    	    dgettext(TEXT_DOMAIN, "cannot mount '%s'"),
    	    mountpoint));
    
    should not come before the call to zfs_is_mountable().
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>
    Closes #2284

--- lib/libzfs/libzfs_mount.c
+++ lib/libzfs/libzfs_mount.c
@@ -405,6 +405,9 @@ zfs_mount(zfs_handle_t *zhp, const char *options, int flags)
 	if (zpool_get_prop_int(zhp->zpool_hdl, ZPOOL_PROP_READONLY, NULL))
 		(void) strlcat(mntopts, "," MNTOPT_RO, sizeof (mntopts));
 
+	if (!zfs_is_mountable(zhp, mountpoint, sizeof (mountpoint), NULL))
+		return (0);
+
 	/*
 	 * Append default mount options which apply to the mount point.
 	 * This is done because under Linux (unlike Solaris) multiple mount
@@ -425,9 +428,6 @@ zfs_mount(zfs_handle_t *zhp, const char *options, int flags)
 	 * Append zfsutil option so the mount helper allow the mount
 	 */
 	strlcat(mntopts, "," MNTOPT_ZFSUTIL, sizeof (mntopts));
-
-	if (!zfs_is_mountable(zhp, mountpoint, sizeof (mountpoint), NULL))
-		return (0);
 
 	/* Create the directory if it doesn't already exist */
 	if (lstat(mountpoint, &buf) != 0) {
