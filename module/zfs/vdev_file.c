3b44229aeabf697924c210fe929384fb2d6547a8
rootfs <hchen@redhat.com>
2014-05-07 13:51:23 -0400

    turn on nocacheflush for file vdev

--- module/zfs/vdev_file.c
+++ module/zfs/vdev_file.c
@@ -174,8 +174,10 @@ vdev_file_io_start(zio_t *zio)
 
 		switch (zio->io_cmd) {
 		case DKIOCFLUSHWRITECACHE:
-			zio->io_error = VOP_FSYNC(vf->vf_vnode, FSYNC | FDSYNC,
-			    kcred, NULL);
+			if (!zfs_nocacheflush) {
+			    zio->io_error = VOP_FSYNC(vf->vf_vnode, FSYNC | FDSYNC,
+			        kcred, NULL);
+                        }
 			break;
 		default:
 			zio->io_error = SET_ERROR(ENOTSUP);
