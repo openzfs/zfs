0e9aa31e9f170ac193d0392e96634b093207da7f
Chunwei Chen <tuxoko@gmail.com>
2014-05-28 15:15:48 +0800

    Refactor sha256 in preparation for asm optimization
    The asm optimization for sha256 we'll be using transforms multiple blocks in a
    single function call. Thus, we refactor the existing generic code in
    preparation for the upcoming change.
    
    Signed-off-by: Chunwei Chen <tuxoko@gmail.com>

---
+++ include/sys/sha256.h
@@ -0,0 +1,17 @@
+#ifndef	_SYS_SHA256_H
+#define	_SYS_SHA256_H
+
+#include <sys/zfs_context.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define	SHA256_SHIFT	(6)
+#define	SHA256_BLOCK	(1<<SHA256_SHIFT)
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* _SYS_SHA256_H */
