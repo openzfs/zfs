fe21c598adbbe9e8e598507cdbad7a9075b5541b
Matthew Ahrens <mahrens@delphix.com>
2014-07-07 11:49:36 -0800

    Illumos #4950 files sometimes can't be removed from a full filesystem
    Reviewed by: Adam Leventhal <adam.leventhal@delphix.com>
    Reviewed by: George Wilson <george.wilson@delphix.com>
    Reviewed by: Sebastien Roy <sebastien.roy@delphix.com>
    Reviewed by: Boris Protopopov <bprotopopov@hotmail.com>
    Approved by: Dan McDonald <danmcd@omniti.com>
    Ported-by: Richard Yao <richard.yao@clusterhq.com>
    
    Porting notes:
    1. ZoL currently does not log discards to zvols, so the portion of this patch that
    modifies the discard logging to mark it as freeing space has been discarded.
    
    2. may_delete_now had been removed from zfs_remove() in ZoL. It has been
    reintroduced.
    
    3. We do not try to emulate vnodes, so the following lines are not valid
    on Linux:
    
    	mutex_enter(&vp->v_lock);
    	may_delete_now = vp->v_count == 1 && !vn_has_cached_data(vp);
    	mutex_exit(&vp->v_lock);
    
    This has been replaced with:
    
    	mutex_enter(&zp->z_lock);
    	may_delete_now = atomic_read(&ip->i_count) == 1 && !(zp->z_is_mapped);
    	mutex_exit(&zp->z_lock);

--- module/zfs/dmu_tx.c
+++ module/zfs/dmu_tx.c
@@ -21,7 +21,7 @@
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright 2011 Nexenta Systems, Inc.  All rights reserved.
- * Copyright (c) 2013 by Delphix. All rights reserved.
+ * Copyright (c) 2012, 2014 by Delphix. All rights reserved.
  */
 
 #include <sys/dmu.h>
@@ -597,6 +597,32 @@ dmu_tx_count_free(dmu_tx_hold_t *txh, uint64_t off, uint64_t len)
 	}
 	txh->txh_space_tofree += space;
 	txh->txh_space_tounref += unref;
+}
+
+/*
+ * This function marks the transaction as being a "net free".  The end
+ * result is that refquotas will be disabled for this transaction, and
+ * this transaction will be able to use half of the pool space overhead
+ * (see dsl_pool_adjustedsize()).  Therefore this function should only
+ * be called for transactions that we expect will not cause a net increase
+ * in the amount of space used (but it's OK if that is occasionally not true).
+ */
+void
+dmu_tx_mark_netfree(dmu_tx_t *tx)
+{
+	dmu_tx_hold_t *txh;
+
+	txh = dmu_tx_hold_object_impl(tx, tx->tx_objset,
+	    DMU_NEW_OBJECT, THT_FREE, 0, 0);
+
+	/*
+	 * Pretend that this operation will free 1GB of space.  This
+	 * should be large enough to cancel out the largest write.
+	 * We don't want to use something like UINT64_MAX, because that would
+	 * cause overflows when doing math with these values (e.g. in
+	 * dmu_tx_try_assign()).
+	 */
+	txh->txh_space_tofree = txh->txh_space_tounref = 1024 * 1024 * 1024;
 }
 
 void
