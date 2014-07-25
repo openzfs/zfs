138f7a3e8ebc0621bbd95882e17cb26db93bcd09
Turbo Fredriksson <turbo@bayour.com>
2014-07-25 12:42:00 +0200

    Create an 'overlay' property (default 'off') that will control wether the filesystem should be mounted even if the mountpoint is busy or if it should fail with a 'mountpoint not empty'.
    Doing overlay mounts is the default mount behaviour on Linux, but not
    in ZFS. It have been decided that following the ZFS behaviour should
    be the default, but this overlay allows for site administrator to override
    this decision on a per-dataset basis.
    
    Signed-off-by: Turbo Fredriksson <turbo@bayour.com>
    Closes: #2503

--- module/zcommon/zfs_prop.c
+++ module/zcommon/zfs_prop.c
@@ -295,6 +295,8 @@ zfs_prop_init(void)
 	zprop_register_index(ZFS_PROP_NBMAND, "nbmand", 0, PROP_INHERIT,
 	    ZFS_TYPE_FILESYSTEM | ZFS_TYPE_SNAPSHOT, "on | off", "NBMAND",
 	    boolean_table);
+	zprop_register_index(ZFS_PROP_OVERLAY, "overlay", 0, PROP_INHERIT,
+	    ZFS_TYPE_FILESYSTEM, "on | off", "OVERLAY", boolean_table);
 
 	/* default index properties */
 	zprop_register_index(ZFS_PROP_VERSION, "version", 0, PROP_DEFAULT,
