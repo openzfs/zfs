acfe32658b125afe446482581c785d46caec898c
Matthew Ahrens <mahrens@delphix.com>
2014-11-26 09:57:30 -0800

    Illumos 5349 - verify that block pointer is plausible before reading
    5349 verify that block pointer is plausible before reading
    Reviewed by: Alex Reece <alex.reece@delphix.com>
    Reviewed by: Christopher Siden <christopher.siden@delphix.com>
    Reviewed by: Dan McDonald <danmcd@omniti.com>
    Reviewed by: George Wilson <george.wilson@delphix.com>
    Reviewed by: Richard Lowe <richlowe@richlowe.net>
    Reviewed by: Xin Li <delphij@FreeBSD.org>
    Reviewed by: Josef 'Jeff' Sipek <josef.sipek@nexenta.com>
    Approved by: Gordon Ross <gwr@nexenta.com>
    
    References:
      https://www.illumos.org/issues/5349
      https://github.com/illumos/illumos-gate/commit/f63ab3d5
    
    Porting notes:
    * Several variable declarations were moved due to C style needs
    
    Ported-by: DHE <git@dehacked.net>

--- include/sys/spa.h
+++ include/sys/spa.h
@@ -830,6 +830,7 @@ extern boolean_t spa_has_slogs(spa_t *spa);
 extern boolean_t spa_is_root(spa_t *spa);
 extern boolean_t spa_writeable(spa_t *spa);
 extern boolean_t spa_has_pending_synctask(spa_t *spa);
+extern void zfs_blkptr_verify(spa_t *spa, const blkptr_t *bp);
 
 extern int spa_mode(spa_t *spa);
 extern uint64_t strtonum(const char *str, char **nptr);
