115a8c22e6616644afc1ebf15f29b79ccdf5116d
George Wilson <george.wilson@delphix.com>
2014-12-06 09:24:32 -0800

    Illumos 5369 - arc flags should be an enum
    5369 arc flags should be an enum
    5370 consistent arc_buf_hdr_t naming scheme
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Alex Reece <alex.reece@delphix.com>
    Reviewed by: Sebastien Roy <sebastien.roy@delphix.com>
    Reviewed by: Richard Elling <richard.elling@richardelling.com>
    Approved by: Richard Lowe <richlowe@richlowe.net>
    
    conflicts due to #2129, in:
    
    arc.c
    
    static void arc_evict_ghost :
    (with additional argument "arc_buf_contents_t type" since
    Revert "Allow arc_evict_ghost() to only evict meta data" is left out)
    
    l2arc_release_cdata_buf(arc_buf_hdr_t *ab) :
    zio_data_buf_free(l2hdr->b_tmp_cdata, ab->b_size);
    is changed to
    abd_free(l2hdr->b_tmp_cdata, hdr->b_size);
    
    fix commit 989fd514b1053d5443b4e6155af9c8d863f5f0f2
    "Change ASSERT(!"...") to  cmn_err(CE_PANIC, ...)"

--- module/zfs/zil.c
+++ module/zfs/zil.c
@@ -204,7 +204,7 @@ zil_read_log_block(zilog_t *zilog, const blkptr_t *bp, blkptr_t *nbp, void *dst,
     char **end)
 {
 	enum zio_flag zio_flags = ZIO_FLAG_CANFAIL;
-	uint32_t aflags = ARC_WAIT;
+	arc_flags_t aflags = ARC_FLAG_WAIT;
 	arc_buf_t *abuf = NULL;
 	zbookmark_phys_t zb;
 	int error;
@@ -277,7 +277,7 @@ zil_read_log_data(zilog_t *zilog, const lr_write_t *lr, void *wbuf)
 {
 	enum zio_flag zio_flags = ZIO_FLAG_CANFAIL;
 	const blkptr_t *bp = &lr->lr_blkptr;
-	uint32_t aflags = ARC_WAIT;
+	arc_flags_t aflags = ARC_FLAG_WAIT;
 	arc_buf_t *abuf = NULL;
 	zbookmark_phys_t zb;
 	int error;
