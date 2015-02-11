a1d4875ca5e9e9fbadc2a73a9cd6275b036f9550
Chunwei Chen <tuxoko@gmail.com>
2015-02-11 16:45:53 +0800

    Handle abd_t in sa_impl.h, sa.c, space_map.c, spa.c, spa_history.c and zil.c
    Signed-off-by: Chunwei Chen <tuxoko@gmail.com>

--- module/zfs/space_map.c
+++ module/zfs/space_map.c
@@ -349,7 +349,7 @@ space_map_open_impl(space_map_t *sm)
 		return (error);
 
 	dmu_object_size_from_db(sm->sm_dbuf, &sm->sm_blksz, &blocks);
-	sm->sm_phys = sm->sm_dbuf->db_data;
+	sm->sm_phys = ABD_TO_BUF(sm->sm_dbuf->db_data);
 	return (0);
 }
 
