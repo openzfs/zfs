3da8f7aaffe9c035c72586d6827594c0efdf4f74
Chunwei Chen <tuxoko@gmail.com>
2015-05-11 23:58:53 +0800

    Handle abd_t in dbuf.c, ddt.c, dmu*.c
    Signed-off-by: Chunwei Chen <tuxoko@gmail.com>

--- module/zfs/dmu.c
+++ module/zfs/dmu.c
@@ -26,6 +26,7 @@
  * Copyright (c) 2015 by Chunwei Chen. All rights reserved.
  */
 
+#include <sys/abd.h>
 #include <sys/dmu.h>
 #include <sys/dmu_impl.h>
 #include <sys/dmu_tx.h>
@@ -798,7 +799,7 @@ dmu_read(objset_t *os, uint64_t object, uint64_t offset, uint64_t size,
 			bufoff = offset - db->db_offset;
 			tocpy = MIN(db->db_size - bufoff, size);
 
-			(void) memcpy(buf, (char *)db->db_data + bufoff, tocpy);
+			abd_copy_to_buf_off(buf, db->db_data, tocpy, bufoff);
 
 			offset += tocpy;
 			size -= tocpy;
@@ -840,7 +841,7 @@ dmu_write(objset_t *os, uint64_t object, uint64_t offset, uint64_t size,
 		else
 			dmu_buf_will_dirty(db, tx);
 
-		(void) memcpy((char *)db->db_data + bufoff, buf, tocpy);
+		abd_copy_from_buf_off(db->db_data, buf, tocpy, bufoff);
 
 		if (tocpy == db->db_size)
 			dmu_buf_fill_done(db, tx);
@@ -965,6 +966,7 @@ dmu_xuio_fini(xuio_t *xuio)
  * Initialize iov[priv->next] and priv->bufs[priv->next] with { off, n, abuf }
  * and increase priv->next by 1.
  */
+/* TODO: abd handle xuio */
 int
 dmu_xuio_add(xuio_t *xuio, arc_buf_t *abuf, offset_t off, size_t n)
 {
@@ -1049,7 +1051,8 @@ xuio_stat_wbuf_nocopy()
  * return value is the number of bytes successfully copied to arg_buf.
  */
 static int
-dmu_req_copy(void *arg_buf, int size, struct request *req, size_t req_offset)
+dmu_req_copy(abd_t *db_data, int db_offset, int size, struct request *req,
+    size_t req_offset)
 {
 	struct bio_vec bv, *bvp;
 	struct req_iterator iter;
@@ -1083,9 +1086,11 @@ dmu_req_copy(void *arg_buf, int size, struct request *req, size_t req_offset)
 		ASSERT3P(bv_buf, !=, NULL);
 
 		if (rq_data_dir(req) == WRITE)
-			memcpy(arg_buf + offset, bv_buf, tocpy);
+			abd_copy_from_buf_off(db_data, bv_buf, tocpy,
+			    db_offset + offset);
 		else
-			memcpy(bv_buf, arg_buf + offset, tocpy);
+			abd_copy_to_buf_off(bv_buf, db_data, tocpy,
+			    db_offset + offset);
 
 		offset += tocpy;
 	}
@@ -1125,7 +1130,7 @@ dmu_read_req(objset_t *os, uint64_t object, struct request *req)
 		if (tocpy == 0)
 			break;
 
-		didcpy = dmu_req_copy(db->db_data + bufoff, tocpy, req,
+		didcpy = dmu_req_copy(db->db_data, bufoff, tocpy, req,
 		    req_offset);
 
 		if (didcpy < tocpy)
@@ -1182,7 +1187,7 @@ dmu_write_req(objset_t *os, uint64_t object, struct request *req, dmu_tx_t *tx)
 		else
 			dmu_buf_will_dirty(db, tx);
 
-		didcpy = dmu_req_copy(db->db_data + bufoff, tocpy, req,
+		didcpy = dmu_req_copy(db->db_data, bufoff, tocpy, req,
 		    req_offset);
 
 		if (tocpy == db->db_size)
@@ -1245,8 +1250,8 @@ dmu_read_uio_dnode(dnode_t *dn, uio_t *uio, uint64_t size)
 			else
 				XUIOSTAT_BUMP(xuiostat_rbuf_copied);
 		} else {
-			err = uiomove((char *)db->db_data + bufoff, tocpy,
-			    UIO_READ, uio);
+			err = abd_uiomove_off(db->db_data, tocpy, UIO_READ,
+			    uio, bufoff);
 		}
 		if (err)
 			break;
@@ -1346,8 +1351,8 @@ dmu_write_uio_dnode(dnode_t *dn, uio_t *uio, uint64_t size, dmu_tx_t *tx)
 		 * to lock the pages in memory, so that uiomove won't
 		 * block.
 		 */
-		err = uiomove((char *)db->db_data + bufoff, tocpy,
-		    UIO_WRITE, uio);
+		err = abd_uiomove_off(db->db_data, tocpy, UIO_WRITE, uio,
+		    bufoff);
 
 		if (tocpy == db->db_size)
 			dmu_buf_fill_done(db, tx);
@@ -1474,6 +1479,7 @@ dmu_assign_arcbuf(dmu_buf_t *handle, uint64_t offset, arc_buf_t *buf,
 	} else {
 		objset_t *os;
 		uint64_t object;
+		void *tmp_buf;
 
 		DB_DNODE_ENTER(dbuf);
 		dn = DB_DNODE(dbuf);
@@ -1482,7 +1488,13 @@ dmu_assign_arcbuf(dmu_buf_t *handle, uint64_t offset, arc_buf_t *buf,
 		DB_DNODE_EXIT(dbuf);
 
 		dbuf_rele(db, FTAG);
-		dmu_write(os, object, offset, blksz, buf->b_data, tx);
+
+		tmp_buf = abd_borrow_buf_copy(buf->b_data, blksz);
+
+		dmu_write(os, object, offset, blksz, tmp_buf, tx);
+
+		abd_return_buf(buf->b_data, tmp_buf, blksz);
+
 		dmu_return_arcbuf(buf);
 		XUIOSTAT_BUMP(xuiostat_wbuf_copied);
 	}
