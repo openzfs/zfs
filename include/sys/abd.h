64e703a566d768a738c25ecb7bf41ee9ac7ff540
Chunwei Chen <tuxoko@gmail.com>
2015-05-11 22:39:00 +0800

    Introduce ABD: linear/scatter dual typed buffer for ARC
    zfsolinux currently uses vmalloc backed slab for ARC buffers. There are some
    major problems with this approach. One is that 32-bit system have only a
    handful of vmalloc space. Another is that the fragmentation in slab will easily
    deplete memory in busy system.
    
    With ABD, we use scatterlist to allocate data buffers. In this approach we can
    allocate in HIGHMEM, which alleviates vmalloc space pressure on 32-bit. Also,
    we don't have to rely on slab, so there's no fragmentation issue.
    
    But for metadata buffers, we still uses linear buffer from slab. The reason for
    this is that there are a lot of *_phys pointers directly point to metadata
    buffers. Thus, it would be much more complicated to use scatter buffer for
    metadata.
    
    Currently, ABD is not enabled and its API will treat them as normal buffers.
    We will enable it once all relevant code is modified to use the API.
    
    Signed-off-by: Chunwei Chen <tuxoko@gmail.com>

---
+++ include/sys/abd.h
@@ -0,0 +1,304 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright (c) 2015 by Chunwei Chen. All rights reserved.
+ */
+
+/*
+ * ABD - ARC buffer data
+ * ABD is an abstract data structure for ARC. There are two types of ABD:
+ * linear for metadata and scatter for data.
+ * The public API will automatically determine the type.
+ */
+
+#ifndef _ABD_H
+#define	_ABD_H
+
+#include <sys/zfs_context.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#if 0
+#define	ARC_BUF_DATA_MAGIC 0xa7cb0fda
+
+
+typedef struct arc_buf_data {
+	uint32_t	abd_magic;	/* ARC_BUF_DATA_MAGIC */
+	uint32_t	abd_flags;
+	size_t		abd_size;	/* buffer size, excluding offset */
+	size_t		abd_offset;	/* offset in the first segment */
+	int		abd_nents;	/* num of sgl entries */
+	union {
+		struct scatterlist *abd_sgl;
+		void *abd_buf;
+	};
+} abd_t;
+
+#define	ABD_F_SCATTER	(0)		/* abd is scatter */
+#define	ABD_F_LINEAR	(1)		/* abd is linear */
+#define	ABD_F_OWNER	(1<<1)		/* abd owns the buffer */
+#define	ABD_F_HIGHMEM	(1<<2)		/* abd uses highmem */
+#define	ABD_F_SG_CHAIN	(1<<3)		/* scatterlist is chained */
+
+#define	ABD_IS_SCATTER(abd)	(!((abd)->abd_flags & ABD_F_LINEAR))
+#define	ABD_IS_LINEAR(abd)	(!ABD_IS_SCATTER(abd))
+#define	ASSERT_ABD_SCATTER(abd)	ASSERT(ABD_IS_SCATTER(abd))
+#define	ASSERT_ABD_LINEAR(abd)	ASSERT(ABD_IS_LINEAR(abd))
+
+/*
+ * Convert an linear ABD to normal buffer
+ */
+static inline void *
+abd_to_buf(abd_t *abd)
+{
+	ASSERT(abd->abd_magic == ARC_BUF_DATA_MAGIC);
+	ASSERT_ABD_LINEAR(abd);
+	return (abd->abd_buf);
+}
+#define	ABD_TO_BUF(abd)		abd_to_buf(abd)
+
+void abd_init(void);
+void abd_fini(void);
+
+/*
+ * Allocations and deallocations
+ */
+abd_t *abd_alloc_scatter(size_t);
+abd_t *abd_alloc_linear(size_t);
+void abd_free(abd_t *, size_t);
+abd_t *abd_get_offset(abd_t *, size_t);
+abd_t *abd_get_from_buf(void *, size_t);
+void abd_put(abd_t *);
+
+/*
+ * ABD operations
+ */
+void abd_iterate_rfunc(abd_t *, size_t,
+    int (*)(const void *, uint64_t, void *), void *);
+void abd_iterate_wfunc(abd_t *, size_t,
+    int (*)(void *, uint64_t, void *), void *);
+void abd_iterate_func2(abd_t *, abd_t *, size_t, size_t,
+    int (*)(void *, void *, uint64_t, uint64_t, void *), void *);
+void abd_copy_off(abd_t *, abd_t *, size_t, size_t, size_t);
+void abd_copy_from_buf_off(abd_t *, const void *, size_t, size_t);
+void abd_copy_to_buf_off(void *, abd_t *, size_t, size_t);
+int abd_cmp(abd_t *, abd_t *, size_t);
+int abd_cmp_buf_off(abd_t *, const void *, size_t, size_t);
+void abd_zero_off(abd_t *, size_t, size_t);
+#ifdef _KERNEL
+int abd_copy_to_user_off(void __user *, abd_t *, size_t, size_t);
+int abd_copy_from_user_off(abd_t *, const void __user *, size_t, size_t);
+int abd_uiomove_off(abd_t *, size_t, enum uio_rw, uio_t *, size_t);
+int abd_uiocopy_off(abd_t *, size_t, enum uio_rw, uio_t *, size_t *,
+    size_t);
+unsigned int abd_scatter_bio_map_off(struct bio *, abd_t *, unsigned int,
+    size_t);
+unsigned long abd_bio_nr_pages_off(abd_t *, unsigned int, size_t);
+
+#define	abd_bio_map_off(bio, abd, size, off)				\
+(									\
+{									\
+	unsigned int ___ret;						\
+	if (ABD_IS_LINEAR(abd))						\
+		___ret = bio_map(bio, ABD_TO_BUF(abd) + (off), size);	\
+	else								\
+		___ret = abd_scatter_bio_map_off(bio, abd, size, off);	\
+	___ret;								\
+}									\
+)
+#endif	/* _KERNEL */
+
+/* forward declaration for abd_borrow_buf, etc. */
+void *zio_buf_alloc(size_t size);
+void zio_buf_free(void *buf, size_t size);
+
+/*
+ * Borrow a linear buffer for an ABD
+ * Will allocate if ABD is scatter
+ */
+static inline void*
+abd_borrow_buf(abd_t *abd, size_t size)
+{
+	if (!abd)
+		return (NULL);
+	if (ABD_IS_LINEAR(abd))
+		return (ABD_TO_BUF(abd));
+	return (zio_buf_alloc(size));
+}
+
+/*
+ * Borrow a linear buffer for an ABD
+ * Will allocate and copy if ABD is scatter
+ */
+static inline void *
+abd_borrow_buf_copy(abd_t *abd, size_t size)
+{
+	void *buf = abd_borrow_buf(abd, size);
+	if (buf && !ABD_IS_LINEAR(abd))
+		abd_copy_to_buf_off(buf, abd, size, 0);
+	return (buf);
+}
+
+/*
+ * Return the borrowed linear buffer
+ */
+static inline void
+abd_return_buf(abd_t *abd, void *buf, size_t size)
+{
+	if (buf) {
+		if (ABD_IS_LINEAR(abd))
+			ASSERT(buf == ABD_TO_BUF(abd));
+		else
+			zio_buf_free(buf, size);
+	}
+}
+
+/*
+ * Copy back to ABD and return the borrowed linear buffer
+ */
+static inline void
+abd_return_buf_copy(abd_t *abd, void *buf, size_t size)
+{
+	if (buf && !ABD_IS_LINEAR(abd))
+		abd_copy_from_buf_off(abd, buf, size, 0);
+	abd_return_buf(abd, buf, size);
+}
+#else	/* 0 */
+typedef void abd_t;
+#define	ABD_TO_BUF(abd)		((void *)abd)
+#define	ABD_IS_SCATTER(abd)	(0)
+#define	ABD_IS_LINEAR(abd)	(1)
+#define	ASSERT_ABD_SCATTER(abd)	((void)0)
+#define	ASSERT_ABD_LINEAR(abd)	((void)0)
+void *zio_buf_alloc(size_t);
+void zio_buf_free(void *, size_t);
+static inline abd_t *abd_alloc_linear(size_t size)
+{
+	return ((abd_t *)zio_buf_alloc(size));
+}
+static inline void abd_free(abd_t *abd, size_t size)
+{
+	zio_buf_free((void *)abd, size);
+}
+#define	abd_alloc_scatter		abd_alloc_linear
+#define	abd_get_offset(abd, off)	((void *)(abd)+(off))
+#define	abd_get_from_buf(buf, size)	(buf)
+#define	abd_put(abd)			do { } while (0)
+
+#define	abd_iterate_rfunc(a, n, f, p) \
+	(void) f(a, n, p)
+
+#define	abd_iterate_wfunc(a, n, f, p) \
+	(void) f(a, n, p)
+
+#define	abd_iterate_func2(a, b, an, bn, f, p) \
+	(void) f(a, b, an, bn, p)
+
+#define	abd_copy_off(a, b, n, aoff, boff) \
+	(void) memcpy((void *)(a)+(aoff), (void *)(b)+(boff), n)
+
+#define	abd_copy_from_buf_off(a, b, n, off) \
+	(void) memcpy((void *)(a)+(off), b, n)
+
+#define	abd_copy_to_buf_off(a, b, n, off) \
+	(void) memcpy(a, (void *)(b)+(off), n)
+
+#define	abd_cmp(a, b, n) \
+	memcmp(a, b, n)
+
+#define	abd_cmp_buf_off(a, b, n, off) \
+	memcmp((void *)(a)+(off), b, n)
+
+#define	abd_zero_off(a, n, off) \
+	(void) memset((void *)(a)+(off), 0, n)
+
+#ifdef _KERNEL
+#define	abd_copy_to_user_off(a, b, n, off) \
+	copy_to_user(a, (void *)(b)+(off), n)
+
+#define	abd_copy_from_user_off(a, b, n, off) \
+	copy_from_user((void *)(a)+(off), b, n)
+
+#define	abd_uiomove_off(p, n, rw, uio, off) \
+	uiomove((void *)(p)+(off), n, rw, uio)
+
+#define	abd_uiocopy_off(p, n, rw, uio, c, off) \
+	uiocopy((void *)(p)+(off), n, rw, uio, c)
+
+#define	abd_bio_map_off(bio, a, n, off) \
+	bio_map(bio, (void *)(a)+(off), n)
+
+#define	abd_bio_nr_pages_off(a, n, off) \
+	bio_nr_pages((void *)(a)+(off), n)
+#endif /* _KERNEL */
+
+#define	abd_borrow_buf(a, n) \
+	((void *)a)
+
+#define	abd_borrow_buf_copy(a, n) \
+	((void *)a)
+
+#define	abd_return_buf(a, b, n) \
+	do { } while (0)
+
+#define	abd_return_buf_copy(a, b, n) \
+	do { } while (0)
+#endif	/* 0 */
+
+/*
+ * Wrappers for zero off functions
+ */
+#define	abd_copy(dabd, sabd, size) \
+	abd_copy_off(dabd, sabd, size, 0, 0)
+
+#define	abd_copy_from_buf(abd, buf, size) \
+	abd_copy_from_buf_off(abd, buf, size, 0)
+
+#define	abd_copy_to_buf(buf, abd, size) \
+	abd_copy_to_buf_off(buf, abd, size, 0)
+
+#define	abd_cmp_buf(abd, buf, size) \
+	abd_cmp_buf_off(abd, buf, size, 0)
+
+#define	abd_zero(abd, size) \
+	abd_zero_off(abd, size, 0)
+
+#ifdef _KERNEL
+#define	abd_copy_to_user(buf, abd, size) \
+	abd_copy_to_user_off(buf, abd, size, 0)
+
+#define	abd_copy_from_user(abd, buf, size) \
+	abd_copy_from_user_off(abd, buf, size, 0)
+
+#define	abd_uiomove(abd, n, rw, uio) \
+	abd_uiomove_off(abd, n, rw, uio, 0)
+
+#define	abd_uiocopy(abd, n, rw, uio, c) \
+	abd_uiocopy_off(abd, n, rw, uio, c, 0)
+#endif	/* _KERNEL */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* _ABD_H */
