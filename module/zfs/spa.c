7485f83f949b8cbf0bc8b7d43e0cf4c4d4c9f59a
Chunwei Chen <tuxoko@gmail.com>
2015-05-12 16:58:48 +0800

    Handle abd_t in sa_impl.h, sa.c, space_map.c, spa.c, spa_history.c and zil.c
    Signed-off-by: Chunwei Chen <tuxoko@gmail.com>

--- module/zfs/spa.c
+++ module/zfs/spa.c
@@ -24,6 +24,7 @@
  * Copyright (c) 2013 by Delphix. All rights reserved.
  * Copyright (c) 2013, 2014, Nexenta Systems, Inc.  All rights reserved.
  * Copyright (c) 2014 Spectra Logic Corporation, All rights reserved.
+ * Copyright (c) 2015 by Chunwei Chen. All rights reserved.
  */
 
 /*
@@ -61,6 +62,7 @@
 #include <sys/dsl_synctask.h>
 #include <sys/fs/zfs.h>
 #include <sys/arc.h>
+#include <sys/abd.h>
 #include <sys/callb.h>
 #include <sys/systeminfo.h>
 #include <sys/spa_boot.h>
@@ -1618,7 +1620,7 @@ load_nvlist(spa_t *spa, uint64_t obj, nvlist_t **value)
 	if (error)
 		return (error);
 
-	nvsize = *(uint64_t *)db->db_data;
+	nvsize = *(uint64_t *)ABD_TO_BUF(db->db_data);
 	dmu_buf_rele(db, FTAG);
 
 	packed = vmem_alloc(nvsize, KM_SLEEP);
@@ -1902,7 +1904,7 @@ spa_load_verify_done(zio_t *zio)
 		else
 			atomic_inc_64(&sle->sle_data_count);
 	}
-	zio_data_buf_free(zio->io_data, zio->io_size);
+	abd_free(zio->io_data, zio->io_size);
 
 	mutex_enter(&spa->spa_scrub_lock);
 	spa->spa_scrub_inflight--;
@@ -1925,7 +1927,7 @@ spa_load_verify_cb(spa_t *spa, zilog_t *zilog, const blkptr_t *bp,
 {
 	zio_t *rio;
 	size_t size;
-	void *data;
+	abd_t *data;
 
 	if (bp == NULL || BP_IS_HOLE(bp) || BP_IS_EMBEDDED(bp))
 		return (0);
@@ -1941,7 +1943,7 @@ spa_load_verify_cb(spa_t *spa, zilog_t *zilog, const blkptr_t *bp,
 
 	rio = arg;
 	size = BP_GET_PSIZE(bp);
-	data = zio_data_buf_alloc(size);
+	data = abd_alloc_linear(size);
 
 	mutex_enter(&spa->spa_scrub_lock);
 	while (spa->spa_scrub_inflight >= spa_load_verify_maxinflight)
@@ -6037,7 +6039,7 @@ spa_sync_nvlist(spa_t *spa, uint64_t obj, nvlist_t *nv, dmu_tx_t *tx)
 
 	VERIFY(0 == dmu_bonus_hold(spa->spa_meta_objset, obj, FTAG, &db));
 	dmu_buf_will_dirty(db, tx);
-	*(uint64_t *)db->db_data = nvsize;
+	*(uint64_t *)ABD_TO_BUF(db->db_data) = nvsize;
 	dmu_buf_rele(db, FTAG);
 }
 
--- module/zfs/spa_history.c
+++ module/zfs/spa_history.c
@@ -22,8 +22,10 @@
 /*
  * Copyright (c) 2006, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2011, 2014 by Delphix. All rights reserved.
+ * Copyright (c) 2015 by Chunwei Chen. All rights reserved.
  */
 
+#include <sys/abd.h>
 #include <sys/spa.h>
 #include <sys/spa_impl.h>
 #include <sys/zap.h>
@@ -99,7 +101,7 @@ spa_history_create_obj(spa_t *spa, dmu_tx_t *tx)
 	VERIFY(0 == dmu_bonus_hold(mos, spa->spa_history, FTAG, &dbp));
 	ASSERT(dbp->db_size >= sizeof (spa_history_phys_t));
 
-	shpp = dbp->db_data;
+	shpp = ABD_TO_BUF(dbp->db_data);
 	dmu_buf_will_dirty(dbp, tx);
 
 	/*
@@ -222,7 +224,7 @@ spa_history_log_sync(void *arg, dmu_tx_t *tx)
 	 * Update the offset when the write completes.
 	 */
 	VERIFY0(dmu_bonus_hold(mos, spa->spa_history, FTAG, &dbp));
-	shpp = dbp->db_data;
+	shpp = ABD_TO_BUF(dbp->db_data);
 
 	dmu_buf_will_dirty(dbp, tx);
 
@@ -361,7 +363,7 @@ spa_history_get(spa_t *spa, uint64_t *offp, uint64_t *len, char *buf)
 
 	if ((err = dmu_bonus_hold(mos, spa->spa_history, FTAG, &dbp)) != 0)
 		return (err);
-	shpp = dbp->db_data;
+	shpp = ABD_TO_BUF(dbp->db_data);
 
 #ifdef ZFS_DEBUG
 	{
