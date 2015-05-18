4bf82b2c1966ce35a01497e5bc5dd1617c5a58e2
Chunwei Chen <tuxoko@gmail.com>
2015-05-18 23:06:43 +0800

    Enable scatter ABD for DMU_OT_DNODE
    Use non-highmem scatter ABD for dnode array blocks, abd_array should be used
    to access the dnodes.
    
    Signed-off-by: Chunwei Chen <tuxoko@gmail.com>

--- module/zfs/dmu_traverse.c
+++ module/zfs/dmu_traverse.c
@@ -331,16 +331,17 @@ traverse_visitbp(traverse_data_t *td, const dnode_phys_t *dnp,
 		    ZIO_PRIORITY_ASYNC_READ, ZIO_FLAG_CANFAIL, &flags, zb);
 		if (err != 0)
 			goto post;
-		cdnp = ABD_TO_BUF(buf->b_data);
 
 		for (i = 0; i < epb; i++) {
-			prefetch_dnode_metadata(td, &cdnp[i], zb->zb_objset,
+			cdnp = abd_array(buf->b_data, i, dnode_phys_t);
+			prefetch_dnode_metadata(td, cdnp, zb->zb_objset,
 			    zb->zb_blkid * epb + i);
 		}
 
 		/* recursively visitbp() blocks below this */
 		for (i = 0; i < epb; i++) {
-			err = traverse_dnode(td, &cdnp[i], zb->zb_objset,
+			cdnp = abd_array(buf->b_data, i, dnode_phys_t);
+			err = traverse_dnode(td, cdnp, zb->zb_objset,
 			    zb->zb_blkid * epb + i);
 			if (err != 0)
 				break;
