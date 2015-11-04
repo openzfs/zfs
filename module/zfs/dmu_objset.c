3aa4fe6c89e62d02320fc52923be41e4b0b2c299
Justin T. Gibbs <gibbs@scsiguy.com>
2015-11-05 00:00:58 +0100

    Illumos 6171 dsl_prop_unregister() slows down dataset eviction.
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Prakash Surya <prakash.surya@delphix.com>
    Approved by: Dan McDonald <danmcd@omniti.com>
    
    References:
    https://www.illumos.org/issues/6171 dsl_prop_unregister() slows down dataset eviction.
    https://github.com/illumos/illumos-gate/commit/03bad06fbb261fd4a7151a70dfeff2f5041cce1f#diff-832418f8bef771ab321cc8bca7235e4dL297
    
    Porting notes:
    Notable changes from upstream are caused by the following commits:
    https://github.com/zfsonlinux/zfs/commit/3558fd73b5d863304102f6745c26e0b592aca60a Prototype/structure update for Linux
    https://github.com/zfsonlinux/zfs/commit/2cf7f52bc42f215d4ef27d0fd75fc1b1417cb841 Linux compat 2.6.39: mount_nodev()
    https://github.com/zfsonlinux/zfs/commit/13fe019870c8779bf2f5b3ff731b512cf89133ef Illumos #3464
    
    Fix compilation error:
    dsl_prop.c:1211:15: error: ‘dsl_prop_unregister’ undeclared here (not in a function)
     EXPORT_SYMBOL(dsl_prop_unregister);
    
    Replacing
    EXPORT_SYMBOL(dsl_prop_unregister);
    with
    EXPORT_SYMBOL(dsl_prop_unregister_all);
    
    Fixing 'space or tab at end of line' in include/sys/dsl_dataset.h
    
    Conflicts during rebase on December 9th 2015
    in include/sys/dsl_dataset.h
    due to https://github.com/zfsonlinux/zfs/commit/241b5415748859a3c272fc8f570f2368e93adde9
    (Illumos 5959 - clean up per-dataset feature count code)
    
    Ported-by: kernelOfTruth kerneloftruth@gmail.com

--- module/zfs/dmu_objset.c
+++ module/zfs/dmu_objset.c
@@ -688,40 +688,8 @@ dmu_objset_evict(objset_t *os)
 	for (t = 0; t < TXG_SIZE; t++)
 		ASSERT(!dmu_objset_is_dirty(os, t));
 
-	if (ds) {
-		if (!ds->ds_is_snapshot) {
-			VERIFY0(dsl_prop_unregister(ds,
-			    zfs_prop_to_name(ZFS_PROP_CHECKSUM),
-			    checksum_changed_cb, os));
-			VERIFY0(dsl_prop_unregister(ds,
-			    zfs_prop_to_name(ZFS_PROP_COMPRESSION),
-			    compression_changed_cb, os));
-			VERIFY0(dsl_prop_unregister(ds,
-			    zfs_prop_to_name(ZFS_PROP_COPIES),
-			    copies_changed_cb, os));
-			VERIFY0(dsl_prop_unregister(ds,
-			    zfs_prop_to_name(ZFS_PROP_DEDUP),
-			    dedup_changed_cb, os));
-			VERIFY0(dsl_prop_unregister(ds,
-			    zfs_prop_to_name(ZFS_PROP_LOGBIAS),
-			    logbias_changed_cb, os));
-			VERIFY0(dsl_prop_unregister(ds,
-			    zfs_prop_to_name(ZFS_PROP_SYNC),
-			    sync_changed_cb, os));
-			VERIFY0(dsl_prop_unregister(ds,
-			    zfs_prop_to_name(ZFS_PROP_REDUNDANT_METADATA),
-			    redundant_metadata_changed_cb, os));
-			VERIFY0(dsl_prop_unregister(ds,
-			    zfs_prop_to_name(ZFS_PROP_RECORDSIZE),
-			    recordsize_changed_cb, os));
-		}
-		VERIFY0(dsl_prop_unregister(ds,
-		    zfs_prop_to_name(ZFS_PROP_PRIMARYCACHE),
-		    primary_cache_changed_cb, os));
-		VERIFY0(dsl_prop_unregister(ds,
-		    zfs_prop_to_name(ZFS_PROP_SECONDARYCACHE),
-		    secondary_cache_changed_cb, os));
-	}
+	if (ds)
+		dsl_prop_unregister_all(ds, os);
 
 	if (os->os_sa)
 		sa_tear_down(os);
