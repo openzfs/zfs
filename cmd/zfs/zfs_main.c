cfe86c01d5b3eec4a490de16a1db79c1244395ed
Milan Jurik <milan.jurik@xylab.cz>
2015-12-30 14:43:27 -0800

    Illumos 2077 - lots of unreachable breaks in illumos gate
    2077 lots of unreachable breaks in illumos gate
    Reviewed by: Dan McDonald <danmcd@nexenta.com>
    Reviewed by: Garrett D'Amore <garrett@damore.org>
    Approved by: Richard Lowe <richlowe@richlowe.net>
    
    References:
      https://www.illumos.org/issues/2077
      https://github.com/illumos/illumos-gate/commit/33f5ff1
    
    Porting notes:
    - Only one file of the original patch applied to ZFS
    - Minor formating change to align copyright block with upstream
    
    Ported-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- cmd/zfs/zfs_main.c
+++ cmd/zfs/zfs_main.c
@@ -21,10 +21,11 @@
 
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2011, 2014 by Delphix. All rights reserved.
+ * Copyright (c) 2011, 2015 by Delphix. All rights reserved.
+ * Copyright 2012 Milan Jurik. All rights reserved.
  * Copyright (c) 2012, Joyent, Inc. All rights reserved.
  * Copyright (c) 2013 Steven Hartland.  All rights reserved.
- * Copyright 2013 Nexenta Systems, Inc. All rights reserved.
+ * Copyright 2013 Nexenta Systems, Inc.  All rights reserved.
  */
 
 #include <assert.h>
@@ -802,7 +803,6 @@ zfs_do_create(int argc, char **argv)
 			(void) fprintf(stderr, gettext("missing size "
 			    "argument\n"));
 			goto badusage;
-			break;
 		case '?':
 			(void) fprintf(stderr, gettext("invalid option '%c'\n"),
 			    optopt);
