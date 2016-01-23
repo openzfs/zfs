7a771d8b9e961ab903d788cf1ed47c89ed88499c
Simon Klinkert <simon.klinkert@gmail.com>
2016-01-22 16:00:59 -0800

    llumos 6334 - Cannot unlink files when over quota
    6334 Cannot unlink files when over quota
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Toomas Soome <tsoome@me.com>
    Approved by: Dan McDonald <danmcd@omniti.com>
    
    References:
      https://www.illumos.org/issues/6334
      https://github.com/illumos/illumos-gate/commit/6575bca
    
    Ported-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- module/zfs/zfs_vnops.c
+++ module/zfs/zfs_vnops.c
@@ -1628,12 +1628,9 @@ top:
 	dmu_tx_hold_zap(tx, zsb->z_unlinkedobj, FALSE, NULL);
 
 	/*
-	 * Mark this transaction as typically resulting in a net free of
-	 * space, unless object removal will be delayed indefinitely
-	 * (due to active holds on the vnode due to the file being open).
+	 * Mark this transaction as typically resulting in a net free of space
 	 */
-	if (may_delete_now)
-		dmu_tx_mark_netfree(tx);
+	dmu_tx_mark_netfree(tx);
 
 	error = dmu_tx_assign(tx, waited ? TXG_WAITED : TXG_NOWAIT);
 	if (error) {
