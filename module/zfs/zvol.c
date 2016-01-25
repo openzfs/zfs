0a74ece5e2d1693389a0e10514742879507fcf33
Brian Behlendorf <behlendorf1@llnl.gov>
2016-01-25 01:12:22 +0100

    https://github.com/zfsonlinux/zfs/issues/4203

--- module/zfs/zvol.c
+++ module/zfs/zvol.c
@@ -1361,6 +1361,7 @@ __zvol_create_minor(const char *name, boolean_t ignore_snapdev)
 	blk_queue_max_segments(zv->zv_queue, UINT16_MAX);
 	blk_queue_max_segment_size(zv->zv_queue, UINT_MAX);
 	blk_queue_physical_block_size(zv->zv_queue, zv->zv_volblocksize);
+	blk_queue_logical_block_size(zv->zv_queue, zv->zv_volblocksize);
 	blk_queue_io_opt(zv->zv_queue, zv->zv_volblocksize);
 	blk_queue_max_discard_sectors(zv->zv_queue,
 	    (zvol_max_discard_blocks * zv->zv_volblocksize) >> 9);
