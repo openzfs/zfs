a1bc34c0a7929130577e4c9dd418845674dd35c3
Dimitri John Ledkov <xnox@ubuntu.com>
2016-03-16 21:53:20 +0000

    Add support for s390[x].
    Signed-off-by: Dimitri John Ledkov <xnox@ubuntu.com>
    Signed-off-by: Richard Yao <ryao@gentoo.org>
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Closes #4425

--- lib/libefi/rdwr_efi.c
+++ lib/libefi/rdwr_efi.c
@@ -154,7 +154,7 @@ struct dk_map2  default_vtoc_map[NDKMAP] = {
 #if defined(_SUNOS_VTOC_16)
 
 #if defined(i386) || defined(__amd64) || defined(__arm) || \
-    defined(__powerpc) || defined(__sparc)
+    defined(__powerpc) || defined(__sparc) || defined(__s390__)
 	{	V_BOOT,		V_UNMNT	},		/* i - 8 */
 	{	V_ALTSCTR,	0	},		/* j - 9 */
 
