openzfs/zfs

commit e4023e42a8cb0d267870db82f75e23d4efb9fbd9

Only build devname2devid when libudev headers are available
Accidentally introduced by commit 39fc0cb.  The devname2devid utility
which depends on libudev must only be built when libudev headers are
available.  This is accomplished through an AM_CONDITIONAL.

Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
Issue #4416

config/user-libudev.m4
@@ -4,10 +4,17 @@ dnl #
 AC_DEFUN([ZFS_AC_CONFIG_USER_LIBUDEV], [
 	LIBUDEV=
 
-	AC_CHECK_HEADER([libudev.h], [AC_SUBST([LIBUDEV], ["-ludev"])
-	    AC_DEFINE([HAVE_LIBUDEV], 1, [Define if you have libudev])], [])
+	AC_CHECK_HEADER([libudev.h], [
+	    libudev=yes
+	    AC_SUBST([LIBUDEV], ["-ludev"])
+	    AC_DEFINE([HAVE_LIBUDEV], 1, [Define if you have libudev])
+	], [
+	    libudev=no
+	])
 
-	AC_SEARCH_LIBS(i[udev_device_get_is_initialized], [udev], [
+	AM_CONDITIONAL([WANT_DEVNAME2DEVID], [ test x$libudev = xyes ])
+	AC_SEARCH_LIBS([udev_device_get_is_initialized], [udev], [
 	    AC_DEFINE([HAVE_LIBUDEV_UDEV_DEVICE_GET_IS_INITIALIZED], 1, [
 	    Define if udev_device_get_is_initialized is available])], [])
+
 ])

tests/zfs-tests/cmd/devname2devid/Makefile.am
@@ -2,6 +2,8 @@ include $(top_srcdir)/config/Rules.am
 
 pkgexecdir = $(datadir)/@PACKAGE@/zfs-tests/bin
 
+if WANT_DEVNAME2DEVID
 pkgexec_PROGRAMS = devname2devid
 devname2devid_SOURCES = devname2devid.c
 devname2devid_LDADD = -ludev
+endif

commit 63e0828c174e84ec583d34327b3fc1c5f53fa39c

Fix WANT_DEVNAME2DEVID configure error
Accidentally introduced by commit e4023e4.  The AM_CONDITIONAL
cannot be located where it can be invoked conditionally, as in
the `--with-config=user` case.  Relocate it to the top level
ZFS_AC_CONFIG macro along with the other AM_CONDITIONALs.

Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
Issue #4416

config/user-libudev.m4
@@ -5,14 +5,13 @@ AC_DEFUN([ZFS_AC_CONFIG_USER_LIBUDEV], [
 	LIBUDEV=
 
 	AC_CHECK_HEADER([libudev.h], [
-	    libudev=yes
+	    user_libudev=yes
 	    AC_SUBST([LIBUDEV], ["-ludev"])
 	    AC_DEFINE([HAVE_LIBUDEV], 1, [Define if you have libudev])
 	], [
-	    libudev=no
+	    user_libudev=no
 	])
 
-	AM_CONDITIONAL([WANT_DEVNAME2DEVID], [ test x$libudev = xyes ])
 	AC_SEARCH_LIBS([udev_device_get_is_initialized], [udev], [
 	    AC_DEFINE([HAVE_LIBUDEV_UDEV_DEVICE_GET_IS_INITIALIZED], 1, [
 	    Define if udev_device_get_is_initialized is available])], [])

config/zfs-build.m4
@@ -100,10 +100,12 @@ AC_DEFUN([ZFS_AC_CONFIG], [
 	esac
 
 	AM_CONDITIONAL([CONFIG_USER],
-		       [test "$ZFS_CONFIG" = user -o "$ZFS_CONFIG" = all])
+	    [test "$ZFS_CONFIG" = user -o "$ZFS_CONFIG" = all])
 	AM_CONDITIONAL([CONFIG_KERNEL],
-		       [test "$ZFS_CONFIG" = kernel -o "$ZFS_CONFIG" = all] &&
-		       [test "x$enable_linux_builtin" != xyes ])
+	    [test "$ZFS_CONFIG" = kernel -o "$ZFS_CONFIG" = all] &&
+	    [test "x$enable_linux_builtin" != xyes ])
+	AM_CONDITIONAL([WANT_DEVNAME2DEVID],
+	    [test "x$user_libudev" = xyes ])
 ])
 
 dnl #