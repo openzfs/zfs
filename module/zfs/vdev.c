657a74d6b6e8ab0d2294ab6e1bf464c423cfb03a
Chris Dunlop <chris@onthe.net.au>
2016-06-24 00:45:19 +1000

    FreeBSD rS271776 Persist vdev_resilver_txg changes
    Persist vdev_resilver_txg changes to avoid panic caused by validation
    vs a vdev_resilver_txg value from a previous resilver.
    
    Author: smh <smh@FreeBSD.org>
    
    References:
      https://reviews.freebsd.org/rS271776
      https://github.com/freebsd/freebsd@5c7a6f5d
      https://www.illumos.org/issues/5154
    
    Ported-by: Chris Dunlop <chris@onthe.net.au>

--- module/zfs/vdev.c
+++ module/zfs/vdev.c
@@ -1909,12 +1909,15 @@ vdev_dtl_reassess(vdev_t *vd, uint64_t txg, uint64_t scrub_txg, int scrub_done)
 
 		/*
 		 * If the vdev was resilvering and no longer has any
-		 * DTLs then reset its resilvering flag.
+		 * DTLs then reset its resilvering flag and dirty
+		 * the top level so that we persist the change.
 		 */
 		if (vd->vdev_resilver_txg != 0 &&
 		    range_tree_space(vd->vdev_dtl[DTL_MISSING]) == 0 &&
-		    range_tree_space(vd->vdev_dtl[DTL_OUTAGE]) == 0)
+		    range_tree_space(vd->vdev_dtl[DTL_OUTAGE]) == 0) {
 			vd->vdev_resilver_txg = 0;
+			vdev_config_dirty(vd->vdev_top);
+		}
 
 		mutex_exit(&vd->vdev_dtl_lock);
 
