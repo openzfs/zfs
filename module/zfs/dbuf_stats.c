b75f670b0f66bd4ae50b834f8325bae268e5dabe
Chunwei Chen <david.chen@osnexus.com>
2016-07-12 12:23:43 -0700

    Fix dbuf_stats_hash_table_data race
    Dropping DBUF_HASH_MUTEX when walking the hash list is unsafe. The dbuf can be
    freed at any time.
    
    Signed-off-by: Chunwei Chen <david.chen@osnexus.com>

--- module/zfs/dbuf_stats.c
+++ module/zfs/dbuf_stats.c
@@ -148,7 +148,6 @@ dbuf_stats_hash_table_data(char *buf, size_t size, void *data)
 		}
 
 		mutex_enter(&db->db_mtx);
-		mutex_exit(DBUF_HASH_MUTEX(h, dsh->idx));
 
 		if (db->db_state != DB_EVICTING) {
 			length = __dbuf_stats_hash_table_data(buf, size, db);
@@ -157,7 +156,6 @@ dbuf_stats_hash_table_data(char *buf, size_t size, void *data)
 		}
 
 		mutex_exit(&db->db_mtx);
-		mutex_enter(DBUF_HASH_MUTEX(h, dsh->idx));
 	}
 	mutex_exit(DBUF_HASH_MUTEX(h, dsh->idx));
 
