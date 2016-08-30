413c7139b5fdf14f462375e3cbf4dba9f6c2295d
cao <cao.xuewen@zte.com.cn>
2016-08-30 19:32:22 +0800

    delete unuse function zfsctl_snapdir_inactive.
    zfsctl_snapdir_inactive is define zfs-0.6.3 ,
    In zfs-0.6.5.7, this is declaration , but isn't define.
    delete fastreboot_disable_highpil , this is unuse.
    
    Signed-off-by: caoxuewen cao.xuewen@zte.com.cn

--- include/sys/zfs_ctldir.h
+++ include/sys/zfs_ctldir.h
@@ -74,7 +74,6 @@ extern int zfsctl_snapdir_remove(struct inode *dip, char *name, cred_t *cr,
     int flags);
 extern int zfsctl_snapdir_mkdir(struct inode *dip, char *dirname, vattr_t *vap,
     struct inode **ipp, cred_t *cr, int flags);
-extern void zfsctl_snapdir_inactive(struct inode *ip);
 extern int zfsctl_snapshot_mount(struct path *path, int flags);
 extern int zfsctl_snapshot_unmount(char *snapname, int flags);
 extern int zfsctl_snapshot_unmount_delay(spa_t *spa, uint64_t objsetid,
