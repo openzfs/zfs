e3ad187b78b2b31f435a84b9c2258f5bba411241
Brian Behlendorf <behlendorf1@llnl.gov>
2016-10-18 23:49:23 +0000

    Linux 4.9 compat: inode_change_ok() renamed setattr_prepare()
    In torvalds/linux@31051c8 the inode_change_ok() function was
    renamed setattr_prepare() and updated to take a dentry ratheri
    than an inode.  Update the code to call the setattr_prepare()
    and add a wrapper function which call inode_change_ok() for
    older kernels.
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Chunwei Chen <david.chen@osnexus.com>
    Requires-builders: style

--- include/linux/vfs_compat.h
+++ include/linux/vfs_compat.h
@@ -362,4 +362,15 @@ static inline struct inode *file_inode(const struct file *f)
 #define	zpl_follow_up(path)			follow_up(path)
 #endif
 
+/*
+ * 4.9 API change
+ */
+#ifndef HAVE_SETATTR_PREPARE
+static inline int
+setattr_prepare(struct dentry *dentry, struct iattr *ia)
+{
+	return (inode_change_ok(dentry->d_inode, ia));
+}
+#endif
+
 #endif /* _ZFS_VFS_H */
