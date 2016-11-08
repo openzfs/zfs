a36cc8d2428259f5cfefb653bff39ca7afc66518
cao <cao.xuewen@zte.com.cn>
2016-11-09 06:28:17 +0800

    Fix coverity defects: CID 147626, 147628
    CID 147626: Type:Dereference before null check
    CID 147628: Type:Dereference before null check
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov
    Reviewed-by: Chunwei Chen <david.chen@osnexus.com>
    Signed-off-by: cao.xuewen <cao.xuewen@zte.com.cn>
    Closes #5304

--- module/zfs/zfs_vnops.c
+++ module/zfs/zfs_vnops.c
@@ -4256,8 +4256,7 @@ zfs_putpage(struct inode *ip, struct page *pp, struct writeback_control *wbc)
 		 * writepages() normally handles the entire commit for
 		 * performance reasons.
 		 */
-		if (zsb->z_log != NULL)
-			zil_commit(zsb->z_log, zp->z_id);
+		zil_commit(zsb->z_log, zp->z_id);
 	}
 
 	ZFS_EXIT(zsb);
