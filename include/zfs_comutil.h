edd6dacd1754e07fd86b3f0e0f7d4bb86598a3cd
Pavel Zakharov <pavel.zakharov@delphix.com>
2017-02-10 17:51:09 -0500

    OpenZFS 9235 - rename zpool_rewind_policy_t to zpool_load_policy_t
    We want to be able to pass various settings during import/open of a
    pool, which are not only related to rewind. Instead of adding a new
    policy and duplicate a bunch of code, we should just rename
    rewind_policy to a more generic term like load_policy.
    
    For instance, we'd like to set spa->spa_import_flags from the nvlist,
    rather from a flags parameter passed to spa_import as in some cases we
    want those flags not only for the import case, but also for the open
    case. One such flag could be ZFS_IMPORT_MISSING_LOG (as used in zdb)
    which would allow zfs to open a pool when logs are missing.
    
    Authored by: Pavel Zakharov <pavel.zakharov@delphix.com>
    Reviewed by: Matt Ahrens <matt@delphix.com>
    Reviewed by: George Wilson <george.wilson@delphix.com>
    Approved by: Robert Mustacchi <rm@joyent.com>
    Ported-by: Brian Behlendorf <behlendorf1@llnl.gov>
    
    OpenZFS-issue: https://illumos.org/issues/9235
    OpenZFS-commit: https://github.com/openzfs/openzfs/commit/d2b1e44

--- include/zfs_comutil.h
+++ include/zfs_comutil.h
@@ -20,7 +20,7 @@
  */
 /*
  * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2012 by Delphix. All rights reserved.
+ * Copyright (c) 2012, 2017 by Delphix. All rights reserved.
  */
 
 #ifndef	_ZFS_COMUTIL_H
@@ -34,7 +34,7 @@ extern "C" {
 #endif
 
 extern boolean_t zfs_allocatable_devs(nvlist_t *);
-extern void zpool_get_rewind_policy(nvlist_t *, zpool_rewind_policy_t *);
+extern void zpool_get_load_policy(nvlist_t *, zpool_load_policy_t *);
 
 extern int zfs_zpl_version_map(int spa_version);
 extern int zfs_spa_version_map(int zpl_version);
