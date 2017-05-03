35973b5d0ce058537d8aea55bf303714758ab9a5
Brian Behlendorf <behlendorf1@llnl.gov>
2017-05-02 17:37:14 -0700

    Default to zvol_request_async=0
    Change the default ZVOL behavior so requests are handled asynchronously.
    This behavior is functionally the same as in the zfs-0.6.4 release.
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- module/zfs/zvol.c
+++ module/zfs/zvol.c
@@ -57,7 +57,7 @@
 unsigned int zvol_inhibit_dev = 0;
 unsigned int zvol_major = ZVOL_MAJOR;
 unsigned int zvol_threads = 32;
-unsigned int zvol_request_sync = 1;
+unsigned int zvol_request_sync = 0;
 unsigned int zvol_prefetch_bytes = (128 * 1024);
 unsigned long zvol_max_discard_blocks = 16384;
 
