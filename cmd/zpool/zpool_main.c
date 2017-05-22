a9fcf1eb14fe404a00ea7095f2f3b050df296956
Saso Kiselkov <skiselkov@gmail.com>
2017-05-22 09:21:08 +0200

    Matt Ahrens' review comments, round 5.

--- cmd/zpool/zpool_main.c
+++ cmd/zpool/zpool_main.c
@@ -6534,7 +6534,8 @@ zpool_do_resilver(int argc, char **argv)
  * zpool trim [-s|-r <rate>] <pool> ...
  *
  *	-s		Stop. Stops any in-progress trim.
- *	-r <rate>	Sets the TRIM rate.
+ *	-r <rate>	Sets the TRIM rate in bytes (per second). Supports
+ *			adding a multiplier suffix such as 'k' or 'm'.
  */
 int
 zpool_do_trim(int argc, char **argv)
@@ -6908,11 +6909,11 @@ print_trim_status(uint64_t trim_prog, uint64_t total_size, uint64_t rate,
 			char rate_str[32];
 			zfs_nicenum(rate, rate_str, sizeof (rate_str));
 			(void) printf("  trim: %.02f%%\tstarted: %s\t"
-			    "(rate: %s/s)\n", MIN((((double)trim_prog) /
+			    "(rate limit: %s/s)\n", MIN((((double)trim_prog) /
 			    total_size) * 100, 100), buf, rate_str);
 		} else {
 			(void) printf("  trim: %.02f%%\tstarted: %s\t"
-			    "(rate: max)\n", MIN((((double)trim_prog) /
+			    "(rate limit: none)\n", MIN((((double)trim_prog) /
 			    total_size) * 100, 100), buf);
 		}
 	} else {
