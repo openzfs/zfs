3b19d22664336389bfd136afa2c136367794b94a
chrisrd <chris@onthe.net.au>
2017-09-30 08:49:19 +1000

    Scale the dbuf cache with arc_c
    Commit d3c2ae1 introduced a dbuf cache with a default size of the
    minimum of 100M or 1/32 maximum ARC size. (These figures may be adjusted
    using dbuf_cache_max_bytes and dbuf_cache_max_shift.) The dbuf cache
    is counted as metadata for the purposes of ARC size calculations.
    
    On a 1GB box the ARC maximum size defaults to c_max 493M which gives a
    dbuf cache default minimum size of 15.4M, and the ARC metadata defaults
    to minimum 16M. I.e. the dbuf cache is an significant proportion of the
    minimum metadata size. With other overheads involved this actually means
    the ARC metadata doesn't get down to the minimum.
    
    This patch dynamically scales the dbuf cache to the target ARC size
    instead of statically scaling it to the maximum ARC size. (The scale is
    still set by dbuf_cache_max_shift and the maximum size is still fixed by
    dbuf_cache_max_bytes.) Using the target ARC size rather than the current
    ARC size is done to help the ARC reach the target rather than simply
    focusing on the current size.
    
    Reviewed-by: Chunwei Chen <tuxoko@gmail.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: George Melikov <mail@gmelikov.ru>
    Signed-off-by: Chris Dunlop <chris@onthe.net.au>
    Issue #6506
    Closes #6561

--- include/sys/arc.h
+++ include/sys/arc.h
@@ -248,7 +248,7 @@ void arc_flush(spa_t *spa, boolean_t retry);
 void arc_tempreserve_clear(uint64_t reserve);
 int arc_tempreserve_space(uint64_t reserve, uint64_t txg);
 
-uint64_t arc_max_bytes(void);
+uint64_t arc_target_bytes(void);
 void arc_init(void);
 void arc_fini(void);
 
