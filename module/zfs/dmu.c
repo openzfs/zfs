93c958393a88b2351ca918f25bf4ed89e7b3d3e0
loli10K <ezomori.nozomu@gmail.com>
2017-10-07 15:05:11 +0200

    ZFS send fails to dump objects larger than 128PiB
    When dumping objects larger than 128PiB it's possible for do_dump() to
    miscalculate the FREE_RECORD offset due to an integer overflow
    condition: this prevents the receiving end from correctly restoring
    the dumped object.
    
    Signed-off-by: loli10K <ezomori.nozomu@gmail.com>

--- module/zfs/dmu.c
+++ module/zfs/dmu.c
@@ -967,7 +967,7 @@ dmu_free_range(objset_t *os, uint64_t object, uint64_t offset,
 	if (err)
 		return (err);
 	ASSERT(offset < UINT64_MAX);
-	ASSERT(size == -1ULL || size <= UINT64_MAX - offset);
+	ASSERT(size == DMU_OBJECT_END || size <= UINT64_MAX - offset);
 	dnode_free_range(dn, offset, size, tx);
 	dnode_rele(dn, FTAG);
 	return (0);
