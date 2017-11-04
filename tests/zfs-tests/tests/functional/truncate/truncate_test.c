8cf5b757aa51e494571fbf9380efe605cda262b8
loli10K <ezomori.nozomu@gmail.com>
2017-11-04 15:08:32 +0100

    Fix truncate(2) mtime and ctime handling
    On Linux, ftruncate(2) always changes the file timestamps, even if the
    file size is not changed. However, in case of a successfull
    truncate(2), the timestamps are updated only if the file size changes.
    This translates to the VFS calling the ZFS Posix Layer "setattr"
    function (zpl_setattr) with ATTR_MTIME and ATTR_CTIME unconditionally
    set on the iattr mask only when doing a ftruncate(2), while the
    truncate(2) is left to the filesystem implementation to be dealt with.
    
    This behaviour is consistent with POSIX:2004/SUSv3 specifications
    where there's no explicit requirement for file size changes to update
    the timestamps only for ftruncate(2):
    
    http://pubs.opengroup.org/onlinepubs/009695399/functions/truncate.html
    http://pubs.opengroup.org/onlinepubs/009695399/functions/ftruncate.html
    
    This has been later updated in POSIX:2008/SUSv4 where, for both
    truncate(2)/ftruncate(2), there's no mention of this size change
    requirement:
    
    http://austingroupbugs.net/view.php?id=489
    http://pubs.opengroup.org/onlinepubs/9699919799/functions/truncate.html
    http://pubs.opengroup.org/onlinepubs/9699919799/functions/ftruncate.html
    
    Unfortunately the Linux VFS is still calling into the ZPL without
    ATTR_MTIME/ATTR_CTIME set in the truncate(2) case: we fix this by
    explicitly updating the timestamps when detecting the ATTR_SIZE bit,
    which is always set in do_truncate(), on the iattr mask.
    
    Signed-off-by: loli10K <ezomori.nozomu@gmail.com>

---
+++ tests/zfs-tests/tests/functional/truncate/truncate_test.c
@@ -0,0 +1,103 @@
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source.  A copy of the CDDL is also available via the Internet at
+ * http://www.illumos.org/license/CDDL.
+ */
+
+/*
+ * Copyright (c) 2012, 2014 by Delphix. All rights reserved.
+ * Copyright 2017, loli10K <ezomori.nozomu@gmail.com>. All rights reserved.
+ */
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define	FSIZE	256*1024*1024
+
+static long	fsize = FSIZE;
+static int	errflag = 0;
+static char	*filename = NULL;
+static int	ftruncflag = 0;
+
+static void parse_options(int argc, char *argv[]);
+
+static void
+usage(char *execname)
+{
+	(void) fprintf(stderr,
+	    "usage: %s [-s filesize] [-f] /path/to/file\n", execname);
+	(void) exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int fd;
+
+	parse_options(argc, argv);
+
+	if (ftruncflag) {
+		fd = open(filename, O_RDWR|O_CREAT, 0666);
+		if (fd < 0) {
+			perror("open");
+			return (1);
+		}
+		if (ftruncate(fd, fsize) < 0) {
+			perror("ftruncate");
+			return (1);
+		}
+		if (close(fd)) {
+			perror("close");
+			return (1);
+		}
+	} else {
+		if (truncate(filename, fsize) < 0) {
+			perror("truncate");
+			return (1);
+		}
+	}
+	return (0);
+}
+
+static void
+parse_options(int argc, char *argv[])
+{
+	int c;
+	extern char *optarg;
+	extern int optind, optopt;
+
+	while ((c = getopt(argc, argv, "s:f")) != -1) {
+		switch (c) {
+			case 's':
+				fsize = atoi(optarg);
+				break;
+			case 'f':
+				ftruncflag++;
+				break;
+			case ':':
+				(void) fprintf(stderr,
+				    "Option -%c requires an operand\n", optopt);
+				errflag++;
+				break;
+		}
+		if (errflag) {
+			(void) usage(argv[0]);
+		}
+	}
+
+	if (argc <= optind) {
+		(void) fprintf(stderr, "No filename specified\n");
+		usage(argv[0]);
+	}
+	filename = argv[optind];
+}
