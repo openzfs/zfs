fd4d275816b911b4f0abc7ad2f9f637bded686e0
Tony Hutter <hutter2@llnl.gov>
2018-02-16 00:11:52 -0800

    4.16 kernel: Use correct *_dec_and_test()
    Use refcount_dec_and_test() on 4.16+ kernels, atomic_dec_and_test()
    on older kernels.  https://lwn.net/Articles/714974/
    
    Signed-off-by: Tony Hutter <hutter2@llnl.gov>
    Closes: #7179

--- include/linux/vfs_compat.h
+++ include/linux/vfs_compat.h
@@ -288,9 +288,13 @@ zpl_posix_acl_release(struct posix_acl *acl)
 {
 	if ((acl == NULL) || (acl == ACL_NOT_CACHED))
 		return;
-
+#ifdef HAVE_ACL_REFCOUNT
+	if (refcount_dec_and_test(&acl->a_refcount))
+		zpl_posix_acl_release_impl(acl);
+#else
 	if (atomic_dec_and_test(&acl->a_refcount))
 		zpl_posix_acl_release_impl(acl);
+#endif
 }
 #endif /* HAVE_POSIX_ACL_RELEASE */
 
