openzfs/zfs

commit 587e228a6fcc03aa1135fda368666841dcb0e4c8

Allow bookmark short name in 'zfs bookmark'
This lets users create a bookmark from the command line by its name
only, without the need to specify the dataset path which is extacted
from the snapshot parameter.

These commands are now equivalent:

   zfs bookmark poolname/fs@snap poolname/fs#bookmark
   zfs bookmark @snap poolname/fs#bookmark
   zfs bookmark poolname/fs@snap \#bookmark

Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
Signed-off-by: loli10K <ezomori.nozomu@gmail.com>
Closes #3665 
Closes #6027

cmd/zfs/zfs_main.c
@@ -6864,6 +6864,7 @@ static int
 zfs_do_bookmark(int argc, char **argv)
 {
 	char snapname[ZFS_MAX_DATASET_NAME_LEN];
+	char bookname[ZFS_MAX_DATASET_NAME_LEN];
 	zfs_handle_t *zhp;
 	nvlist_t *nvl;
 	int ret = 0;
@@ -6894,7 +6895,7 @@ zfs_do_bookmark(int argc, char **argv)
 
 	if (strchr(argv[1], '#') == NULL) {
 		(void) fprintf(stderr,
-		    gettext("invalid bookmark name '%s' -- "
+		    gettext("invalid bookmark name '%s': "
 		    "must contain a '#'\n"), argv[1]);
 		goto usage;
 	}
@@ -6910,6 +6911,18 @@ zfs_do_bookmark(int argc, char **argv)
 	} else {
 		(void) strlcpy(snapname, argv[0], sizeof (snapname));
 	}
+	if (argv[1][0] == '#') {
+		/*
+		 * Bookmark name begins with #.
+		 * Default to same fs as snapshot.
+		 */
+		(void) strlcpy(bookname, argv[0], sizeof (bookname));
+		*strchr(bookname, '@') = '\0';
+		(void) strlcat(bookname, argv[1], sizeof (bookname));
+	} else {
+		(void) strlcpy(bookname, argv[1], sizeof (bookname));
+	}
+
 	zhp = zfs_open(g_zfs, snapname, ZFS_TYPE_SNAPSHOT);
 	if (zhp == NULL)
 		goto usage;
@@ -6917,7 +6930,7 @@ zfs_do_bookmark(int argc, char **argv)
 
 
 	nvl = fnvlist_alloc();
-	fnvlist_add_string(nvl, argv[1], snapname);
+	fnvlist_add_string(nvl, bookname, snapname);
 	ret = lzc_bookmark(nvl, NULL);
 	fnvlist_free(nvl);
 
@@ -6927,7 +6940,7 @@ zfs_do_bookmark(int argc, char **argv)
 
 		(void) snprintf(errbuf, sizeof (errbuf),
 		    dgettext(TEXT_DOMAIN,
-		    "cannot create bookmark '%s'"), argv[1]);
+		    "cannot create bookmark '%s'"), bookname);
 
 		switch (ret) {
 		case EXDEV:
@@ -6944,6 +6957,9 @@ zfs_do_bookmark(int argc, char **argv)
 			break;
 		case ENOSPC:
 			err_msg = "out of space";
+			break;
+		case ENOENT:
+			err_msg = "dataset does not exist";
 			break;
 		default:
 			err_msg = "unknown error";

commit 30422a3be1b960604808be26931bd2aef88e0a81

Fix segfault in zfs_do_bookmark()
When invoked with wrong parameters 'zfs bookmark' fails to gracefully
validate user input and crashes.

This is a regression accidentally introduced in 587e228; this commit
adds additional tests to the ZFS Test Suite to exercise this codepath.

Signed-off-by:  loli10K <ezomori.nozomu@gmail.com>

cmd/zfs/zfs_main.c
@@ -7112,6 +7112,12 @@ zfs_do_bookmark(int argc, char **argv)
 		goto usage;
 	}
 
+	if (strchr(argv[0], '@') == NULL) {
+		(void) fprintf(stderr,
+		    gettext("invalid snapshot name '%s': "
+		    "must contain a '@'\n"), argv[0]);
+		goto usage;
+	}
 	if (strchr(argv[1], '#') == NULL) {
 		(void) fprintf(stderr,
 		    gettext("invalid bookmark name '%s': "

tests/zfs-tests/tests/functional/cli_root/zfs_bookmark/zfs_bookmark_cliargs.ksh
@@ -35,6 +35,8 @@
 # 2. Verify we can create a bookmark specifying snapshot and bookmark full paths
 # 3. Verify we can create a bookmark specifying the snapshot name
 # 4. Verify we can create a bookmark specifying the bookmark name
+# 5. Verify at least a full dataset path is required and both snapshot and
+#    bookmark name must be valid
 #
 
 verify_runnable "both"
@@ -49,7 +51,7 @@ function cleanup
 	fi
 }
 
-log_assert "'zfs bookmark' works as expected when passed valid arguments."
+log_assert "'zfs bookmark' should work only when passed valid arguments."
 log_onexit cleanup
 
 DATASET="$TESTPOOL/$TESTFS"
@@ -74,4 +76,25 @@ log_must zfs bookmark "$DATASET@$TESTSNAP" "#$TESTBM"
 log_must eval "bkmarkexists $DATASET#$TESTBM"
 log_must zfs destroy "$DATASET#$TESTBM"
 
-log_pass "'zfs bookmark' works as expected when passed valid arguments."
+# Verify at least a full dataset path is required and both snapshot and
+# bookmark name must be valid
+log_mustnot zfs bookmark "@$TESTSNAP" "#$TESTBM"
+log_mustnot zfs bookmark "$TESTSNAP" "#$TESTBM"
+log_mustnot zfs bookmark "@$TESTSNAP" "$TESTBM"
+log_mustnot zfs bookmark "$TESTSNAP" "$TESTBM"
+log_mustnot zfs bookmark "$TESTSNAP" "$DATASET#$TESTBM"
+log_mustnot zfs bookmark "$DATASET" "$TESTBM"
+log_mustnot zfs bookmark "$DATASET@" "$TESTBM"
+log_mustnot zfs bookmark "$DATASET" "#$TESTBM"
+log_mustnot zfs bookmark "$DATASET@" "#$TESTBM"
+log_mustnot zfs bookmark "$DATASET@$TESTSNAP" "$TESTBM"
+log_mustnot zfs bookmark "@" "#$TESTBM"
+log_mustnot zfs bookmark "@" "#"
+log_mustnot zfs bookmark "@$TESTSNAP" "#"
+log_mustnot zfs bookmark "@$TESTSNAP" "$DATASET#"
+log_mustnot zfs bookmark "@$TESTSNAP" "$DATASET"
+log_mustnot zfs bookmark "$TESTSNAP" "$DATASET#"
+log_mustnot zfs bookmark "$TESTSNAP" "$DATASET"
+log_mustnot eval "bkmarkexists $DATASET#$TESTBM"
+
+log_pass "'zfs bookmark' works as expected only when passed valid arguments."