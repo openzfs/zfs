001e9b856e5d52551c5c5636595cf6d164601979
Tom Caputi <tcaputi@datto.com>
2018-04-17 14:13:57 -0400

    Fix object reclaim when using large dnodes
    Currently, when the receive_object() code wants to reclaim an
    object, it always assumes that the dnode is the legacy 512 bytes,
    even when the incoming bonus buffer exceeds this length. This
    causes a buffer overflow if --enable-debug is not provided and
    triggers an ASSERT if it is. This patch resolves this issue and
    adds an ASSERT to ensure this can't happen again.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Tom Caputi <tcaputi@datto.com>
    Closes #7097
    Closes #7433

--- module/zfs/dmu_object.c
+++ module/zfs/dmu_object.c
@@ -249,7 +249,7 @@ dmu_object_reclaim(objset_t *os, uint64_t object, dmu_object_type_t ot,
     int blocksize, dmu_object_type_t bonustype, int bonuslen, dmu_tx_t *tx)
 {
 	return (dmu_object_reclaim_dnsize(os, object, ot, blocksize, bonustype,
-	    bonuslen, 0, tx));
+	    bonuslen, DNODE_MIN_SIZE, tx));
 }
 
 int
--- module/zfs/dnode.c
+++ module/zfs/dnode.c
@@ -662,8 +662,7 @@ dnode_reallocate(dnode_t *dn, dmu_object_type_t ot, int blocksize,
 	ASSERT(DMU_OT_IS_VALID(bonustype));
 	ASSERT3U(bonuslen, <=,
 	    DN_BONUS_SIZE(spa_maxdnodesize(dmu_objset_spa(dn->dn_objset))));
-
-	dn_slots = dn_slots > 0 ? dn_slots : DNODE_MIN_SLOTS;
+	ASSERT3U(bonuslen, <=, DN_BONUS_SIZE(dn_slots << DNODE_SHIFT));
 
 	dnode_free_interior_slots(dn);
 	DNODE_STAT_BUMP(dnode_reallocate);
