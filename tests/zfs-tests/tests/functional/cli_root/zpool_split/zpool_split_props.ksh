openzfs/zfs

commit 7fab6361882753d2703dbe4c6f6badc956b40484

Add 'zpool split' coverage to the ZFS Test Suite
This change adds five new tests to the ZTS:

 * zpool_split_cliargs: verify command line options and arguments
 * zpool_split_devices: verify zpool split accepts a device list
 * zpool_split_encryption: verify zpool can split encrypted pools
 * zpool_split_props: verify zpool split can set property values
 * zpool_split_vdevs: verify vdev layout when splitting the pool

Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
Signed-off-by: loli10K <ezomori.nozomu@gmail.com>
Closes #7409

tests/zfs-tests/tests/functional/cli_root/zpool_split/zpool_split_props.ksh
@@ -1,0 +1,83 @@
+#!/bin/ksh -p
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source.  A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+
+#
+# Copyright 2018, loli10K <ezomori.nozomu@gmail.com>. All rights reserved.
+#
+
+. $STF_SUITE/include/libtest.shlib
+
+#
+# DESCRIPTION:
+# 'zpool split' can set new property values on the new pool
+#
+# STRATEGY:
+# 1. Create a mirror pool
+# 2. Verify 'zpool split' can set property values on the new pool, but only if
+#    they are valid.
+#
+
+verify_runnable "both"
+
+function cleanup
+{
+	destroy_pool $TESTPOOL
+	destroy_pool $TESTPOOL2
+	rm -f $DEVICE1 $DEVICE2
+}
+
+function setup_mirror
+{
+	truncate -s $SPA_MINDEVSIZE $DEVICE1
+	truncate -s $SPA_MINDEVSIZE $DEVICE2
+	log_must zpool create -f $TESTPOOL mirror $DEVICE1 $DEVICE2
+}
+
+log_assert "'zpool split' can set new property values on the new pool"
+log_onexit cleanup
+
+typeset good_props=('comment=text' 'ashift=12' 'multihost=on'
+    'listsnapshots=on' 'autoexpand=on' 'autoreplace=on' 'dedupditto=1234'
+    'delegation=off' 'failmode=continue')
+typeset bad_props=("bootfs=$TESTPOOL2/bootfs" 'version=28' 'ashift=4'
+    'allocated=1234' 'capacity=5678' 'dedupditto=42' 'multihost=none'
+    'feature@async_destroy=disabled' 'feature@xxx_fake_xxx=enabled'
+    'propname=propval' 'readonly=on')
+
+DEVICE1="$TEST_BASE_DIR/device-1"
+DEVICE2="$TEST_BASE_DIR/device-2"
+
+# Needed to set multihost=on
+zgenhostid
+
+# Verify we can set a combination of valid property values on the new pool
+for prop in "${good_props[@]}"
+do
+	propname="$(awk -F= '{print $1}' <<< $prop)"
+	propval="$(awk -F= '{print $2}' <<< $prop)"
+	setup_mirror
+	log_must zpool split -o $prop $TESTPOOL $TESTPOOL2
+	log_must zpool import -N -d $TEST_BASE_DIR $TESTPOOL2
+	log_must test "$(get_pool_prop $propname $TESTPOOL2)" == "$propval"
+	cleanup
+done
+
+# Verify we cannot set invalid property values
+setup_mirror
+zfs create $TESTPOOL/bootfs
+for prop in "${bad_props[@]}"
+do
+	log_mustnot zpool split -o $prop $TESTPOOL $TESTPOOL2
+	log_mustnot zpool import -N -d $TEST_BASE_DIR $TESTPOOL2
+done
+
+log_pass "'zpool split' can set new property values on the new pool"

configure.ac
@@ -243,6 +243,7 @@ AC_CONFIG_FILES([
 	tests/zfs-tests/tests/functional/cli_root/zpool_replace/Makefile
 	tests/zfs-tests/tests/functional/cli_root/zpool_scrub/Makefile
 	tests/zfs-tests/tests/functional/cli_root/zpool_set/Makefile
+	tests/zfs-tests/tests/functional/cli_root/zpool_split/Makefile
 	tests/zfs-tests/tests/functional/cli_root/zpool_status/Makefile
 	tests/zfs-tests/tests/functional/cli_root/zpool_sync/Makefile
 	tests/zfs-tests/tests/functional/cli_root/zpool_upgrade/Makefile

lib/libzfs/libzfs_pool.c
@@ -3019,7 +3019,7 @@ zpool_vdev_split(zpool_handle_t *zhp, char *newname, nvlist_t **newroot,
 	nvlist_t **varray = NULL, *zc_props = NULL;
 	uint_t c, children, newchildren, lastlog = 0, vcount, found = 0;
 	libzfs_handle_t *hdl = zhp->zpool_hdl;
-	uint64_t vers;
+	uint64_t vers, readonly = B_FALSE;
 	boolean_t freelist = B_FALSE, memory_err = B_TRUE;
 	int retval = 0;
 
@@ -3044,6 +3044,14 @@ zpool_vdev_split(zpool_handle_t *zhp, char *newname, nvlist_t **newroot,
 		if ((zc_props = zpool_valid_proplist(hdl, zhp->zpool_name,
 		    props, vers, flags, msg)) == NULL)
 			return (-1);
+		(void) nvlist_lookup_uint64(zc_props,
+		    zpool_prop_to_name(ZPOOL_PROP_READONLY), &readonly);
+		if (readonly) {
+			zfs_error_aux(hdl, dgettext(TEXT_DOMAIN,
+			    "property %s can only be set at import time"),
+			    zpool_prop_to_name(ZPOOL_PROP_READONLY));
+			return (-1);
+		}
 	}
 
 	if (nvlist_lookup_nvlist_array(tree, ZPOOL_CONFIG_CHILDREN, &child,

tests/runfiles/linux.run
@@ -407,6 +407,11 @@ tests = ['zpool_set_001_pos', 'zpool_set_002_neg', 'zpool_set_003_neg',
     'zpool_set_ashift', 'zpool_set_features']
 tags = ['functional', 'cli_root', 'zpool_set']
 
+[tests/functional/cli_root/zpool_split]
+tests = ['zpool_split_cliargs', 'zpool_split_devices',
+    'zpool_split_encryption', 'zpool_split_props', 'zpool_split_vdevs']
+tags = ['functional', 'cli_root', 'zpool_split']
+
 [tests/functional/cli_root/zpool_status]
 tests = ['zpool_status_001_pos', 'zpool_status_002_pos','zpool_status_003_pos',
     'zpool_status_-c_disable', 'zpool_status_-c_homedir',

tests/zfs-tests/tests/functional/cli_root/Makefile.am
@@ -52,6 +52,7 @@ SUBDIRS = \
 	zpool_replace \
 	zpool_scrub \
 	zpool_set \
+	zpool_split \
 	zpool_status \
 	zpool_sync \
 	zpool_upgrade

tests/zfs-tests/tests/functional/cli_root/zpool_split/Makefile.am
@@ -1,0 +1,12 @@
+include $(top_srcdir)/config/Rules.am
+
+pkgdatadir = $(datadir)/@PACKAGE@/zfs-tests/tests/functional/cli_root/zpool_split
+
+dist_pkgdata_SCRIPTS = \
+	cleanup.ksh \
+	setup.ksh \
+	zpool_split_cliargs.ksh \
+	zpool_split_devices.ksh \
+	zpool_split_encryption.ksh \
+	zpool_split_props.ksh \
+	zpool_split_vdevs.ksh

tests/zfs-tests/tests/functional/cli_root/zpool_split/cleanup.ksh
@@ -1,0 +1,19 @@
+#!/bin/ksh -p
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source.  A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+
+#
+# Copyright 2018, loli10K <ezomori.nozomu@gmail.com>. All rights reserved.
+#
+
+. $STF_SUITE/include/libtest.shlib
+
+default_cleanup

tests/zfs-tests/tests/functional/cli_root/zpool_split/setup.ksh
@@ -1,0 +1,17 @@
+#!/bin/ksh -p
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source.  A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+
+#
+# Copyright 2018, loli10K <ezomori.nozomu@gmail.com>. All rights reserved.
+#
+
+. $STF_SUITE/include/libtest.shlib

commit 269827c08ed1b63e4a10c03a05b8a29fef156389

ZTS: Re-enable MMP tests
Commit 7fab6361 inadvertently disabled the MMP test cases by creating
and not removing an /etc/hostid file in the new zpool_split_props test
case.  When the file exists the ZTS skips the entire MMP test group
rather than modify what may be a system which is already configured.
Update the test case to remove the file.

Additionally, because the MMP tests were disabled a regression slipped
in as part of commit 9eb7b46ed0.  Fix it.

Reviewed-by: loli10K <ezomori.nozomu@gmail.com>
Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

module/zfs/spa.c
@@ -3180,6 +3180,7 @@ spa_ld_get_props(spa_t *spa)
 		spa_prop_find(spa, ZPOOL_PROP_DELEGATION, &spa->spa_delegation);
 		spa_prop_find(spa, ZPOOL_PROP_FAILUREMODE, &spa->spa_failmode);
 		spa_prop_find(spa, ZPOOL_PROP_AUTOEXPAND, &spa->spa_autoexpand);
+		spa_prop_find(spa, ZPOOL_PROP_MULTIHOST, &spa->spa_multihost);
 		spa_prop_find(spa, ZPOOL_PROP_DEDUPDITTO,
 		    &spa->spa_dedup_ditto);
 

tests/zfs-tests/tests/functional/cli_root/zpool_split/zpool_split_props.ksh
@@ -15,6 +15,7 @@
 #
 
 . $STF_SUITE/include/libtest.shlib
+. $STF_SUITE/tests/functional/mmp/mmp.kshlib
 
 #
 # DESCRIPTION:
@@ -33,6 +34,7 @@ function cleanup
 	destroy_pool $TESTPOOL
 	destroy_pool $TESTPOOL2
 	rm -f $DEVICE1 $DEVICE2
+	log_must mmp_clear_hostid
 }
 
 function setup_mirror
@@ -44,6 +46,10 @@ function setup_mirror
 
 log_assert "'zpool split' can set new property values on the new pool"
 log_onexit cleanup
+
+if [ -e $HOSTID_FILE ]; then
+	log_unsupported "System has existing $HOSTID_FILE file"
+fi
 
 typeset good_props=('comment=text' 'ashift=12' 'multihost=on'
     'listsnapshots=on' 'autoexpand=on' 'autoreplace=on' 'dedupditto=1234'
@@ -57,7 +63,7 @@ DEVICE1="$TEST_BASE_DIR/device-1"
 DEVICE2="$TEST_BASE_DIR/device-2"
 
 # Needed to set multihost=on
-zgenhostid
+log_must mmp_set_hostid $HOSTID1
 
 # Verify we can set a combination of valid property values on the new pool
 for prop in "${good_props[@]}"
@@ -68,7 +74,10 @@ do
 	log_must zpool split -o $prop $TESTPOOL $TESTPOOL2
 	log_must zpool import -N -d $TEST_BASE_DIR $TESTPOOL2
 	log_must test "$(get_pool_prop $propname $TESTPOOL2)" == "$propval"
-	cleanup
+
+	destroy_pool $TESTPOOL
+	destroy_pool $TESTPOOL2
+	rm -f $DEVICE1 $DEVICE2
 done
 
 # Verify we cannot set invalid property values