8e07f9948373e23ea72d38ba5c2c4d2bed4a0e36
Paul Dagnelie <pcd@delphix.com>
2018-05-24 13:38:47 -0700

    OpenZFS 9559 - zfs diff handles files on delete queue in fromsnap poorly
    Authored by: Paul Dagnelie <pcd@delphix.com>
    Reviewed by: Joshua M. Clulow <josh@sysmgr.org>
    Approved by: Richard Lowe <richlowe@richlowe.net>
    Ported-by: Brian Behlendorf <behlendorf1@llnl.gov>
    
    OpenZFS-issue: https://www.illumos.org/issues/9559
    OpenZFS-commit: https://github.com/openzfs/openzfs/commit/d7e45412

--- lib/libzfs/libzfs_diff.c
+++ lib/libzfs/libzfs_diff.c
@@ -22,7 +22,7 @@
 /*
  * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright 2015 Nexenta Systems, Inc. All rights reserved.
- * Copyright (c) 2015, 2017 by Delphix. All rights reserved.
+ * Copyright (c) 2015, 2018 by Delphix. All rights reserved.
  * Copyright 2016 Joyent, Inc.
  * Copyright 2016 Igor Kozhukhov <ikozhukhov@gmail.com>
  */
@@ -367,12 +367,12 @@ describe_free(FILE *fp, differ_info_t *di, uint64_t object, char *namebuf,
 
 	if (get_stats_for_obj(di, di->fromsnap, object, namebuf,
 	    maxlen, &sb) != 0) {
-		/* Let it slide, if in the delete queue on from side */
-		if (di->zerr == ENOENT && sb.zs_links == 0) {
-			di->zerr = 0;
-			return (0);
-		}
 		return (-1);
+	}
+	/* Don't print if in the delete queue on from side */
+	if (di->zerr == ESTALE) {
+		di->zerr = 0;
+		return (0);
 	}
 
 	print_file(fp, di, ZDIFF_REMOVED, namebuf, &sb);
