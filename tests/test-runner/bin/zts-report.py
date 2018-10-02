838bd5ff35db04695ba4eadc7fdb2d56ff816ee5
Brian Behlendorf <behlendorf1@llnl.gov>
2018-10-01 17:15:57 -0700

    ZTS: Fix snapshot_009_pos, snapshot_010_pos
    Mitigate the likelihood of the newly created volumes being busy
    when the 'zfs destroy -r' is issued by waiting for udev to settle.
    Since this is not a iron clad fix I've added the test case to
    the known list of possible failures and referenced issue #7961.
    
    Finally, in the case this test does fail fix the cleanup logic
    so subsequent tests won't incorrectly fail.
    
    Reviewed-by: Giuseppe Di Natale <guss80@gmail.com>
    Reviewed-by: George Melikov <mail@gmelikov.ru>
    Reviewed-by: John Kennedy <john.kennedy@delphix.com>
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Closes #7961 
    Closes #7962

--- tests/test-runner/bin/zts-report.py
+++ tests/test-runner/bin/zts-report.py
@@ -264,6 +264,8 @@ maybe = {
     'rsend/rsend_024_pos': ['FAIL', '5665'],
     'rsend/send-c_volume': ['FAIL', '6087'],
     'snapshot/clone_001_pos': ['FAIL', known_reason],
+    'snapshot/snapshot_009_pos': ['FAIL', '7961'],
+    'snapshot/snapshot_010_pos': ['FAIL', '7961'],
     'snapused/snapused_004_pos': ['FAIL', '5513'],
     'tmpfile/setup': ['SKIP', tmpfile_reason],
     'threadsappend/threadsappend_001_pos': ['FAIL', '6136'],
