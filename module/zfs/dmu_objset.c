bb1be77a35d3cf5389a36f1d13935811962278c3
loli10K <loli10K@users.noreply.github.com>
2019-02-20 03:41:18 +0100

    Prevent user accounting on readonly pool
    Trying to mount a dataset from a readonly pool could inadvertently start
    the user accounting upgrade task, leading to the following failure:
    
    VERIFY3(tx->tx_threads == 2) failed (0 == 2)
    PANIC at txg.c:680:txg_wait_synced()
    Showing stack for process 2541
    CPU: 2 PID: 2541 Comm: z_upgrade Tainted: P           O  3.16.0-4-amd64 #1 Debian 3.16.51-3
    Hardware name: Bochs Bochs, BIOS Bochs 01/01/2011
    Call Trace:
     [<0>] ? dump_stack+0x5d/0x78
     [<0>] ? spl_panic+0xc9/0x110 [spl]
     [<0>] ? dnode_next_offset+0x1d4/0x2c0 [zfs]
     [<0>] ? dmu_object_next+0x77/0x130 [zfs]
     [<0>] ? dnode_rele_and_unlock+0x4d/0x120 [zfs]
     [<0>] ? txg_wait_synced+0x91/0x220 [zfs]
     [<0>] ? dmu_objset_id_quota_upgrade_cb+0x10f/0x140 [zfs]
     [<0>] ? dmu_objset_upgrade_task_cb+0xe3/0x170 [zfs]
     [<0>] ? taskq_thread+0x2cc/0x5d0 [spl]
     [<0>] ? wake_up_state+0x10/0x10
     [<0>] ? taskq_thread_should_stop.part.3+0x70/0x70 [spl]
     [<0>] ? kthread+0xbd/0xe0
     [<0>] ? kthread_create_on_node+0x180/0x180
     [<0>] ? ret_from_fork+0x58/0x90
     [<0>] ? kthread_create_on_node+0x180/0x180
    
    This patch updates both functions responsible for checking if we can
    perform user accounting to verify the pool is not readonly.
    
    Reviewed-by: Alek Pinchuk <apinchuk@datto.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: loli10K <ezomori.nozomu@gmail.com>
    Closes #8424

--- module/zfs/dmu_objset.c
+++ module/zfs/dmu_objset.c
@@ -2433,7 +2433,8 @@ dmu_objset_userobjspace_upgradable(objset_t *os)
 	return (dmu_objset_type(os) == DMU_OST_ZFS &&
 	    !dmu_objset_is_snapshot(os) &&
 	    dmu_objset_userobjused_enabled(os) &&
-	    !dmu_objset_userobjspace_present(os));
+	    !dmu_objset_userobjspace_present(os) &&
+	    spa_writeable(dmu_objset_spa(os)));
 }
 
 boolean_t
@@ -2442,7 +2443,8 @@ dmu_objset_projectquota_upgradable(objset_t *os)
 	return (dmu_objset_type(os) == DMU_OST_ZFS &&
 	    !dmu_objset_is_snapshot(os) &&
 	    dmu_objset_projectquota_enabled(os) &&
-	    !dmu_objset_projectquota_present(os));
+	    !dmu_objset_projectquota_present(os) &&
+	    spa_writeable(dmu_objset_spa(os)));
 }
 
 void
