55372e5e92f92a1faf2b754290749ae3af02a39e
Tom Caputi <tcaputi@datto.com>
2019-03-25 17:26:40 -0400

    Dont iterate through filesystems unnecessarily
    Currently, when attempting to list snapshots ZFS may do a lot of
    extra work checking child datasets. This is because the code does
    not realize that it will not be able to reach any snapshots
    conatined within snapshots that are at the depth limit since the
    snapshots of those datasets are counted as an additional layer
    deeper. This patch corrects this issue.
    
    In addition, this patch adds the ability to do perform the commands:
    
    $ zfs list -t snapshot <dataset>
    $ zfs get -t snapshot <prop> <dataset>
    
    as a convenient way to list out properties of all snapshots of a
    given dataset without having to use the depth limit.
    
    Signed-off-by: Tom Caputi <tcaputi@datto.com>

--- cmd/zfs/zfs_iter.c
+++ cmd/zfs/zfs_iter.c
@@ -134,16 +134,31 @@ zfs_callback(zfs_handle_t *zhp, void *data)
 	    ((cb->cb_flags & ZFS_ITER_DEPTH_LIMIT) == 0 ||
 	    cb->cb_depth < cb->cb_depth_limit)) {
 		cb->cb_depth++;
-		if (zfs_get_type(zhp) == ZFS_TYPE_FILESYSTEM)
+
+		/*
+		 * If we are not looking for filesystems, we don't need to
+		 * recurse into filesystems when we are at our depth limit.
+		 */
+		if ((cb->cb_depth < cb->cb_depth_limit ||
+		    (cb->cb_flags & ZFS_ITER_DEPTH_LIMIT) == 0 ||
+		    (cb->cb_types &
+		    (ZFS_TYPE_FILESYSTEM | ZFS_TYPE_VOLUME))) &&
+		    zfs_get_type(zhp) == ZFS_TYPE_FILESYSTEM) {
 			(void) zfs_iter_filesystems(zhp, zfs_callback, data);
+		}
+
 		if (((zfs_get_type(zhp) & (ZFS_TYPE_SNAPSHOT |
-		    ZFS_TYPE_BOOKMARK)) == 0) && include_snaps)
+		    ZFS_TYPE_BOOKMARK)) == 0) && include_snaps) {
 			(void) zfs_iter_snapshots(zhp,
-			    (cb->cb_flags & ZFS_ITER_SIMPLE) != 0, zfs_callback,
-			    data, 0, 0);
+			    (cb->cb_flags & ZFS_ITER_SIMPLE) != 0,
+			    zfs_callback, data, 0, 0);
+		}
+
 		if (((zfs_get_type(zhp) & (ZFS_TYPE_SNAPSHOT |
-		    ZFS_TYPE_BOOKMARK)) == 0) && include_bmarks)
+		    ZFS_TYPE_BOOKMARK)) == 0) && include_bmarks) {
 			(void) zfs_iter_bookmarks(zhp, zfs_callback, data);
+		}
+
 		cb->cb_depth--;
 	}
 
