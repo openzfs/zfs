4ca407bdcf16ce5fa40242a7d88f4d9d1007160b
Dipack P Panjabi <dpanjabi@hudson-trading.com>
2019-06-26 09:50:42 -0400

    Segregate libzfs src and added src to maybe make testing breaks in API easier
    Signed-off-by: Dipack P Panjabi <dpanjabi@hudson-trading.com>

--- contrib/pyzfs/libzfs/bindings/libzfs.py
+++ contrib/pyzfs/libzfs/bindings/libzfs.py
@@ -22,13 +22,11 @@ SOURCE = """
 
 LIBRARY = "zfs"
 
-CDEF = """
+_added_src = """
 ///////////////////////////////////////////////////////
 // ADDED
 ///////////////////////////////////////////////////////
-#define MAXPATHLEN 500
-#define    ZFS_MAXPROPLEN        64
-#define    ZPOOL_MAXPROPLEN    64
+#define    MAXPATHLEN       1024
 #define    ZFS_MAX_DATASET_NAME_LEN 256
 #define    ZFSSHARE_MISS    0x01    /* Didn't find entry in cache */
 
@@ -78,102 +76,6 @@ struct avl_tree {
     size_t avl_size;        /* sizeof user type struct */
 };
 
-
-/*
- * libzfs errors
- */
-typedef enum zfs_error {
-    EZFS_SUCCESS = 0,    /* no error -- success */
-    EZFS_NOMEM = 2000,    /* out of memory */
-    EZFS_BADPROP,        /* invalid property value */
-    EZFS_PROPREADONLY,    /* cannot set readonly property */
-    EZFS_PROPTYPE,        /* property does not apply to dataset type */
-    EZFS_PROPNONINHERIT,    /* property is not inheritable */
-    EZFS_PROPSPACE,        /* bad quota or reservation */
-    EZFS_BADTYPE,        /* dataset is not of appropriate type */
-    EZFS_BUSY,        /* pool or dataset is busy */
-    EZFS_EXISTS,        /* pool or dataset already exists */
-    EZFS_NOENT,        /* no such pool or dataset */
-    EZFS_BADSTREAM,        /* bad backup stream */
-    EZFS_DSREADONLY,    /* dataset is readonly */
-    EZFS_VOLTOOBIG,        /* volume is too large for 32-bit system */
-    EZFS_INVALIDNAME,    /* invalid dataset name */
-    EZFS_BADRESTORE,    /* unable to restore to destination */
-    EZFS_BADBACKUP,        /* backup failed */
-    EZFS_BADTARGET,        /* bad attach/detach/replace target */
-    EZFS_NODEVICE,        /* no such device in pool */
-    EZFS_BADDEV,        /* invalid device to add */
-    EZFS_NOREPLICAS,    /* no valid replicas */
-    EZFS_RESILVERING,    /* currently resilvering */
-    EZFS_BADVERSION,    /* unsupported version */
-    EZFS_POOLUNAVAIL,    /* pool is currently unavailable */
-    EZFS_DEVOVERFLOW,    /* too many devices in one vdev */
-    EZFS_BADPATH,        /* must be an absolute path */
-    EZFS_CROSSTARGET,    /* rename or clone across pool or dataset */
-    EZFS_ZONED,        /* used improperly in local zone */
-    EZFS_MOUNTFAILED,    /* failed to mount dataset */
-    EZFS_UMOUNTFAILED,    /* failed to unmount dataset */
-    EZFS_UNSHARENFSFAILED,    /* unshare(1M) failed */
-    EZFS_SHARENFSFAILED,    /* share(1M) failed */
-    EZFS_PERM,        /* permission denied */
-    EZFS_NOSPC,        /* out of space */
-    EZFS_FAULT,        /* bad address */
-    EZFS_IO,        /* I/O error */
-    EZFS_INTR,        /* signal received */
-    EZFS_ISSPARE,        /* device is a hot spare */
-    EZFS_INVALCONFIG,    /* invalid vdev configuration */
-    EZFS_RECURSIVE,        /* recursive dependency */
-    EZFS_NOHISTORY,        /* no history object */
-    EZFS_POOLPROPS,        /* couldn't retrieve pool props */
-    EZFS_POOL_NOTSUP,    /* ops not supported for this type of pool */
-    EZFS_POOL_INVALARG,    /* invalid argument for this pool operation */
-    EZFS_NAMETOOLONG,    /* dataset name is too long */
-    EZFS_OPENFAILED,    /* open of device failed */
-    EZFS_NOCAP,        /* couldn't get capacity */
-    EZFS_LABELFAILED,    /* write of label failed */
-    EZFS_BADWHO,        /* invalid permission who */
-    EZFS_BADPERM,        /* invalid permission */
-    EZFS_BADPERMSET,    /* invalid permission set name */
-    EZFS_NODELEGATION,    /* delegated administration is disabled */
-    EZFS_UNSHARESMBFAILED,    /* failed to unshare over smb */
-    EZFS_SHARESMBFAILED,    /* failed to share over smb */
-    EZFS_BADCACHE,        /* bad cache file */
-    EZFS_ISL2CACHE,        /* device is for the level 2 ARC */
-    EZFS_VDEVNOTSUP,    /* unsupported vdev type */
-    EZFS_NOTSUP,        /* ops not supported on this dataset */
-    EZFS_ACTIVE_SPARE,    /* pool has active shared spare devices */
-    EZFS_UNPLAYED_LOGS,    /* log device has unplayed logs */
-    EZFS_REFTAG_RELE,    /* snapshot release: tag not found */
-    EZFS_REFTAG_HOLD,    /* snapshot hold: tag already exists */
-    EZFS_TAGTOOLONG,    /* snapshot hold/rele: tag too long */
-    EZFS_PIPEFAILED,    /* pipe create failed */
-    EZFS_THREADCREATEFAILED, /* thread create failed */
-    EZFS_POSTSPLIT_ONLINE,    /* onlining a disk after splitting it */
-    EZFS_SCRUBBING,        /* currently scrubbing */
-    EZFS_NO_SCRUB,        /* no active scrub */
-    EZFS_DIFF,        /* general failure of zfs diff */
-    EZFS_DIFFDATA,        /* bad zfs diff data */
-    EZFS_POOLREADONLY,    /* pool is in read-only mode */
-    EZFS_SCRUB_PAUSED,    /* scrub currently paused */
-    EZFS_ACTIVE_POOL,    /* pool is imported on a different system */
-    EZFS_CRYPTOFAILED,    /* failed to setup encryption */
-    EZFS_NO_PENDING,    /* cannot cancel, no operation is pending */
-    EZFS_CHECKPOINT_EXISTS,    /* checkpoint exists */
-    EZFS_DISCARDING_CHECKPOINT,    /* currently discarding a checkpoint */
-    EZFS_NO_CHECKPOINT,    /* pool has no checkpoint */
-    EZFS_DEVRM_IN_PROGRESS,    /* a device is currently being removed */
-    EZFS_VDEV_TOO_BIG,    /* a device is too big to be used */
-    EZFS_IOC_NOTSUPPORTED,    /* operation not supported by zfs module */
-    EZFS_TOOMANY,        /* argument list too long */
-    EZFS_INITIALIZING,    /* currently initializing */
-    EZFS_NO_INITIALIZE,    /* no active initialize */
-    EZFS_WRONG_PARENT,    /* invalid parent dataset (e.g ZVOL) */
-    EZFS_TRIMMING,        /* currently trimming */
-    EZFS_NO_TRIM,        /* no active trim */
-    EZFS_TRIM_NOTSUP,    /* device does not support trim */
-    EZFS_NO_RESILVER_DEFER,    /* pool doesn't support resilver_defer */
-    EZFS_UNKNOWN
-} zfs_error_t;
 
 /*
  * vdev states are ordered from least to most healthy.
@@ -426,42 +328,6 @@ typedef struct pool_removal_stat {
     uint64_t prs_mapping_memory;
 } pool_removal_stat_t;
 
-/*
- * The following data structures are all part
- * of the zfs_allow_t data structure which is
- * used for printing 'allow' permissions.
- * It is a linked list of zfs_allow_t's which
- * then contain avl tree's for user/group/sets/...
- * and each one of the entries in those trees have
- * avl tree's for the permissions they belong to and
- * whether they are local,descendent or local+descendent
- * permissions.  The AVL trees are used primarily for
- * sorting purposes, but also so that we can quickly find
- * a given user and or permission.
- */
-typedef struct zfs_perm_node {
-    avl_node_t z_node;
-    char z_pname[MAXPATHLEN];
-} zfs_perm_node_t;
-
-typedef struct zfs_allow_node {
-    avl_node_t z_node;
-    char z_key[MAXPATHLEN];        /* name, such as joe */
-    avl_tree_t z_localdescend;    /* local+descendent perms */
-    avl_tree_t z_local;        /* local permissions */
-    avl_tree_t z_descend;        /* descendent permissions */
-} zfs_allow_node_t;
-
-typedef struct zfs_allow {
-    struct zfs_allow *z_next;
-    char z_setpoint[MAXPATHLEN];
-    avl_tree_t z_sets;
-    avl_tree_t z_crperms;
-    avl_tree_t z_user;
-    avl_tree_t z_group;
-    avl_tree_t z_everyone;
-} zfs_allow_t;
-
 struct libzfs_handle {
     int libzfs_error;
     int libzfs_fd;
@@ -523,6 +389,146 @@ struct zpool_handle {
 ///////////////////////////////////////////////////////
 // END ADDED
 ///////////////////////////////////////////////////////
+
+"""
+
+_libzfs_src = """
+// These values are MAXPATHLEN, but they have been changed
+// to their actual value as CFFI does not like inferring stuff
+#define    ZFS_MAXPROPLEN   1024
+#define    ZPOOL_MAXPROPLEN 1024
+
+/*
+ * libzfs errors
+ */
+typedef enum zfs_error {
+    EZFS_SUCCESS = 0,    /* no error -- success */
+    EZFS_NOMEM = 2000,    /* out of memory */
+    EZFS_BADPROP,        /* invalid property value */
+    EZFS_PROPREADONLY,    /* cannot set readonly property */
+    EZFS_PROPTYPE,        /* property does not apply to dataset type */
+    EZFS_PROPNONINHERIT,    /* property is not inheritable */
+    EZFS_PROPSPACE,        /* bad quota or reservation */
+    EZFS_BADTYPE,        /* dataset is not of appropriate type */
+    EZFS_BUSY,        /* pool or dataset is busy */
+    EZFS_EXISTS,        /* pool or dataset already exists */
+    EZFS_NOENT,        /* no such pool or dataset */
+    EZFS_BADSTREAM,        /* bad backup stream */
+    EZFS_DSREADONLY,    /* dataset is readonly */
+    EZFS_VOLTOOBIG,        /* volume is too large for 32-bit system */
+    EZFS_INVALIDNAME,    /* invalid dataset name */
+    EZFS_BADRESTORE,    /* unable to restore to destination */
+    EZFS_BADBACKUP,        /* backup failed */
+    EZFS_BADTARGET,        /* bad attach/detach/replace target */
+    EZFS_NODEVICE,        /* no such device in pool */
+    EZFS_BADDEV,        /* invalid device to add */
+    EZFS_NOREPLICAS,    /* no valid replicas */
+    EZFS_RESILVERING,    /* currently resilvering */
+    EZFS_BADVERSION,    /* unsupported version */
+    EZFS_POOLUNAVAIL,    /* pool is currently unavailable */
+    EZFS_DEVOVERFLOW,    /* too many devices in one vdev */
+    EZFS_BADPATH,        /* must be an absolute path */
+    EZFS_CROSSTARGET,    /* rename or clone across pool or dataset */
+    EZFS_ZONED,        /* used improperly in local zone */
+    EZFS_MOUNTFAILED,    /* failed to mount dataset */
+    EZFS_UMOUNTFAILED,    /* failed to unmount dataset */
+    EZFS_UNSHARENFSFAILED,    /* unshare(1M) failed */
+    EZFS_SHARENFSFAILED,    /* share(1M) failed */
+    EZFS_PERM,        /* permission denied */
+    EZFS_NOSPC,        /* out of space */
+    EZFS_FAULT,        /* bad address */
+    EZFS_IO,        /* I/O error */
+    EZFS_INTR,        /* signal received */
+    EZFS_ISSPARE,        /* device is a hot spare */
+    EZFS_INVALCONFIG,    /* invalid vdev configuration */
+    EZFS_RECURSIVE,        /* recursive dependency */
+    EZFS_NOHISTORY,        /* no history object */
+    EZFS_POOLPROPS,        /* couldn't retrieve pool props */
+    EZFS_POOL_NOTSUP,    /* ops not supported for this type of pool */
+    EZFS_POOL_INVALARG,    /* invalid argument for this pool operation */
+    EZFS_NAMETOOLONG,    /* dataset name is too long */
+    EZFS_OPENFAILED,    /* open of device failed */
+    EZFS_NOCAP,        /* couldn't get capacity */
+    EZFS_LABELFAILED,    /* write of label failed */
+    EZFS_BADWHO,        /* invalid permission who */
+    EZFS_BADPERM,        /* invalid permission */
+    EZFS_BADPERMSET,    /* invalid permission set name */
+    EZFS_NODELEGATION,    /* delegated administration is disabled */
+    EZFS_UNSHARESMBFAILED,    /* failed to unshare over smb */
+    EZFS_SHARESMBFAILED,    /* failed to share over smb */
+    EZFS_BADCACHE,        /* bad cache file */
+    EZFS_ISL2CACHE,        /* device is for the level 2 ARC */
+    EZFS_VDEVNOTSUP,    /* unsupported vdev type */
+    EZFS_NOTSUP,        /* ops not supported on this dataset */
+    EZFS_ACTIVE_SPARE,    /* pool has active shared spare devices */
+    EZFS_UNPLAYED_LOGS,    /* log device has unplayed logs */
+    EZFS_REFTAG_RELE,    /* snapshot release: tag not found */
+    EZFS_REFTAG_HOLD,    /* snapshot hold: tag already exists */
+    EZFS_TAGTOOLONG,    /* snapshot hold/rele: tag too long */
+    EZFS_PIPEFAILED,    /* pipe create failed */
+    EZFS_THREADCREATEFAILED, /* thread create failed */
+    EZFS_POSTSPLIT_ONLINE,    /* onlining a disk after splitting it */
+    EZFS_SCRUBBING,        /* currently scrubbing */
+    EZFS_NO_SCRUB,        /* no active scrub */
+    EZFS_DIFF,        /* general failure of zfs diff */
+    EZFS_DIFFDATA,        /* bad zfs diff data */
+    EZFS_POOLREADONLY,    /* pool is in read-only mode */
+    EZFS_SCRUB_PAUSED,    /* scrub currently paused */
+    EZFS_ACTIVE_POOL,    /* pool is imported on a different system */
+    EZFS_CRYPTOFAILED,    /* failed to setup encryption */
+    EZFS_NO_PENDING,    /* cannot cancel, no operation is pending */
+    EZFS_CHECKPOINT_EXISTS,    /* checkpoint exists */
+    EZFS_DISCARDING_CHECKPOINT,    /* currently discarding a checkpoint */
+    EZFS_NO_CHECKPOINT,    /* pool has no checkpoint */
+    EZFS_DEVRM_IN_PROGRESS,    /* a device is currently being removed */
+    EZFS_VDEV_TOO_BIG,    /* a device is too big to be used */
+    EZFS_IOC_NOTSUPPORTED,    /* operation not supported by zfs module */
+    EZFS_TOOMANY,        /* argument list too long */
+    EZFS_INITIALIZING,    /* currently initializing */
+    EZFS_NO_INITIALIZE,    /* no active initialize */
+    EZFS_WRONG_PARENT,    /* invalid parent dataset (e.g ZVOL) */
+    EZFS_TRIMMING,        /* currently trimming */
+    EZFS_NO_TRIM,        /* no active trim */
+    EZFS_TRIM_NOTSUP,    /* device does not support trim */
+    EZFS_NO_RESILVER_DEFER,    /* pool doesn't support resilver_defer */
+    EZFS_UNKNOWN
+} zfs_error_t;
+
+/*
+ * The following data structures are all part
+ * of the zfs_allow_t data structure which is
+ * used for printing 'allow' permissions.
+ * It is a linked list of zfs_allow_t's which
+ * then contain avl tree's for user/group/sets/...
+ * and each one of the entries in those trees have
+ * avl tree's for the permissions they belong to and
+ * whether they are local,descendent or local+descendent
+ * permissions.  The AVL trees are used primarily for
+ * sorting purposes, but also so that we can quickly find
+ * a given user and or permission.
+ */
+typedef struct zfs_perm_node {
+    avl_node_t z_node;
+    char z_pname[MAXPATHLEN];
+} zfs_perm_node_t;
+
+typedef struct zfs_allow_node {
+    avl_node_t z_node;
+    char z_key[MAXPATHLEN];        /* name, such as joe */
+    avl_tree_t z_localdescend;    /* local+descendent perms */
+    avl_tree_t z_local;        /* local permissions */
+    avl_tree_t z_descend;        /* descendent permissions */
+} zfs_allow_node_t;
+
+typedef struct zfs_allow {
+    struct zfs_allow *z_next;
+    char z_setpoint[MAXPATHLEN];
+    avl_tree_t z_sets;
+    avl_tree_t z_crperms;
+    avl_tree_t z_user;
+    avl_tree_t z_group;
+    avl_tree_t z_everyone;
+} zfs_allow_t;
 
 
 /*
@@ -1488,3 +1494,5 @@ extern int zfs_remap_indirects(libzfs_handle_t *hdl, const char *);
 
 
 """
+
+CDEF = _added_src + _libzfs_src
