d8750b0adfaf55af4646a6fe52f645f8517fad26
Paul Dagnelie <pcd@delphix.com>
2019-06-28 14:02:41 -0700

    Don't activate metaslabs with weight 0
    Signed-off-by: Paul Dagnelie <pcd@delphix.com>

--- module/zfs/metaslab.c
+++ module/zfs/metaslab.c
@@ -2485,6 +2485,18 @@ metaslab_activate(metaslab_t *msp, int allocator, uint64_t activation_weight)
 		return (0);
 	}
 
+	/*
+	 * If the metaslab has literally 0 space, it will have weight 0. In
+	 * that case, don't bother activating it. This can happen if the
+	 * metaslab had space during find_valid_metaslab, but another thread
+	 * loaded it and used all that space while we were waiting to grab the
+	 * lock.
+	 */
+	if (msp->ms_weight == 0) {
+		ASSERT0(range_tree_space(msp->ms_allocatable));
+		return (SET_ERROR(ENOSPC));
+	}
+
 	if ((error = metaslab_activate_allocator(msp->ms_group, msp,
 	    allocator, activation_weight)) != 0) {
 		return (error);
