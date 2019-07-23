71cab9c68c5e65f3d946c40e8fc452382487439f
Ryan Moeller <ryan@ixsystems.com>
2019-07-23 12:02:37 -0400

    Skip activity check when MMP magic not present
    Signed-off-by: Ryan Moeller <ryan@ixsystems.com>

--- module/zfs/spa.c
+++ module/zfs/spa.c
@@ -2953,9 +2953,10 @@ spa_activity_check_required(spa_t *spa, uberblock_t *ub, nvlist_t *label,
 		return (B_FALSE);
 
 	/*
-	 * Skip the activity check when the MMP feature is disabled.
+	 * Skip the activity check when the MMP feature is not present or is
+	 * disabled.
 	 */
-	if (ub->ub_mmp_magic == MMP_MAGIC && ub->ub_mmp_delay == 0)
+	if (ub->ub_mmp_magic != MMP_MAGIC || ub->ub_mmp_delay == 0)
 		return (B_FALSE);
 
 	/*
@@ -3496,8 +3497,7 @@ spa_ld_select_uberblock(spa_t *spa, spa_import_type_t type)
 	activity_check = spa_activity_check_required(spa, ub, label,
 	    spa->spa_config);
 	if (activity_check) {
-		if (ub->ub_mmp_magic == MMP_MAGIC && ub->ub_mmp_delay &&
-		    spa_get_hostid(spa) == 0) {
+		if (spa_get_hostid(spa) == 0) {
 			nvlist_free(label);
 			fnvlist_add_uint64(spa->spa_load_info,
 			    ZPOOL_CONFIG_MMP_STATE, MMP_STATE_NO_HOSTID);
