4475761570d1775a34c299e6488b6f4c63a3c10d
George Melikov <mail@gmelikov.ru>
2019-09-10 23:34:53 +0300

    zio_compress: introduce compress_threshold
    Now default compression is lz4, which can stop
    compression process by itself on incompressible data.
    If there are additional size checks -
    we will only make our compressratio worse.
    
    New usable compression thresholds are:
    - less than BPE_PAYLOAD_SIZE (embedded_data feature);
    - at least one saved sector.
    
    Old 12.5% threshold is left to minimize affect
    on existing user expectations of CPU utilization.
    
    If data wasn't compressed - it will be saved as
    ZIO_COMPRESS_OFF, so if we really need to recompress
    data without ashift info and check anything -
    we can just compress it with zero threshold.
    So, we don't need a new feature flag here!
    
    Signed-off-by: George Melikov <mail@gmelikov.ru>

--- module/zfs/zio_compress.c
+++ module/zfs/zio_compress.c
@@ -22,15 +22,11 @@
 /*
  * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
- */
-/*
  * Copyright (c) 2013 by Saso Kiselkov. All rights reserved.
- */
-
-/*
  * Copyright (c) 2013, 2018 by Delphix. All rights reserved.
  * Copyright (c) 2019, Klara Inc.
  * Copyright (c) 2019, Allan Jude
+ * Copyright (c) 2021, 2024 by George Melikov. All rights reserved.
  */
 
 #include <sys/zfs_context.h>
@@ -124,9 +120,12 @@ zio_compress_zeroed_cb(void *data, size_t len, void *private)
 	return (0);
 }
 
+/*
+ * Set compress_threshold=0 for reproducibility of ARC checks.
+ */
 size_t
 zio_compress_data(enum zio_compress c, abd_t *src, void **dst, size_t s_len,
-    uint8_t level)
+    uint8_t level, int compress_threshold)
 {
 	size_t c_len, d_len;
 	uint8_t complevel;
@@ -145,8 +144,19 @@ zio_compress_data(enum zio_compress c, abd_t *src, void **dst, size_t s_len,
 	if (c == ZIO_COMPRESS_EMPTY)
 		return (s_len);
 
-	/* Compress at least 12.5% */
-	d_len = s_len - (s_len >> 3);
+	/*
+	 * Usable compression thresholds are:
+	 * - less than BPE_PAYLOAD_SIZE (embedded_data feature)
+	 * - at least one saved sector
+	 * - minimun 12.5% should be saved (legacy value)
+	 */
+	if (compress_threshold > 0) {
+		d_len = MAX(BPE_PAYLOAD_SIZE,
+		    s_len - MAX(s_len % compress_threshold, s_len >> 3));
+	} else {
+		/* Special case for reproducibility of ARC checks */
+		d_len = s_len;
+	}
 
 	complevel = ci->ci_level;
 
@@ -174,7 +184,6 @@ zio_compress_data(enum zio_compress c, abd_t *src, void **dst, size_t s_len,
 	if (c_len > d_len)
 		return (s_len);
 
-	ASSERT3U(c_len, <=, d_len);
 	return (c_len);
 }
 
