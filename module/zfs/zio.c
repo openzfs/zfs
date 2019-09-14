f2f2f68a15fea02ac64e1605bd062af406f0d3c8
Allan Jude <allanjude@freebsd.org>
2019-09-14 20:11:37 +0000

    Disable an unused function to pass the compile tests
    Signed-off-by: Allan Jude <allanjude@freebsd.org>

--- module/zfs/zio.c
+++ module/zfs/zio.c
@@ -556,6 +556,8 @@ error:
 	}
 }
 
+#if 0
+/* We might still need this in the future */
 static void
 zio_getcomplevel(zio_t *zio, abd_t *data, uint64_t size)
 {
@@ -576,6 +578,7 @@ zio_getcomplevel(zio_t *zio, abd_t *data, uint64_t size)
 			zio->io_error = SET_ERROR(EIO);
 	}
 }
+#endif
 
 /*
  * ==========================================================================
