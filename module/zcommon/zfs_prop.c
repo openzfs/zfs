3ef695cc97361257bb8183da18193bc55b807ed6
Brian Behlendorf <behlendorf1@llnl.gov>
2019-09-24 13:40:22 -0700

    Increase allowed 'special_small_blocks' maximum value
    There may be circumstances where it's desirable that all blocks
    in a specified dataset be stored on the special device.  Relax
    the artificial 128K limit and allow the special_small_blocks
    property to be set up to 1M.  When blocks >1MB have been enabled
    via the zfs_max_recordsize module option, this limit is increased
    accordingly.
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Issue #9131

--- module/zcommon/zfs_prop.c
+++ module/zcommon/zfs_prop.c
@@ -544,7 +544,7 @@ zfs_prop_init(void)
 	    ZFS_TYPE_FILESYSTEM, "512 to 1M, power of 2", "RECSIZE");
 	zprop_register_number(ZFS_PROP_SPECIAL_SMALL_BLOCKS,
 	    "special_small_blocks", 0, PROP_INHERIT, ZFS_TYPE_FILESYSTEM,
-	    "zero or 512 to 128K, power of 2", "SPECIAL_SMALL_BLOCKS");
+	    "zero or 512 to 1M, power of 2", "SPECIAL_SMALL_BLOCKS");
 
 	/* hidden properties */
 	zprop_register_hidden(ZFS_PROP_NUMCLONES, "numclones", PROP_TYPE_NUMBER,
