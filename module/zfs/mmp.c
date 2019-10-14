dbb93b567c84fda4061f4fac10b5157d029b3c4d
Matthew Macy <mmacy@freebsd.org>
2019-10-14 14:50:11 -0700

    Move linux specific mmp module_param_call handler to platform code
    Signed-off-by: Matt Macy <mmacy@FreeBSD.org>

--- module/zfs/mmp.c
+++ module/zfs/mmp.c
@@ -716,36 +716,8 @@ mmp_signal_all_threads(void)
 	mutex_exit(&spa_namespace_lock);
 }
 
-#if defined(_KERNEL)
-
-static int
-param_set_multihost_interval(const char *val, zfs_kernel_param_t *kp)
-{
-	int ret;
-
-	ret = param_set_ulong(val, kp);
-	if (ret < 0)
-		return (ret);
-
-	if (spa_mode_global != 0)
-		mmp_signal_all_threads();
-
-	return (ret);
-}
-
-#endif
-
-/* BEGIN CSTYLED */
-#if defined(_KERNEL)
-module_param_call(zfs_multihost_interval, param_set_multihost_interval,
-    param_get_ulong, &zfs_multihost_interval, 0644);
-MODULE_PARM_DESC(zfs_multihost_interval,
-	"Milliseconds between mmp writes to each leaf");
-#endif
-
 ZFS_MODULE_PARAM(zfs_multihost, zfs_multihost_, fail_intervals, UINT, ZMOD_RW,
 	"Max allowed period without a successful mmp write");
 
 ZFS_MODULE_PARAM(zfs_multihost, zfs_multihost_, import_intervals, UINT, ZMOD_RW,
 	"Number of zfs_multihost_interval periods to wait for activity");
-/* END CSTYLED */
