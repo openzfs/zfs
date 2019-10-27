2c365a4eea200b6923f63912ccdfda51ab998c1d
Romain Dolbeau <romain.dolbeau@atos.net>
2019-10-27 10:13:42 +0100

    Add AVX512BW variant of fletcher
    It is much faster than AVX512F when byteswapping on Skylake-SP
    and newer, as we can do the byteswap in a single vshufb insteads
    of many instructions.
    
    Signed-off-by: Romain Dolbeau <romain.dolbeau@atos.net>

--- module/zcommon/zfs_fletcher.c
+++ module/zcommon/zfs_fletcher.c
@@ -184,6 +184,9 @@ static const fletcher_4_ops_t *fletcher_4_impls[] = {
 #if defined(__x86_64) && defined(HAVE_AVX512F)
 	&fletcher_4_avx512f_ops,
 #endif
+#if defined(__x86_64) && defined(HAVE_AVX512BW)
+	&fletcher_4_avx512bw_ops,
+#endif
 #if defined(__aarch64__)
 	&fletcher_4_aarch64_neon_ops,
 #endif
