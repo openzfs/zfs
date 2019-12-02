479b09d12d6a448bb32355acea4e86c44a642b13
Paul Dagnelie <pcd@delphix.com>
2019-12-02 13:57:16 -0800

    Fix cstyle in zfs_ids_to_path.c

--- cmd/zfs_ids_to_path/zfs_ids_to_path.c
+++ cmd/zfs_ids_to_path/zfs_ids_to_path.c
@@ -35,42 +35,44 @@ libzfs_handle_t *g_zfs;
 void
 usage(int err)
 {
-        fprintf(stderr, "Usage: ./os_id_to_name <pool> <objset id> <object id>\n");
-        exit(err);
+	fprintf(stderr, "Usage: ./os_id_to_name <pool> <objset id> "
+	    "<object id>\n");
+	exit(err);
 }
 
 int
 main(int argc, char **argv)
 {
-        if (argc != 4) {
-                (void) fprintf(stderr, "Incorrect number of arguments: %d\n", argc);
-                usage(1);
-        }
+	if (argc != 4) {
+		(void) fprintf(stderr, "Incorrect number of arguments: %d\n",
+		    argc);
+		usage(1);
+	}
 
-        uint64_t objset, object;
-        if (sscanf(argv[2], "%lu", &objset) != 1) {
-                (void) fprintf(stderr, "Invalid objset id: %s\n", argv[2]);
-                usage(2);
-        }
-        if (sscanf(argv[3], "%lu", &object) != 1) {
-                (void) fprintf(stderr, "Invalid object id: %s\n", argv[3]);
-                usage(3);
-        }
-        if ((g_zfs = libzfs_init()) == NULL) {
-                (void) fprintf(stderr, "%s\n", libzfs_error_init(errno));
-                return (4);
-        }
-        zpool_handle_t *pool = zpool_open(g_zfs, argv[1]);
-        if (pool == NULL) {
-                fprintf(stderr, "Could not open pool %s\n", argv[1]);
-                libzfs_fini(g_zfs);
-                return (5);
-        }
+	uint64_t objset, object;
+	if (sscanf(argv[2], "%lu", &objset) != 1) {
+		(void) fprintf(stderr, "Invalid objset id: %s\n", argv[2]);
+		usage(2);
+	}
+	if (sscanf(argv[3], "%lu", &object) != 1) {
+		(void) fprintf(stderr, "Invalid object id: %s\n", argv[3]);
+		usage(3);
+	}
+	if ((g_zfs = libzfs_init()) == NULL) {
+		(void) fprintf(stderr, "%s\n", libzfs_error_init(errno));
+		return (4);
+	}
+	zpool_handle_t *pool = zpool_open(g_zfs, argv[1]);
+	if (pool == NULL) {
+		fprintf(stderr, "Could not open pool %s\n", argv[1]);
+		libzfs_fini(g_zfs);
+		return (5);
+	}
 
-        char pathname[PATH_MAX * 2];
-        zpool_obj_to_path(pool, objset, object, pathname, PATH_MAX * 2);
-        printf("%s\n", pathname);
-        zpool_close(pool);
-        libzfs_fini(g_zfs);
+	char pathname[PATH_MAX * 2];
+	zpool_obj_to_path(pool, objset, object, pathname, PATH_MAX * 2);
+	printf("%s\n", pathname);
+	zpool_close(pool);
+	libzfs_fini(g_zfs);
 	return (0);
 }
