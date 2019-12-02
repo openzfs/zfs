e311598e30a4cb13599d2a947e27df5e02a8bc7a
Matthew Macy <mmacy@FreeBSD.org>
2019-12-02 13:49:20 -0800

    move linux qsort def to platform header, fix fallout
    Signed-off-by: Matt Macy <mmacy@FreeBSD.org>

--- include/os/linux/spl/sys/sysmacros.h
+++ include/os/linux/spl/sys/sysmacros.h
@@ -192,7 +192,11 @@ extern void spl_cleanup(void);
 #define	SET_ERROR(err) \
 	(__set_error(__FILE__, __func__, __LINE__, err), err)
 
-#if defined(_KERNEL) && !defined(_KMEMUSER) && !defined(offsetof)
+#include <linux/sort.h>
+#define	qsort(base, num, size, cmp)		\
+	sort(base, num, size, cmp, NULL)
+
+#if !defined(_KMEMUSER) && !defined(offsetof)
 
 /* avoid any possibility of clashing with <stddef.h> version */
 
