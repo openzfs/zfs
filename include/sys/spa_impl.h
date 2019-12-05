e64e84eca57966cff1314d61f5b9557876e856e3
Matthew Macy <mmacy@freebsd.org>
2019-12-05 12:40:45 -0800

    Refactor deadman set failmode to be cross platform
    Update zfs_deadman_failmode to use the ZFS_MODULE_PARAM_CALL
    wrapper, and split the common and platform specific portions.
    
    Reviewed-by: Jorgen Lundman <lundman@lundman.net>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Matt Macy <mmacy@FreeBSD.org>
    Closes #9670

--- include/sys/spa_impl.h
+++ include/sys/spa_impl.h
@@ -433,6 +433,7 @@ struct spa {
 };
 
 extern char *spa_config_path;
+extern char *zfs_deadman_failmode;
 extern int spa_slop_shift;
 extern void spa_taskq_dispatch_ent(spa_t *spa, zio_type_t t, zio_taskq_type_t q,
     task_func_t *func, void *arg, uint_t flags, taskq_ent_t *ent);
@@ -443,7 +444,7 @@ extern void spa_load_l2cache(spa_t *spa);
 extern sysevent_t *spa_event_create(spa_t *spa, vdev_t *vd, nvlist_t *hist_nvl,
     const char *name);
 extern void spa_event_post(sysevent_t *ev);
-
+extern int param_set_deadman_failmode_common(const char *val);
 
 #ifdef	__cplusplus
 }
