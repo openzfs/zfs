ff119e8604f86446de26e25f3c1db6b0a06be504
Jorgen Lundman <lundman@lundman.net>
2020-05-20 20:08:58 +0900

    Upstream: Add snapshot events
    For kernel to send snapshot mount/unmount events to zed.
    
    Signed-off-by: Jorgen Lundman <lundman@lundman.net>

--- module/zfs/zfs_fm.c
+++ module/zfs/zfs_fm.c
@@ -1445,7 +1445,29 @@ zfs_ereport_fini(void)
 	list_destroy(&recent_events_list);
 	mutex_destroy(&recent_events_lock);
 }
+#endif
 
+void
+zfs_ereport_snapshot_post(const char *subclass, spa_t *spa, const char *name)
+{
+#ifdef _KERNEL
+	nvlist_t *ereport = NULL;
+	nvlist_t *detector = NULL;
+
+	zfs_ereport_start(&ereport, &detector,
+	    subclass,
+	    spa, NULL, NULL, NULL, 0, 0);
+
+	if (ereport == NULL) return;
+
+	VERIFY0(nvlist_add_string(ereport, "snapshot_name", name));
+
+	/* Cleanup is handled by the callback function */
+	zfs_zevent_post(ereport, detector, zfs_zevent_post_cb);
+#endif
+}
+
+#if defined(_KERNEL)
 EXPORT_SYMBOL(zfs_ereport_post);
 EXPORT_SYMBOL(zfs_ereport_is_valid);
 EXPORT_SYMBOL(zfs_ereport_post_checksum);
