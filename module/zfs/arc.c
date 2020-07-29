843e9ca2e1ef347cf6b0271ed84438afc145dfd4
Serapheim Dimitropoulos <serapheim@delphix.com>
2020-07-29 09:43:33 -0700

    Introduce names for ZTHRs
    When debugging issues or generally analyzing the runtime of
    a system it would be nice to be able to tell the different
    ZTHRs running by name rather than having to analyze their
    stack.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Matthew Ahrens <mahrens@delphix.com>
    Co-authored-by: Ryan Moeller <ryan@iXsystems.com>
    Signed-off-by: Serapheim Dimitropoulos <serapheim@delphix.com>
    Closes #10630

--- module/zfs/arc.c
+++ module/zfs/arc.c
@@ -7339,10 +7339,10 @@ arc_init(void)
 		kstat_install(arc_ksp);
 	}
 
-	arc_evict_zthr = zthr_create_timer(arc_evict_cb_check,
-	    arc_evict_cb, NULL, SEC2NSEC(1));
-	arc_reap_zthr = zthr_create_timer(arc_reap_cb_check,
-	    arc_reap_cb, NULL, SEC2NSEC(1));
+	arc_evict_zthr = zthr_create_timer("arc_evict",
+	    arc_evict_cb_check, arc_evict_cb, NULL, SEC2NSEC(1));
+	arc_reap_zthr = zthr_create_timer("arc_reap",
+	    arc_reap_cb_check, arc_reap_cb, NULL, SEC2NSEC(1));
 
 	arc_warm = B_FALSE;
 
