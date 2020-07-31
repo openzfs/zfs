2b0ce55331057d708424918a041744a7d7d80a2b
Allan Jude <allanjude@freebsd.org>
2020-07-31 11:14:30 -0700

    Activate per-dataset feature flags when the property is set
    If a compression algorithm requires a feature flag, activate the
    feature when the property is set, rather than waiting for the first
    block to be born.  This is currently only used by zstd but can be
    extended as needed.
    
    Sponsored-by: The FreeBSD Foundation
    Co-authored-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Co-authored-by: Allan Jude <allanjude@freebsd.org>
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Allan Jude <allanjude@freebsd.org>
    Signed-off-by: Michael Niewöhner <foss@mniewoehner.de>

--- module/zfs/zfs_ioctl.c
+++ module/zfs/zfs_ioctl.c
@@ -2510,6 +2510,15 @@ zfs_prop_set_special(const char *dsname, zprop_source_t source,
 	case ZFS_PROP_REFRESERVATION:
 		err = dsl_dataset_set_refreservation(dsname, source, intval);
 		break;
+	case ZFS_PROP_COMPRESSION:
+		err = dsl_dataset_set_compression(dsname, source, intval);
+		/*
+		 * Set err to -1 to force the zfs_set_prop_nvlist code down the
+		 * default path to set the value in the nvlist.
+		 */
+		if (err == 0)
+			err = -1;
+		break;
 	case ZFS_PROP_VOLSIZE:
 		err = zvol_set_volsize(dsname, intval);
 		break;
