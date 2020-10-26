f5bffd3748ffc92fe1bdae2f9ee4e29de33d3cec
Mateusz Guzik <mjguzik@gmail.com>
2020-10-26 22:32:17 +0100

    FreeBSD: catch up with 1300123 version bump
    - removed thread argument from VOP_INACTIVE
    - removed cred argument from VOP_VPTOCNP
    
    Reviewed-by: Ryan Moeller <ryan@iXsystems.com>
    Reviewed-by: Matt Macy <mmacy@FreeBSD.org>
    Signed-off-by: Mateusz Guzik <mjguzik@gmail.com>
    Closes #11104

--- module/os/freebsd/zfs/zfs_vnops.c
+++ module/os/freebsd/zfs/zfs_vnops.c
@@ -5821,7 +5821,11 @@ zfs_freebsd_inactive(struct vop_inactive_args *ap)
 {
 	vnode_t *vp = ap->a_vp;
 
+#if __FreeBSD_version >= 1300123
+	zfs_inactive(vp, curthread->td_ucred, NULL);
+#else
 	zfs_inactive(vp, ap->a_td->td_ucred, NULL);
+#endif
 	return (0);
 }
 
@@ -6507,8 +6511,13 @@ zfs_vptocnp(struct vop_vptocnp_args *ap)
 	error = vget(covered_vp, LK_SHARED | LK_VNHELD, curthread);
 #endif
 	if (error == 0) {
+#if __FreeBSD_version >= 1300123
+		error = VOP_VPTOCNP(covered_vp, ap->a_vpp, ap->a_buf,
+		    ap->a_buflen);
+#else
 		error = VOP_VPTOCNP(covered_vp, ap->a_vpp, ap->a_cred,
 		    ap->a_buf, ap->a_buflen);
+#endif
 		vput(covered_vp);
 	}
 	vn_lock(vp, ltype | LK_RETRY);
