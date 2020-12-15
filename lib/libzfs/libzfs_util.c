97f38b0dded1a49a8b4cb33cc8662b773d7dc793
Jorgen Lundman <lundman@lundman.net>
2020-12-15 12:29:12 +0900

    Upstream: Windows will do own run_process
    Not using vfork() and execve()

--- lib/libzfs/libzfs_util.c
+++ lib/libzfs/libzfs_util.c
@@ -872,6 +872,7 @@ libzfs_read_stdout_from_fd(int fd, char **lines[])
 	return (lines_cnt);
 }
 
+#ifndef _WIN32
 static int
 libzfs_run_process_impl(const char *path, char *argv[], char *env[], int flags,
     char **lines[], int *lines_cnt)
@@ -940,6 +941,7 @@ libzfs_run_process_impl(const char *path, char *argv[], char *env[], int flags,
 
 	return (-1);
 }
+#endif
 
 int
 libzfs_run_process(const char *path, char *argv[], int flags)
