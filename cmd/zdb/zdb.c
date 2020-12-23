b6722b871b141b29adb5ae46455b7d96620fa993
Matthew Ahrens <mahrens@delphix.com>
2020-12-23 09:52:24 -0800

    nvlist leaked in zpool_find_config()
    In `zpool_find_config()`, the `pools` nvlist is leaked.  Part of it (a
    sub-nvlist) is returned in `*configp`, but the callers also leak that.
    
    Additionally, in `zdb.c:main()`, the `searchdirs` is leaked.
    
    The leaks were detected by ASAN (`configure --enable-asan`).
    
    This commit resolves the leaks.
    
    Reviewed-by: Igor Kozhukhov <igor@dilos.org>
    Reviewed-by: Ryan Moeller <ryan@iXsystems.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Matthew Ahrens <mahrens@delphix.com>
    Closes #11396

--- cmd/zdb/zdb.c
+++ cmd/zdb/zdb.c
@@ -8435,6 +8435,11 @@ main(int argc, char **argv)
 		}
 	}
 
+	if (searchdirs != NULL) {
+		umem_free(searchdirs, nsearch * sizeof (char *));
+		searchdirs = NULL;
+	}
+
 	/*
 	 * import_checkpointed_state makes the assumption that the
 	 * target pool that we pass it is already part of the spa
@@ -8451,6 +8456,11 @@ main(int argc, char **argv)
 
 		if (checkpoint_target != NULL)
 			target = checkpoint_target;
+	}
+
+	if (cfg != NULL) {
+		nvlist_free(cfg);
+		cfg = NULL;
 	}
 
 	if (target_pool != target)
