dad8b4d7656b2b354181104f860e985d181f698f
Fedor Uporov <fuporovvStack@gmail.com>
2021-01-18 03:44:36 -0800

    Add ZFS_ERR_RAIDZ_EXPANSION error in case of checkpoint creation on pool with expanded raidz
    The spa_checkpoint() request checks raidz expasnion zfeature increment.
    So, checkpoint creation is blocked in case if raidz expansion had place.

--- include/libzfs.h
+++ include/libzfs.h
@@ -149,6 +149,7 @@ typedef enum zfs_error {
 	EZFS_NO_RESILVER_DEFER,	/* pool doesn't support resilver_defer */
 	EZFS_EXPORT_IN_PROGRESS,	/* currently exporting the pool */
 	EZFS_REBUILDING,	/* resilvering (sequential reconstrution) */
+	EZFS_RAIDZ_EXPAND_IN_PROGRESS,	/* a raidz is currently expanding */
 	EZFS_UNKNOWN
 } zfs_error_t;
 
