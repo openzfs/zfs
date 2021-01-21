713c6e578a25719a36cf3cbb5f686e1699ea87ac
Ryan Moeller <ryan@iXsystems.com>
2021-01-21 23:20:14 +0000

    FreeBSD: upstream changes to VFS interface
    Set VIRF_MOUNTPOINT flag on snapshot mountpoint.
    
    Authored-by: Mateusz Guzik <mjg@FreeBSD.org>
    Signed-off-by: Ryan Moeller <ryan@iXsystems.com>

--- module/os/freebsd/spl/spl_vfs.c
+++ module/os/freebsd/spl/spl_vfs.c
@@ -240,9 +240,11 @@ mount_snapshot(kthread_t *td, vnode_t **vpp, const char *fstype, char *fspath,
 #endif
 	VI_LOCK(vp);
 	vp->v_iflag &= ~VI_MOUNT;
-	VI_UNLOCK(vp);
-
+#ifdef VIRF_MOUNTPOINT
+	vn_irflag_set_locked(vp, VIRF_MOUNTPOINT);
+#endif
 	vp->v_mountedhere = mp;
+	VI_UNLOCK(vp);
 	/* Put the new filesystem on the mount list. */
 	mtx_lock(&mountlist_mtx);
 	TAILQ_INSERT_TAIL(&mountlist, mp, mnt_list);
