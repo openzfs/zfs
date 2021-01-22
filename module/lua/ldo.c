c55285d420ba8ecd943cfea544e89ace270421a3
Brian Behlendorf <behlendorf1@llnl.gov>
2021-01-22 12:54:34 -0800

    cppcheck: integrete cppcheck
    In order for cppcheck to perform a proper analysis it needs to be
    aware of how the sources are compiled (source files, include
    paths/files, extra defines, etc).  All the needed information is
    available from the Makefiles and can be leveraged with a generic
    cppcheck Makefile target.  So let's add one.
    
    Additional minor changes:
    
    * Removing the cppcheck-suppressions.txt file.  With cppcheck 2.3
      and these changes it appears to no longer be needed.  Some inline
      suppressions were also removed since they appear not to be
      needed.  We can add them back if it turns out they're needed
      for older versions of cppcheck.
    
    * Added the ax_count_cpus m4 macro to detect at configure time how
      many processors are available in order to run multiple cppcheck
      jobs.  This value is also now used as a replacement for nproc
      when executing the kernel interface checks.
    
    * "PHONY =" line moved in to the Rules.am file which is included
      at the top of all Makefile.am's.  This is just convenient becase
      it allows us to use the += syntax to add phony targets.
    
    * One upside of this integration worth mentioning is it now allows
      `make cppcheck` to be run in any directory to check that subtree.
    
    * For the moment, cppcheck is not run against the FreeBSD specific
      kernel sources.  The cppcheck-FreeBSD target will need to be
      implemented and testing on FreeBSD to support this.
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- module/lua/ldo.c
+++ module/lua/ldo.c
@@ -196,7 +196,6 @@ int luaD_rawrunprotected (lua_State *L, Pfunc f, void *ud) {
   struct lua_longjmp lj;
   lj.status = LUA_OK;
   lj.previous = L->errorJmp;  /* chain new error handler */
-  // cppcheck-suppress autoVariables
   L->errorJmp = &lj;
   LUAI_TRY(L, &lj,
     (*f)(L, ud);
