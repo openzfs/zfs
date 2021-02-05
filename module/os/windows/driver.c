d05e786f721738b4b72c32de0a02f758fd38aa3d
Jorgen Lundman <lundman@lundman.net>
2021-02-05 14:51:45 +0900

    Windows: Cleanup CMake work, to build sys
    Make sure driver.c is compiled with msvc++
    
    Set -m64 if 64bit clang compile, it defaults to 32bit.

--- module/os/windows/driver.c
+++ module/os/windows/driver.c
@@ -25,10 +25,8 @@ extern int    zfs_vfsops_fini(void);
 int zfs_kmod_init(void);
 void zfs_kmod_fini(void);
 
-/* Why is this required on one of my build VMs */
-#ifndef _MSC_VER
-void *__guard_eh_cont_table = 0;
-size_t __guard_eh_cont_count = 0;
+#ifdef __clang__
+#error "This file should be compiled with MSVC not Clang"
 #endif
 
 PDRIVER_OBJECT WIN_DriverObject = NULL;
@@ -364,4 +362,65 @@ int spl_kstat_registry(void *arg, kstat_t *ksp)
 	ZwClose(h);
 	return (changed);
 }
+
+/*
+* Visual Studio
+1>    C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29333\bin\HostX86\x64\CL.exe
+/c
+/IC:\src\openzfs\contrib\windows\OpenZFS\..\..\..\include\os\windows\spl
+/IC:\src\openzfs\contrib\windows\OpenZFS\..\..\..\include\os\windows\zfs
+/IC:\src\openzfs\contrib\windows\OpenZFS\..\..\..\include\os\windows
+/Ix64\Debug\
+/Zi
+/nologo
+/W3
+/WX-
+/diagnostics:column
+/Od
+/Oi
+/Oy-
+/D _KERNEL
+/D _KERNEL_
+/D _WIN64
+/D _AMD64_
+/D AMD64
+/D _WIN64
+/D _AMD64_
+/D AMD64
+/D DEPRECATE_DDK_FUNCTIONS=1
+/D MSC_NOOPT
+/D _WIN32_WINNT=0x0601
+/D WINVER=0x0601
+/D WINNT=1
+/D NTDDI_VERSION=0x06010000
+/D DBG=1
+/D KMDF_VERSION_MAJOR=1
+/D KMDF_VERSION_MINOR=9
+/GF
+/Gm-
+/Zp8
+/GS
+/guard:cf
+/Gy
+/fp:precise
+/Qspectre
+/Zc:wchar_t-
+/Zc:forScope
+/Zc:inline
+/GR-
+/Fo"x64\Debug\\"
+/Fd"x64\Debug\vc142.pdb"
+/Gz
+/wd4748 /wd4603 /wd4627 /wd4986 /wd4987
+/FI"C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\shared\warning.h"
+/FC /errorReport:prompt
+/kernel
+-cbstring -d2epilogunwind
+/d1import_no_registry
+/d2AllowCompatibleILVersions
+/d2Zi+ ..\..\..\..\module\os\windows\driver.c
+*
+* 1>    C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29333\bin\HostX86\x64\link.exe /ERRORREPORT:PROMPT /OUT:"C:\src\openzfs\contrib\windows\OpenZFS\x64\Debug\OpenZFS.sys" /VERSION:"10.0" /INCREMENTAL:NO /NOLOGO /LIBPATH:"C:\src\openzfs\contrib\windows\OpenZFS\..\..\..\out\build\x64-Debug\module\os\windows\spl" /LIBPATH:"C:\src\openzfs\contrib\windows\OpenZFS\..\..\..\out\build\x64-Debug\module\os\windows\zfs" /LIBPATH:"C:\src\openzfs\contrib\windows\OpenZFS\..\..\..\out\build\x64-Debug\module\icp" /LIBPATH:"C:\src\openzfs\contrib\windows\OpenZFS\..\..\..\out\build\x64-Debug\module\lua" /LIBPATH:"C:\src\openzfs\contrib\windows\OpenZFS\..\..\..\out\build\x64-Debug\module\nvpair" /LIBPATH:"C:\src\openzfs\contrib\windows\OpenZFS\..\..\..\out\build\x64-Debug\module\unicode" /LIBPATH:"C:\src\openzfs\contrib\windows\OpenZFS\..\..\..\out\build\x64-Debug\module\zcommon" /LIBPATH:"C:\src\openzfs\contrib\windows\OpenZFS\..\..\..\out\build\x64-Debug\module\zfs" /LIBPATH:"C:\src\openzfs\contrib\windows\OpenZFS\..\..\..\out\build\x64-Debug\module\zstd" /LIBPATH:"C:\src\openzfs\contrib\windows\OpenZFS\..\..\..\out\build\x64-Debug\lib\os\windows\zlib-1.2.3" /WX /SECTION:"INIT,d" wdmsec.lib Storport.lib scsiwmi.lib splkern.lib icpkern.lib luakern.lib nvpairkern.lib unicodekern.lib zcommonkern.lib zlibkern.lib zstdkern.lib zfskern.lib zfskern_os.lib "C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\km\x64\BufferOverflowK.lib" "C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\km\x64\ntoskrnl.lib" "C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\km\x64\hal.lib" "C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\km\x64\wmilib.lib" "C:\Program Files (x86)\Windows Kits\10\lib\wdf\kmdf\x64\1.9\WdfLdr.lib" "C:\Program Files (x86)\Windows Kits\10\lib\wdf\kmdf\x64\1.9\WdfDriverEntry.lib" "C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\km\x64\BufferOverflowK.lib" "C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\km\x64\ntoskrnl.lib" "C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\km\x64\hal.lib" "C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\km\x64\wmilib.lib" "C:\Program Files (x86)\Windows Kits\10\lib\wdf\kmdf\x64\1.9\WdfLdr.lib" "C:\Program Files (x86)\Windows Kits\10\lib\wdf\kmdf\x64\1.9\WdfDriverEntry.lib" /NODEFAULTLIB /MANIFEST:NO /DEBUG:FASTLINK /PDB:"C:\src\openzfs\contrib\windows\OpenZFS\x64\Debug\OpenZFS.pdb" /SUBSYSTEM:NATIVE,"6.01" /Driver /OPT:REF /OPT:ICF /ENTRY:"FxDriverEntry" /RELEASE /IMPLIB:"C:\src\openzfs\contrib\windows\OpenZFS\x64\Debug\OpenZFS.lib" /MERGE:"_TEXT=.text;_PAGE=PAGE" /MACHINE:X64 /PROFILE /guard:cf /kernel /IGNORE:4198,4010,4037,4039,4065,4070,4078,4087,4089,4221,4108,4088,4218,4218,4235 /osversion:10.0 /pdbcompress /debugtype:pdata x64\Debug\debug.obj
+
+* */
 
