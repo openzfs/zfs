fc70d55f9baf0a2c389e62ec8c2ef6cbff520124
Charlie Root <cedric.maunoury@gmail.com>
2021-02-16 21:25:54 +0100

    checkstyle : 80 character limit patch

--- lib/libzfs/libzfs_sendrecv.c
+++ lib/libzfs/libzfs_sendrecv.c
@@ -322,8 +322,11 @@ send_iterate_snap(zfs_handle_t *zhp, void *arg)
 
 	if (!sd->recursive) {
 
-		// To allow a doall stream to work correctly with a NULL fromsnap
-		if (sd->doall && sd->fromsnap == NULL && sd->seenfrom == B_FALSE) {
+		/*
+		 * To allow a doall stream to work correctly
+		 * with a NULL fromsnap
+		 */
+		if (sd->doall && sd->fromsnap == NULL && !sd->seenfrom) {
 			sd->seenfrom = B_TRUE;
 		}
 
