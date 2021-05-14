49fb50cbc80c2ff074dfca269634cc512ae8437c
Paul Zuchowski <31706010+PaulZ-98@users.noreply.github.com>
2021-05-14 00:46:14 -0400

    Fix dmu_recv_stream test for resumable
    Use dsl_dataset_has_resume_receive_state()
    not dsl_dataset_is_zapified() to check if
    stream is resumable.
    
    Reviewed-by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed-by: Alek Pinchuk <apinchuk@axcient.com>
    Reviewed-by: Ryan Moeller <ryan@ixsystems.com>
    Signed-off-by: Paul Zuchowski <pzuchowski@datto.com>
    Closes #12034

--- module/zfs/dmu_recv.c
+++ module/zfs/dmu_recv.c
@@ -2878,8 +2878,8 @@ dmu_recv_stream(dmu_recv_cookie_t *drc, offset_t *voffp)
 	int err = 0;
 	struct receive_writer_arg *rwa = kmem_zalloc(sizeof (*rwa), KM_SLEEP);
 
-	if (dsl_dataset_is_zapified(drc->drc_ds)) {
-		uint64_t bytes;
+	if (dsl_dataset_has_resume_receive_state(drc->drc_ds)) {
+		uint64_t bytes = 0;
 		(void) zap_lookup(drc->drc_ds->ds_dir->dd_pool->dp_meta_objset,
 		    drc->drc_ds->ds_object, DS_FIELD_RESUME_BYTES,
 		    sizeof (bytes), 1, &bytes);
