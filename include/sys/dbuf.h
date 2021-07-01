15177c1aac9f3c1b23523d021e854da71e15e529
Alexander Motin <mav@FreeBSD.org>
2021-07-01 11:30:31 -0400

    Compact dbuf/buf hashes and lock arrays
    With default dbuf cache size of 1/32 of ARC, it makes no sense to have
    hash table of the same size (or even bigger on Linux).  Reduce it to
    1/8 of ARC's one, still leaving some slack, assuming higher I/O rate
    via dbuf cache than via ARC.
    
    Remove padding from ARC hash locks array.  The idea behind padding
    is to avoid false sharing between locks.  It would have sense if
    there would be a limited number of very busy locks.  But since we
    have no limit on the number, using the same memory for more locks we
    can achieve even lower lock contention with the same false sharing,
    or we can use less memory for the same contention level.
    
    Reduce number of hash locks from 8192 to 2048.  The number is still
    big enough to not cause contention, but reduced memory size improves
    cache hit rate for mutex_tryenter() in ARC eviction thread, saving
    about 1% of the thread time.
    
    Reviewed-by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Alexander Motin <mav@FreeBSD.org>
    Closes #12289

--- include/sys/dbuf.h
+++ include/sys/dbuf.h
@@ -322,12 +322,12 @@ typedef struct dmu_buf_impl {
 } dmu_buf_impl_t;
 
 /* Note: the dbuf hash table is exposed only for the mdb module */
-#define	DBUF_MUTEXES 8192
+#define	DBUF_MUTEXES 2048
 #define	DBUF_HASH_MUTEX(h, idx) (&(h)->hash_mutexes[(idx) & (DBUF_MUTEXES-1)])
 typedef struct dbuf_hash_table {
 	uint64_t hash_table_mask;
 	dmu_buf_impl_t **hash_table;
-	kmutex_t hash_mutexes[DBUF_MUTEXES];
+	kmutex_t hash_mutexes[DBUF_MUTEXES] ____cacheline_aligned;
 } dbuf_hash_table_t;
 
 typedef void (*dbuf_prefetch_fn)(void *, boolean_t);
