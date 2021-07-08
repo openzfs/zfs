d2119d0e6f411be5418f88fdb9b64e5c7b808fce
Jorgen Lundman <lundman@lundman.net>
2021-07-08 12:08:13 +0900

    Replace strchrnul() with strrchr()
    Could have gone either way with this one, either adding it to
    macOS/Windows SPL, or returning it to "classic" usage with strrchr().
    Since the new special way isn't really used, and only used once,
    we have this commit.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Ryan Moeller <ryan@iXsystems.com>
    Signed-off-by: Jorgen Lundman <lundman@lundman.net>
    Closes  #12312

--- lib/libzfs/libzfs_pool.c
+++ lib/libzfs/libzfs_pool.c
@@ -4871,7 +4871,9 @@ zpool_load_compat(const char *compat, boolean_t *features, char *report,
 		    line != NULL;
 		    line = strtok_r(NULL, "\n", &ls)) {
 			/* discard comments */
-			*(strchrnul(line, '#')) = '\0';
+			char *r = strchr(line, '#');
+			if (r != NULL)
+				*r = '\0';
 
 			for (word = strtok_r(line, ", \t", &ws);
 			    word != NULL;
