9fde75deff02d1db027a13bb09b58242103204e3
Jan Kasiak <jan@cybojanek.net>
2021-09-05 21:02:03 -0400

    Add icp algorithm implementation selector
    Signed-off-by: Jan Kasiak <jan@cybojanek.net>

---
+++ module/icp/algs/impl/impl.c
@@ -0,0 +1,350 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+/*
+ * Copyright 2021 Jan Kasiak. All rights reserved.
+ */
+
+/*
+ * Based on aes_impl.c
+ */
+
+#include <sys/simd.h>
+#include <impl/impl.h>
+
+static const struct {
+	char *name;
+	uint32_t sel;
+} alg_impl_opts[] = {
+	{ "cycle", ALG_IMPL_CYCLE },
+	{ "fastest", ALG_IMPL_FASTEST },
+};
+
+#define	BENCHMARK_NS MSEC2NSEC(2)
+
+#if defined(_KERNEL)
+
+/*
+ * Print benchmark stat headers.
+ *
+ * @param[out] buf
+ * @param      size of buffer
+ */
+static int
+impl_kstat_headers(char *buf, size_t size)
+{
+	ssize_t off = 0;
+
+	off += snprintf(buf + off, size, "%-17s", "implementation");
+	off += snprintf(buf + off, size - off, "%-15s\n", "bytes/second");
+
+	return (0);
+}
+
+static int
+impl_kstat_data(char *buf, size_t size, void *data)
+{
+	const alg_impl_ops_bandwidth_t *bw = data;
+
+	ssize_t off = 0;
+
+	off += snprintf(buf + off, size - off, "%-17s", bw->ops->name);
+	off += snprintf(buf + off, size - off, "%-15llu\n",
+	    (u_longlong_t)bw->bandwidth);
+
+	return (0);
+}
+
+/*
+ * Get the next kstat line data.
+ *
+ * @param[in,out] ksp
+ * @param         n
+ *
+ * @retval NULL end of data
+ * @return alg_impl_ops_bandwidth_t* from conf->supported
+ */
+static void *
+impl_kstat_addr(kstat_t *ksp, loff_t n)
+{
+	alg_impl_conf_t *conf = ksp->ks_private;
+
+	if (n == 0) {
+		return (&conf->bandwidth_fastest);
+	}
+
+	if (n <= conf->supported_n) {
+		return (void *)(conf->bandwidth + (n - 1));
+	}
+
+	return (NULL);
+}
+#endif
+
+void
+alg_impl_init(alg_impl_conf_t *conf)
+{
+	const alg_impl_ops_t *curr_impl, *fast_impl = conf->generic;
+	size_t i, c;
+	uint32_t max_priority = 0;
+
+	/* Copy from available to supported */
+	for (i = 0, c = 0; i < conf->available_n; i++) {
+		curr_impl = conf->available[i];
+
+		if (curr_impl->is_supported()) {
+			conf->supported[c] = curr_impl;
+			/* Ops for kstat, even if there is no benchmark. */
+			conf->bandwidth[c].ops = curr_impl;
+			c++;
+
+			/* Keep track of fastest */
+			if (curr_impl->priority > max_priority) {
+				max_priority = curr_impl->priority;
+				fast_impl = curr_impl;
+			}
+		}
+	}
+	conf->supported_n = c;
+
+	/* Run benchmark. */
+	if (conf->benchmark && kfpu_allowed()) {
+		/* Reset fastest assumption. */
+		uint64_t max_bw = 0;
+		fast_impl = conf->generic;
+
+		/* Allocate memory for benchmarking. */
+		size_t buffer_size = conf->benchmark_buffer_size;
+		uint8_t *buffer = vmem_alloc(buffer_size, KM_SLEEP);
+
+		/* Warmup. */
+		for (i = 0; i < conf->benchmark_buffer_size; i++) {
+			buffer[i] = i % 255;
+		}
+
+		/* Loop over supported implementations. */
+		for (i = 0; i < conf->supported_n; i++) {
+			const alg_impl_ops_t *ops = conf->supported[i];
+			uint64_t run_bw;
+			uint64_t run_count = 0;
+			uint64_t start, run_time_ns;
+
+			kpreempt_disable();
+			start = gethrtime();
+			do {
+				for (c = 0; c < 32; c++, run_count++) {
+					conf->benchmark(
+					    ops, buffer, buffer_size);
+				}
+				run_time_ns = gethrtime() - start;
+			} while (run_time_ns < BENCHMARK_NS);
+			kpreempt_enable();
+
+			run_bw = buffer_size * run_count * NANOSEC;
+			run_bw /= run_time_ns;	/* B/s */
+
+			conf->bandwidth[i].bandwidth = run_bw;
+
+			/* Keep track of fastest. */
+			if (run_bw > max_bw) {
+				max_bw = run_bw;
+				fast_impl = ops;
+			}
+		}
+
+		/* Save fastest result. */
+		conf->bandwidth_fastest.bandwidth = max_bw;
+
+		/* Free memory. */
+		vmem_free(buffer, buffer_size);
+	}
+
+	/* Copy the fastest implementation. */
+	memcpy(&conf->fastest, fast_impl, sizeof (*fast_impl));
+	strlcpy(conf->fastest.name, "fastest", ALG_IMPL_NAME_MAX);
+	conf->bandwidth_fastest.ops = &conf->fastest;
+
+#if defined(_KERNEL)
+	/* Create name for kstat_create. It will be copied to ks_name. */
+	char kstat_name[KSTAT_STRLEN];
+	snprintf(kstat_name, sizeof (kstat_name), "%s_bench", conf->name);
+
+	/* Create kstat. */
+	conf->benchmark_kstat = kstat_create(
+	    "zfs", 0, kstat_name, "misc",
+	    KSTAT_TYPE_RAW, 0, KSTAT_FLAG_VIRTUAL);
+
+	/* Install kstat. */
+	if (conf->benchmark_kstat != NULL) {
+		conf->benchmark_kstat->ks_data = NULL;
+		conf->benchmark_kstat->ks_ndata = UINT32_MAX;
+		conf->benchmark_kstat->ks_private = conf;
+		kstat_set_raw_ops(conf->benchmark_kstat,
+		    impl_kstat_headers,
+		    impl_kstat_data,
+		    impl_kstat_addr);
+		kstat_install(conf->benchmark_kstat);
+	}
+#endif
+
+	/*
+	 * Finish initialization. At this time, user_sel_impl can only be one of
+	 * alg_impl_opts, because initialized is not yet set, so
+	 * alg_impl_set would have returned EINVAL for any other value.
+	 */
+	conf->cycle_impl_idx = 0;
+	atomic_swap_32(&conf->icp_alg_impl, conf->user_sel_impl);
+	conf->initialized = B_TRUE;
+}
+
+void
+alg_impl_fini(alg_impl_conf_t *conf)
+{
+#if defined(_KERNEL)
+	if (conf->benchmark_kstat != NULL) {
+		kstat_delete(conf->benchmark_kstat);
+		conf->benchmark_kstat = NULL;
+	}
+#endif
+}
+
+int
+alg_impl_get(alg_impl_conf_t *conf, char *buffer)
+{
+	int cnt = 0;
+	size_t i = 0;
+	char *fmt = NULL;
+	const uint32_t impl = atomic_load_32(&conf->icp_alg_impl);
+
+	ASSERT(conf->initialized);
+
+	/* First check mandatory options. */
+	for (i = 0; i < ARRAY_SIZE(alg_impl_opts); i++) {
+		fmt = (impl == alg_impl_opts[i].sel) ? "[%s] " : "%s ";
+		cnt += sprintf(buffer + cnt, fmt, alg_impl_opts[i].name);
+	}
+
+	/* Then check all supported. */
+	for (i = 0; i < conf->supported_n; i++) {
+		fmt = (i == impl) ? "[%s] " : "%s ";
+		cnt += sprintf(buffer + cnt, fmt, conf->supported[i]->name);
+	}
+
+	return (cnt);
+}
+
+int
+alg_impl_set(alg_impl_conf_t *conf, const char *val)
+{
+	int err = -EINVAL;
+	char req_name[ALG_IMPL_NAME_MAX];
+	uint32_t impl = atomic_load_32(&conf->user_sel_impl);
+	size_t i;
+
+	/* Sanitize input. */
+	i = strnlen(val, ALG_IMPL_NAME_MAX);
+	if (i == 0 || i >= ALG_IMPL_NAME_MAX) {
+		return (err);
+	}
+
+	/* Copy and NULL terminate. */
+	strlcpy(req_name, val, ALG_IMPL_NAME_MAX);
+	while (i > 0 && isspace(req_name[i-1])) {
+		i--;
+	}
+	req_name[i] = '\0';
+
+	/* First check mandatory options. */
+	for (i = 0; i < ARRAY_SIZE(alg_impl_opts); i++) {
+		if (strcmp(req_name, alg_impl_opts[i].name) == 0) {
+			impl = alg_impl_opts[i].sel;
+			err = 0;
+			break;
+		}
+	}
+
+	/* Then check all supported impl if init() was already called. */
+	if (err != 0 && conf->initialized) {
+		for (i = 0; i < conf->supported_n; i++) {
+			if (strcmp(req_name, conf->supported[i]->name) == 0) {
+				impl = i;
+				err = 0;
+				break;
+			}
+		}
+	}
+
+	/* If success, then set depending on configuration state. */
+	if (err == 0) {
+		if (conf->initialized) {
+			atomic_swap_32(&conf->icp_alg_impl, impl);
+		} else {
+			atomic_swap_32(&conf->user_sel_impl, impl);
+		}
+	}
+
+	return (err);
+}
+
+const alg_impl_ops_t *
+alg_impl_get_ops(alg_impl_conf_t *conf)
+{
+	const alg_impl_ops_t *ops = conf->generic;
+
+	/* Check if we are not allowed to use faster versions. */
+	if (!kfpu_allowed()) {
+		return (ops);
+	}
+
+	const uint32_t impl = atomic_load_32(&conf->icp_alg_impl);
+
+	switch (impl) {
+	case ALG_IMPL_FASTEST:
+		ASSERT(conf->initialized);
+		ops = &conf->fastest;
+		break;
+	case ALG_IMPL_CYCLE:
+		/* Cycle through supported implementations */
+		ASSERT(conf->initialized);
+		ASSERT3U(conf->supported_n, >, 0);
+
+		size_t idx = (++conf->cycle_impl_idx) % conf->supported_n;
+		ops = conf->supported[idx];
+		break;
+	default:
+		/* Use a specific implementation. */
+		ASSERT3U(impl, <, conf->supported_n);
+		ASSERT3U(conf->supported_n, >, 0);
+		if (impl < conf->supported_n) {
+			ops = conf->supported[impl];
+		}
+		break;
+	}
+
+	ASSERT3P(ops, !=, NULL);
+
+	return (ops);
+}
+
+boolean_t
+alg_impl_will_always_work(void)
+{
+	return (B_TRUE);
+}
---
+++ module/icp/include/impl/impl.h
@@ -0,0 +1,221 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+/*
+ * Copyright 2021 Jan Kasiak. All rights reserved.
+ */
+
+#ifndef	_ALG_IMPL_H
+#define	_ALG_IMPL_H
+
+/*
+ * Common code for managing algorithm implementations.
+ */
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <sys/zfs_context.h>
+
+/* A function that tests whether method will work. */
+typedef boolean_t (*alg_impl_will_work_f)(void);
+
+/* Maximum length of algorithm implementation name (including NULL). */
+#define	ALG_IMPL_NAME_MAX (16)
+
+/* Algorithm implementation operations and name. */
+typedef struct alg_impl_ops {
+	void *ctx; /* Algorithm implementation context. */
+
+	alg_impl_will_work_f is_supported;
+
+	/* Algorithm priorty. Higher is faster. Used if benchmark not set. */
+	uint64_t priority;
+
+	/* Name of implementation variant. */
+	char name[ALG_IMPL_NAME_MAX];
+} alg_impl_ops_t;
+
+/* Fastest algorithm implementation. */
+#define	ALG_IMPL_FASTEST (UINT32_MAX)
+
+/* Cycle to next implementation. */
+#define	ALG_IMPL_CYCLE (UINT32_MAX-1)
+
+/*
+ * A function that benchmarks a supported imlpementation.
+ *
+ * @param[in] ops
+ * @param[in] buffer
+ * @param     buffer_n size in bytes
+ */
+typedef void (*alg_impl_benchmark_f)(
+	const alg_impl_ops_t *ops, void *buffer, size_t buffer_n);
+
+/*
+ * Algorithm implementation and benchmark bandwidth result.
+ */
+typedef struct {
+	const alg_impl_ops_t *ops;
+	uint64_t bandwidth;
+} alg_impl_ops_bandwidth_t;
+
+/*
+ * Algorithm implementation configuration.
+ */
+typedef struct alg_impl_conf {
+	const char *name; /* Name of algorithm. */
+
+	const alg_impl_ops_t **available; /* Available implementations. */
+	uint32_t available_n; /* Length of available. */
+
+	const alg_impl_ops_t **supported; /* Supported implementations. */
+	uint32_t supported_n; /* Length of supported, <= available_n. */
+
+	uint32_t cycle_impl_idx; /* Index in case of "cycle" implementation. */
+
+	/*
+	 * Index into supported or one of ALG_IMPL_CYCLE or ALG_IMPL_FASTEST.
+	 */
+	uint32_t icp_alg_impl;
+
+	/*
+	 * User desired value of icp_alg_impl, before initialization finishes.
+	 * Must be one of ALG_IMPL_FASTEST or ALG_IMPL_CYCLE.
+	 */
+	uint32_t user_sel_impl;
+
+	alg_impl_benchmark_f benchmark; /* Benchmark function. */
+	size_t benchmark_buffer_size;   /* Size of buffer for benchmarking. */
+	alg_impl_ops_bandwidth_t *bandwidth; /* Benchmark results. */
+	alg_impl_ops_bandwidth_t bandwidth_fastest; /* Fastest result. */
+
+	alg_impl_ops_t fastest; /* Fastest implementation. */
+	alg_impl_ops_t *generic; /* Fallback generic implementation. */
+
+	boolean_t initialized; /* Is configuration initilized. */
+
+#if defined(_KERNEL)
+	kstat_t *benchmark_kstat;
+#endif
+} alg_impl_conf_t;
+
+/*
+ * Delcare a alg_impl_ops_t struct.
+ *
+ * @param name_s     name of algorithm, example: "sha256"
+ * @param avail      array of available implementations
+ * @param supp       array of size size avail, initialized by alg_impl_init
+ * @param gen        generic fallback implementations
+ * @param bench      benchmark function (optional)
+ * @param bench_size size of buffer to use for benchmark
+ * @param bw         array of size avail, initialized by alg_impl_init
+ */
+#define	ALG_IMPL_CONF_DECL(name_s, avail, supp, gen, bench, bench_size, bw) \
+	{ \
+		.name = name_s, \
+		.available = avail, \
+		.available_n = ARRAY_SIZE(avail), \
+		.supported = (const alg_impl_ops_t **)(supp), \
+		.supported_n = 0, \
+		.cycle_impl_idx = 0, \
+		.icp_alg_impl = ALG_IMPL_FASTEST, \
+		.user_sel_impl = ALG_IMPL_FASTEST, \
+		.benchmark = bench, \
+		.benchmark_buffer_size = bench_size, \
+		.bandwidth = bw, \
+		.bandwidth_fastest = {0}, \
+		.fastest = {0}, \
+		.generic = &(gen), \
+		.initialized = 0, \
+	}
+
+/*
+ * Initialize conf.
+ *
+ * The following must be already set:
+ * - available
+ * - available_n
+ * - generic
+ * - supported - points to an empty array of size available_n
+ * - user_sel_impl
+ *
+ * @param[in,out] conf
+ */
+void alg_impl_init(alg_impl_conf_t *conf);
+
+/*
+ * Fini conf.
+ *
+ * @param[in,out] conf
+ */
+void alg_impl_fini(alg_impl_conf_t *conf);
+
+/*
+ * Get the selected and supported implementations.
+ *
+ * Uses sprintf and writes at most: N * (ALG_IMPL_NAME_MAX + 2) bytes to
+ * buffer, where N is ARRAY_SIZE(alg_impl_opts) + conf->supported_n
+ *
+ * @param[in]     conf
+ * @param[in,out] buffer
+ *
+ * @return number of bytes written
+ */
+int alg_impl_get(alg_impl_conf_t *conf, char *buffer);
+
+/*
+ * Set the desired implementation to use.
+ *
+ * @param[in,out] conf
+ * @param[in]     val one of supported[i].name
+ *
+ * @retval 0 success
+ * @retval -EINVAL unknown or unsupported implementation
+ */
+int alg_impl_set(alg_impl_conf_t *conf, const char *val);
+
+/*
+ * Get the implementation ops to use.
+ *
+ * - If !kfpu_allowed, then returns conf->generic
+ * - If alg_impl_set was set to "cycle", then returns the next implementation.
+ * - If alg_impl_set was set to any of supported[i].name, then returns that
+ *   implementation.
+ *
+ * @param[in,out] conf
+ *
+ * @return one of conf->generic, conf->fastest, or conf->supported
+ */
+const alg_impl_ops_t *alg_impl_get_ops(alg_impl_conf_t *conf);
+
+/*
+ * Helper function that always returns true.
+ *
+ * @retval B_TRUE
+ */
+boolean_t alg_impl_will_always_work(void);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* _ALG_IMPL_H */
