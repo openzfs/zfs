openzfsonosx/openzfs

Upstream: unmount snapshots before destroying them on macOS

include/libzfs.h
@@ -641,6 +641,7 @@ extern int zfs_create_ancestors(libzfs_handle_t *, const char *);
 extern int zfs_destroy(zfs_handle_t *, boolean_t);
 extern int zfs_destroy_snaps(zfs_handle_t *, char *, boolean_t);
 extern int zfs_destroy_snaps_nvl(libzfs_handle_t *, nvlist_t *, boolean_t);
+extern int zfs_destroy_snaps_nvl_os(libzfs_handle_t *, nvlist_t *);
 extern int zfs_clone(zfs_handle_t *, const char *, nvlist_t *);
 extern int zfs_snapshot(libzfs_handle_t *, const char *, boolean_t, nvlist_t *);
 extern int zfs_snapshot_nvl(libzfs_handle_t *hdl, nvlist_t *snaps,

openzfs/zfs

Signed-off-by: Jorgen Lundman <lundman@lundman.net>

include/libzfs.h
@@ -650,6 +650,7 @@ _LIBZFS_H int zfs_create_ancestors(libzfs_handle_t *, const char *);
 _LIBZFS_H int zfs_destroy(zfs_handle_t *, boolean_t);
 _LIBZFS_H int zfs_destroy_snaps(zfs_handle_t *, char *, boolean_t);
 _LIBZFS_H int zfs_destroy_snaps_nvl(libzfs_handle_t *, nvlist_t *, boolean_t);
+_LIBZFS_H int zfs_destroy_snaps_nvl_os(libzfs_handle_t *, nvlist_t *);
 _LIBZFS_H int zfs_clone(zfs_handle_t *, const char *, nvlist_t *);
 _LIBZFS_H int zfs_snapshot(libzfs_handle_t *, const char *, boolean_t,
     nvlist_t *);