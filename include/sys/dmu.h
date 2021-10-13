afbc61792116c9599afcccfd61204f968401d06e
Pawel Jakub Dawidek <pawel@dawidek.net>
2021-10-13 11:01:01 -0700

    Remove FreeBSD's local copy of the dmu_buf_hold_array() function
    Make the main dmu_buf_hold_array() function non-static.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Ryan Moeller <ryan@ixsystems.com>
    Signed-off-by: Pawel Jakub Dawidek <pawel@dawidek.net>
    Closes #12628

--- include/sys/dmu.h
+++ include/sys/dmu.h
@@ -559,6 +559,8 @@ int dmu_spill_hold_existing(dmu_buf_t *bonus, void *tag, dmu_buf_t **dbp);
  */
 int dmu_buf_hold(objset_t *os, uint64_t object, uint64_t offset,
     void *tag, dmu_buf_t **, int flags);
+int dmu_buf_hold_array(objset_t *os, uint64_t object, uint64_t offset,
+    uint64_t length, int read, void *tag, int *numbufsp, dmu_buf_t ***dbpp);
 int dmu_buf_hold_by_dnode(dnode_t *dn, uint64_t offset,
     void *tag, dmu_buf_t **dbp, int flags);
 int dmu_buf_hold_array_by_dnode(dnode_t *dn, uint64_t offset,
