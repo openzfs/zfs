2f05032f29357af6e0881e51ace884c22454447e
наб <nabijaczleweli@nabijaczleweli.xyz>
2021-12-05 15:15:11 +0100

    FreeBSD: zfeature_common#zfs_mod_supported_feature(): ignore name
    Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>

--- module/zcommon/zfeature_common.c
+++ module/zcommon/zfeature_common.c
@@ -229,6 +229,7 @@ zfs_mod_supported_feature(const char *name)
 	 */
 
 #if defined(_KERNEL) || defined(LIB_ZPOOL_BUILD) || defined(__FreeBSD__)
+	(void) name;
 	return (B_TRUE);
 #else
 	return (zfs_mod_supported(ZFS_SYSFS_POOL_FEATURES, name));
