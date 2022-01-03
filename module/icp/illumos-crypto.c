4b0977027bae95ab916002e372f339f9ad3902a3
Tino Reichardt <github@mcmilk.de>
2022-01-03 17:43:11 +0100

    Remove sha1 hashing from OpenZFS, it's not used anywhere.
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Attila Fülöp <attila@fueloep.org>
    Signed-off-by: Tino Reichardt <milky-zfs@mcmilk.de>
    Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
    Closes #12895
    Closes #12902
    Signed-off-by: Rich Ercolani <rincebrain@gmail.com>

--- module/icp/illumos-crypto.c
+++ module/icp/illumos-crypto.c
@@ -111,7 +111,6 @@ icp_fini(void)
 {
 	skein_mod_fini();
 	sha2_mod_fini();
-	sha1_mod_fini();
 	edonr_mod_fini();
 	aes_mod_fini();
 	kcf_sched_destroy();
@@ -142,7 +141,6 @@ icp_init(void)
 	/* initialize algorithms */
 	aes_mod_init();
 	edonr_mod_init();
-	sha1_mod_init();
 	sha2_mod_init();
 	skein_mod_init();
 
