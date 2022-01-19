openzfs/zfs

commit 42aaf0e7c4b9cf0fe9c18d218f291f12dd5b26a2

libspl: ASSERT*: mark arguments as used
Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
Closes #12844

include/os/freebsd/spl/sys/debug.h
@@ -133,14 +133,14 @@ void spl_dumpstack(void);
  */
 #ifdef NDEBUG
 
-#define	ASSERT(x)		((void)0)
-#define	ASSERT3B(x,y,z)		((void)0)
-#define	ASSERT3S(x,y,z)		((void)0)
-#define	ASSERT3U(x,y,z)		((void)0)
-#define	ASSERT3P(x,y,z)		((void)0)
-#define	ASSERT0(x)		((void)0)
-#define	IMPLY(A, B)		((void)0)
-#define	EQUIV(A, B)		((void)0)
+#define	ASSERT(x)		((void)sizeof(x))
+#define	ASSERT3B(x,y,z)		((void)sizeof(x), (void)sizeof(z))
+#define	ASSERT3S(x,y,z)		((void)sizeof(x), (void)sizeof(z))
+#define	ASSERT3U(x,y,z)		((void)sizeof(x), (void)sizeof(z))
+#define	ASSERT3P(x,y,z)		((void)sizeof(x), (void)sizeof(z))
+#define	ASSERT0(x)		((void)sizeof(x))
+#define	IMPLY(A, B)		((void)sizeof(A), (void)sizeof(B))
+#define	EQUIV(A, B)		((void)sizeof(A), (void)sizeof(B))
 
 /*
  * Debugging enabled (--enable-debug)

include/os/linux/spl/sys/debug.h
@@ -133,14 +133,14 @@ void spl_dumpstack(void);
  */
 #ifdef NDEBUG
 
-#define	ASSERT(x)		((void)0)
-#define	ASSERT3B(x,y,z)		((void)0)
-#define	ASSERT3S(x,y,z)		((void)0)
-#define	ASSERT3U(x,y,z)		((void)0)
-#define	ASSERT3P(x,y,z)		((void)0)
-#define	ASSERT0(x)		((void)0)
-#define	IMPLY(A, B)		((void)0)
-#define	EQUIV(A, B)		((void)0)
+#define	ASSERT(x)		((void)sizeof(x))
+#define	ASSERT3B(x,y,z)		((void)sizeof(x), (void)sizeof(z))
+#define	ASSERT3S(x,y,z)		((void)sizeof(x), (void)sizeof(z))
+#define	ASSERT3U(x,y,z)		((void)sizeof(x), (void)sizeof(z))
+#define	ASSERT3P(x,y,z)		((void)sizeof(x), (void)sizeof(z))
+#define	ASSERT0(x)		((void)sizeof(x))
+#define	IMPLY(A, B)		((void)sizeof(A), (void)sizeof(B))
+#define	EQUIV(A, B)		((void)sizeof(A), (void)sizeof(B))
 
 /*
  * Debugging enabled (--enable-debug)

lib/libspl/include/assert.h
@@ -120,15 +120,15 @@ do {									\
 	__compile_time_assertion__ ## y[(x) ? 1 : -1]
 
 #ifdef NDEBUG
-#define	ASSERT3B(x, y, z)	((void)0)
-#define	ASSERT3S(x, y, z)	((void)0)
-#define	ASSERT3U(x, y, z)	((void)0)
-#define	ASSERT3P(x, y, z)	((void)0)
-#define	ASSERT0(x)		((void)0)
-#define	ASSERT(x)		((void)0)
-#define	assert(x)		((void)0)
-#define	IMPLY(A, B)		((void)0)
-#define	EQUIV(A, B)		((void)0)
+#define	ASSERT3B(x, y, z)	((void) sizeof (x), (void) sizeof (z))
+#define	ASSERT3S(x, y, z)	((void) sizeof (x), (void) sizeof (z))
+#define	ASSERT3U(x, y, z)	((void) sizeof (x), (void) sizeof (z))
+#define	ASSERT3P(x, y, z)	((void) sizeof (x), (void) sizeof (z))
+#define	ASSERT0(x)		((void) sizeof (x))
+#define	ASSERT(x)		((void) sizeof (x))
+#define	assert(x)		((void) sizeof (x))
+#define	IMPLY(A, B)		((void) sizeof (A), (void) sizeof (B))
+#define	EQUIV(A, B)		((void) sizeof (A), (void) sizeof (B))
 #else
 #define	ASSERT3B	VERIFY3B
 #define	ASSERT3S	VERIFY3S

commit a9ab88ce9819d48e4d02340dd68c2a7e889d853a

libspl: ASSERT*: !! for sizeof
sizeof(bitfield.member) is invalid, and this shows up in some FreeBSD
build configurations: work around this by !!ing ‒
this makes the sizeof target the ! result type (_Bool), instead

Fixes: 42aaf0e ("libspl: ASSERT*: mark arguments as used")
Closes: #12984
Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>

include/os/freebsd/spl/sys/debug.h
@@ -133,14 +133,14 @@ void spl_dumpstack(void);
  */
 #ifdef NDEBUG
 
-#define	ASSERT(x)		((void)sizeof(x))
-#define	ASSERT3B(x,y,z)		((void)sizeof(x), (void)sizeof(z))
-#define	ASSERT3S(x,y,z)		((void)sizeof(x), (void)sizeof(z))
-#define	ASSERT3U(x,y,z)		((void)sizeof(x), (void)sizeof(z))
-#define	ASSERT3P(x,y,z)		((void)sizeof(x), (void)sizeof(z))
-#define	ASSERT0(x)		((void)sizeof(x))
-#define	IMPLY(A, B)		((void)sizeof(A), (void)sizeof(B))
-#define	EQUIV(A, B)		((void)sizeof(A), (void)sizeof(B))
+#define	ASSERT(x)		((void) sizeof (!!(x)))
+#define	ASSERT3B(x,y,z)		((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT3S(x,y,z)		((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT3U(x,y,z)		((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT3P(x,y,z)		((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT0(x)		((void) sizeof (!!(x)))
+#define	IMPLY(A, B)		((void) sizeof (!!(A)), (void) sizeof (!!(B)))
+#define	EQUIV(A, B)		((void) sizeof (!!(A)), (void) sizeof (!!(B)))
 
 /*
  * Debugging enabled (--enable-debug)

include/os/linux/spl/sys/debug.h
@@ -133,14 +133,14 @@ void spl_dumpstack(void);
  */
 #ifdef NDEBUG
 
-#define	ASSERT(x)		((void)sizeof(x))
-#define	ASSERT3B(x,y,z)		((void)sizeof(x), (void)sizeof(z))
-#define	ASSERT3S(x,y,z)		((void)sizeof(x), (void)sizeof(z))
-#define	ASSERT3U(x,y,z)		((void)sizeof(x), (void)sizeof(z))
-#define	ASSERT3P(x,y,z)		((void)sizeof(x), (void)sizeof(z))
-#define	ASSERT0(x)		((void)sizeof(x))
-#define	IMPLY(A, B)		((void)sizeof(A), (void)sizeof(B))
-#define	EQUIV(A, B)		((void)sizeof(A), (void)sizeof(B))
+#define	ASSERT(x)		((void) sizeof (!!(x)))
+#define	ASSERT3B(x,y,z)		((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT3S(x,y,z)		((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT3U(x,y,z)		((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT3P(x,y,z)		((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT0(x)		((void) sizeof (!!(x)))
+#define	IMPLY(A, B)		((void) sizeof (!!(A)), (void) sizeof (!!(B)))
+#define	EQUIV(A, B)		((void) sizeof (!!(A)), (void) sizeof (!!(B)))
 
 /*
  * Debugging enabled (--enable-debug)

lib/libspl/include/assert.h
@@ -120,15 +120,15 @@ do {									\
 	__compile_time_assertion__ ## y[(x) ? 1 : -1]
 
 #ifdef NDEBUG
-#define	ASSERT3B(x, y, z)	((void) sizeof (x), (void) sizeof (z))
-#define	ASSERT3S(x, y, z)	((void) sizeof (x), (void) sizeof (z))
-#define	ASSERT3U(x, y, z)	((void) sizeof (x), (void) sizeof (z))
-#define	ASSERT3P(x, y, z)	((void) sizeof (x), (void) sizeof (z))
-#define	ASSERT0(x)		((void) sizeof (x))
-#define	ASSERT(x)		((void) sizeof (x))
-#define	assert(x)		((void) sizeof (x))
-#define	IMPLY(A, B)		((void) sizeof (A), (void) sizeof (B))
-#define	EQUIV(A, B)		((void) sizeof (A), (void) sizeof (B))
+#define	ASSERT3B(x, y, z)	((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT3S(x, y, z)	((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT3U(x, y, z)	((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT3P(x, y, z)	((void) sizeof (!!(x)), (void) sizeof (!!(z)))
+#define	ASSERT0(x)		((void) sizeof (!!(x)))
+#define	ASSERT(x)		((void) sizeof (!!(x)))
+#define	assert(x)		((void) sizeof (!!(x)))
+#define	IMPLY(A, B)		((void) sizeof (!!(A)), (void) sizeof (!!(B)))
+#define	EQUIV(A, B)		((void) sizeof (!!(A)), (void) sizeof (!!(B)))
 #else
 #define	ASSERT3B	VERIFY3B
 #define	ASSERT3S	VERIFY3S