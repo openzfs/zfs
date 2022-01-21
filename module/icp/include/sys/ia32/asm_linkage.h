56f5bfbe3f922675421424e6afead559c6cddfd5
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-01-21 14:40:12 +0100

    icp: asm_linkage.h: clean out unused bits, CSTYLED
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
    Closes #12993

--- module/icp/include/sys/ia32/asm_linkage.h
+++ module/icp/include/sys/ia32/asm_linkage.h
@@ -111,88 +111,6 @@ extern "C" {
 #define	XMM_SIZE	16
 #define	XMM_ALIGN	16
 
-#if defined(__amd64)
-
-#define	SAVE_XMM_PROLOG(sreg, nreg)				\
-	subq	$_CONST(_MUL(XMM_SIZE, nreg)), %rsp;		\
-	movq	%rsp, sreg
-
-#define	RSTOR_XMM_EPILOG(sreg, nreg)				\
-	addq	$_CONST(_MUL(XMM_SIZE, nreg)), %rsp
-
-#elif defined(__i386)
-
-#define	SAVE_XMM_PROLOG(sreg, nreg)				\
-	subl	$_CONST(_MUL(XMM_SIZE, nreg) + XMM_ALIGN), %esp; \
-	movl	%esp, sreg;					\
-	addl	$XMM_ALIGN, sreg;				\
-	andl	$_BITNOT(XMM_ALIGN-1), sreg
-
-#define	RSTOR_XMM_EPILOG(sreg, nreg)				\
-	addl	$_CONST(_MUL(XMM_SIZE, nreg) + XMM_ALIGN), %esp;
-
-#endif	/* __i386 */
-
-/*
- * profiling causes definitions of the MCOUNT and RTMCOUNT
- * particular to the type
- */
-#ifdef GPROF
-
-#define	MCOUNT(x) \
-	pushl	%ebp; \
-	movl	%esp, %ebp; \
-	call	_mcount; \
-	popl	%ebp
-
-#endif /* GPROF */
-
-#ifdef PROF
-
-#define	MCOUNT(x) \
-/* CSTYLED */ \
-	.lcomm .L_/**/x/**/1, 4, 4; \
-	pushl	%ebp; \
-	movl	%esp, %ebp; \
-/* CSTYLED */ \
-	movl	$.L_/**/x/**/1, %edx; \
-	call	_mcount; \
-	popl	%ebp
-
-#endif /* PROF */
-
-/*
- * if we are not profiling, MCOUNT should be defined to nothing
- */
-#if !defined(PROF) && !defined(GPROF)
-#define	MCOUNT(x)
-#endif /* !defined(PROF) && !defined(GPROF) */
-
-#define	RTMCOUNT(x)	MCOUNT(x)
-
-/*
- * Macro to define weak symbol aliases. These are similar to the ANSI-C
- *	#pragma weak _name = name
- * except a compiler can determine type. The assembler must be told. Hence,
- * the second parameter must be the type of the symbol (i.e.: function,...)
- */
-#define	ANSI_PRAGMA_WEAK(sym, stype)	\
-/* CSTYLED */ \
-	.weak	_/**/sym; \
-/* CSTYLED */ \
-	.type	_/**/sym, @stype; \
-/* CSTYLED */ \
-_/**/sym = sym
-
-/*
- * Like ANSI_PRAGMA_WEAK(), but for unrelated names, as in:
- *	#pragma weak sym1 = sym2
- */
-#define	ANSI_PRAGMA_WEAK2(sym1, sym2, stype)	\
-	.weak	sym1; \
-	.type sym1, @stype; \
-sym1	= sym2
-
 /*
  * ENTRY provides the standard procedure entry code and an easy way to
  * insert the calls to mcount for profiling. ENTRY_NP is identical, but
@@ -212,24 +130,16 @@ x:	MCOUNT(x)
 	.type	x, @function; \
 x:
 
-#define	RTENTRY(x) \
-	.text; \
-	.align	ASM_ENTRY_ALIGN; \
-	.globl	x; \
-	.type	x, @function; \
-x:	RTMCOUNT(x)
-
 /*
  * ENTRY2 is identical to ENTRY but provides two labels for the entry point.
  */
 #define	ENTRY2(x, y) \
-	.text; \
+	.text;	\
 	.align	ASM_ENTRY_ALIGN; \
 	.globl	x, y; \
 	.type	x, @function; \
 	.type	y, @function; \
-/* CSTYLED */ \
-x:	; \
+x:; \
 y:	MCOUNT(x)
 
 #define	ENTRY_NP2(x, y) \
@@ -238,65 +148,15 @@ y:	MCOUNT(x)
 	.globl	x, y; \
 	.type	x, @function; \
 	.type	y, @function; \
-/* CSTYLED */ \
-x:	; \
+x:; \
 y:
 
-
-/*
- * ALTENTRY provides for additional entry points.
- */
-#define	ALTENTRY(x) \
-	.globl x; \
-	.type	x, @function; \
-x:
-
-/*
- * DGDEF and DGDEF2 provide global data declarations.
- *
- * DGDEF provides a word aligned word of storage.
- *
- * DGDEF2 allocates "sz" bytes of storage with **NO** alignment.  This
- * implies this macro is best used for byte arrays.
- *
- * DGDEF3 allocates "sz" bytes of storage with "algn" alignment.
- */
-#define	DGDEF2(name, sz) \
-	.data; \
-	.globl	name; \
-	.type	name, @object; \
-	.size	name, sz; \
-name:
-
-#define	DGDEF3(name, sz, algn) \
-	.data; \
-	.align	algn; \
-	.globl	name; \
-	.type	name, @object; \
-	.size	name, sz; \
-name:
-
-#define	DGDEF(name)	DGDEF3(name, 4, 4)
 
 /*
  * SET_SIZE trails a function and set the size for the ELF symbol table.
  */
 #define	SET_SIZE(x) \
 	.size	x, [.-x]
-
-/*
- * NWORD provides native word value.
- */
-#if defined(__amd64)
-
-/*CSTYLED*/
-#define	NWORD	quad
-
-#elif defined(__i386)
-
-#define	NWORD	long
-
-#endif  /* __i386 */
 
 #endif /* _ASM */
 
