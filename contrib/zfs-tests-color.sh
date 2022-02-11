d814cfba57989b671f96f43af3ff3b2a1196d48f
Rich Ercolani <rincebrain@gmail.com>
2022-02-10 20:25:29 -0500

    Refactored into something marginally less horrible.
    Signed-off-by: Rich Ercolani <rincebrain@gmail.com>

--- contrib/zfs-tests-color.sh
+++ contrib/zfs-tests-color.sh
@@ -1,38 +1,27 @@
-#!/bin/bash
+#!/bin/sh
+# A large mass of sed for coloring zfs-tests.sh output
+# Version 2, thanks to наб.
+# Just pipe zfs-tests.sh output into this, and watch.
 
-# A quick and dirty script for providing a bit of color and contrast
-# in zfs-tests.sh output
-
-# We don't want while read LINE to eat our leading whitespace
-IFS=''
-if [[ "$(uname -s)" == "FreeBSD" ]]; then
-	SED=gsed
-else
-	SED=sed
-fi
-while read LINE; do
-	res=$(echo "${LINE}" | $SED \
-		-e s/'\] \[PASS\]$'/'\] \[\\e\[92mPASS\\e\[0m\]'/ \
-		-e s/'\] \[FAIL\]$'/'\] \[\\e\[1;91mFAIL\\e\[0m\]'/ \
-		-e s/'\] \[KILLED\]$'/'\] \[\\e\[1;101mKILLED\\e\[0m\]'/ \
-		-e s/'\] \[SKIP\]$'/'\] \[\\e\[1mSKIP\\e\[0m\]'/ \
-		-e s/'\] \[RERAN\]$'/'\] \[\\e\[1;93mRERAN\\e\[0m\]'/ \
-		-e s/'^\(PASS\W\)'/'\\e\[92m\1\\e\[0m'/ \
-		-e s/'^\(FAIL\W\)'/'\\e\[1;91m\1\\e\[0m'/ \
-		-e s/'^\(KILLED\W\)'/'\\\e\[1;101m\1\\e\[0m'/ \
-		-e s/'^\(SKIP\W\)'/'\\e\[1m\1\\e\[0m'/ \
-		-e s/'^\(RERAN\W\)'/'\\e\[1;93m\1\\e\[0m'/ \
-		-e s/'^Tests\ with\ result\(.\+\)PASS\(.\+\)$'/'Tests with result\1\\e\[92mPASS\\e\[0m\2'/ \
-		-e s/'^\(\W\+\)\(KILLED\)\(\W\)'/'\1\\e\[1;101m\2\\e\[0m\3'/g \
-		-e s/'^\(\W\+\)\(FAIL\)\(\W\)'/'\1\\e\[1;91m\2\\e\[0m\3'/g \
-		-e s/'^\(\W\+\)\(RERUN\)\(\W\)'/'\1\\e\[1;93m\2\\e\[0m\3'/g \
-		-e s/'^\(\W\+\)\(SKIP\)\(\W\)'/'\1\\e\[1m\2\\e\[0m\3'/g \
-		-e s/'expected \(PASS\))$'/'expected \\e\[92m\1\\e\[0m)'/ \
-		-e s/'expected \(KILLED\))$'/'expected \\e\[1;101m\1\\e\[0m)'/ \
-		-e s/'expected \(FAIL\))$'/'expected \\e\[1;91m\1\\e\[0m)'/ \
-		-e s/'expected \(RERUN\))$'/'expected \\e\[1;93m\1\\e\[0m)'/ \
-		-e s/'expected \(SKIP\))$'/'expected \\e\[1m\1\\e\[0m)'/ \
-		-e s/'^Test\( ([A-Za-z0-9 ]\+)\)\?: \(.\+\) (run as \(.\+\)) \[\([0-9]\+:[0-9]\+\)\] \[\(.\+\)\]$'/'\\e\[1mTest\1: \\e\[0m\2 (run as \\e\[1m\3\\e\[0m) \[\\e\[1m\4\\e\[0m\] \[\5\]'/ \
-	)
-	echo -e "${res}"
-done
+exec "$(command -v gsed || echo sed)" \
+	-e 's/\] \[PASS\]$/] [\x1b[92mPASS\x1b[0m]/' \
+	-e 's/\] \[FAIL\]$/] [\x1b[1;91mFAIL\x1b[0m]/' \
+	-e 's/\] \[KILLED\]$/] [\x1b[1;101mKILLED\x1b[0m]/' \
+	-e 's/\] \[SKIP\]$/] [\x1b[1mSKIP\x1b[0m]/' \
+	-e 's/\] \[RERAN\]$/] [\x1b[1;93mRERAN\x1b[0m]/' \
+	-e 's/^\(PASS\W\)/\x1b[92m\1\x1b[0m/' \
+	-e 's/^\(FAIL\W\)/\x1b[1;91m\1\x1b[0m/' \
+	-e 's/^\(KILLED\W\)/\x1b[1;101m\1\x1b[0m/' \
+	-e 's/^\(SKIP\W\)/\x1b[1m\1\x1b[0m/' \
+	-e 's/^\(RERAN\W\)/\x1b[1;93m\1\x1b[0m/' \
+	-e 's/^Tests with result\(.\+\)PASS\(.\+\)$/Tests with result\1\x1b[92mPASS\x1b[0m\2/' \
+	-e 's/^\(\W\+\)\(KILLED\)\(\W\)/\1\x1b[1;101m\2\x1b[0m\3/g' \
+	-e 's/^\(\W\+\)\(FAIL\)\(\W\)/\1\x1b[1;91m\2\x1b[0m\3/g' \
+	-e 's/^\(\W\+\)\(RERUN\)\(\W\)/\1\x1b[1;93m\2\x1b[0m\3/g' \
+	-e 's/^\(\W\+\)\(SKIP\)\(\W\)/\1\x1b[1m\2\x1b[0m\3/g' \
+	-e 's/expected \(PASS\))$/expected \x1b[92m\1\x1b[0m)/' \
+	-e 's/expected \(KILLED\))$/expected \x1b[1;101m\1\x1b[0m)/' \
+	-e 's/expected \(FAIL\))$/expected \x1b[1;91m\1\x1b[0m)/' \
+	-e 's/expected \(RERUN\))$/expected \x1b[1;93m\1\x1b[0m)/' \
+	-e 's/expected \(SKIP\))$/expected \x1b[1m\1\x1b[0m)/' \
+	-e 's/^Test\( ([[:alnum:] ]\+)\)\?: \(.\+\) (run as \(.\+\)) \[\([0-9]\+:[0-9]\+\)\] \[\(.\+\)\]$/\x1b[1mTest\1: \x1b[0m\2 (run as \x1b[1m\3\x1b[0m) [\x1b[1m\4\x1b[0m\] [\5\]/'
