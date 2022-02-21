32152cd8ae9aa36db7e71a3425195909f9617a27
Jorgen Lundman <lundman@lundman.net>
2022-02-21 09:45:32 +0900

    Upstream: Expose zvol state list
    Until a proper locking solution is found, see macOS

--- module/zfs/zvol.c
+++ module/zfs/zvol.c
@@ -90,7 +90,7 @@ unsigned int zvol_inhibit_dev = 0;
 unsigned int zvol_volmode = ZFS_VOLMODE_GEOM;
 
 struct hlist_head *zvol_htable;
-static list_t zvol_state_list;
+list_t zvol_state_list;
 krwlock_t zvol_state_lock;
 
 typedef enum {
