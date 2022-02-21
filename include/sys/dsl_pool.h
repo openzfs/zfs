5d136383f10d845d60f8352e92a360a3fcb969d0
Jorgen Lundman <lundman@lundman.net>
2022-02-21 09:42:46 +0900

    Upstream: dsl pool.h
    Restore missing tunable, until it can be fixed in earlier
    commits

--- include/sys/dsl_pool.h
+++ include/sys/dsl_pool.h
@@ -59,6 +59,7 @@ struct dsl_deadlist;
 
 extern uint64_t zfs_dirty_data_max;
 extern uint64_t zfs_dirty_data_max_max;
+extern unsigned long zfs_wrlog_data_max;
 extern int zfs_dirty_data_sync_percent;
 extern int zfs_dirty_data_max_percent;
 extern int zfs_dirty_data_max_max_percent;
