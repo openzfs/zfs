21d976a62119d3442a0b83693b96f74ac6d0ff5b
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-02-28 15:00:49 +0100

    libshare: delineate obsolete errors
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
    Closes #13165

--- lib/libspl/include/libshare.h
+++ lib/libspl/include/libshare.h
@@ -29,28 +29,26 @@
 
 #include <sys/types.h>
 
-/* API Initialization */
-#define	SA_INIT_SHARE_API	0x0001	/* init share specific interface */
-#define	SA_INIT_CONTROL_API	0x0002	/* init control specific interface */
-
 /*
  * defined error values
  */
-
 #define	SA_OK			0
-#define	SA_NO_SUCH_PATH		1	/* provided path doesn't exist */
+#define	SA_SYSTEM_ERR		7	/* system error, use errno */
+#define	SA_SYNTAX_ERR		8	/* syntax error on command line */
 #define	SA_NO_MEMORY		2	/* no memory for data structures */
+#define	SA_INVALID_PROTOCOL	13	/* specified protocol not valid */
+#define	SA_NOT_SUPPORTED	21	/* operation not supported for proto */
+
+/* The following errors are never returned by libshare */
+#define	SA_NO_SUCH_PATH		1	/* provided path doesn't exist */
 #define	SA_DUPLICATE_NAME	3	/* object name is already in use */
 #define	SA_BAD_PATH		4	/* not a full path */
 #define	SA_NO_SUCH_GROUP	5	/* group is not defined */
 #define	SA_CONFIG_ERR		6	/* system configuration error */
-#define	SA_SYSTEM_ERR		7	/* system error, use errno */
-#define	SA_SYNTAX_ERR		8	/* syntax error on command line */
 #define	SA_NO_PERMISSION	9	/* no permission for operation */
 #define	SA_BUSY			10	/* resource is busy */
 #define	SA_NO_SUCH_PROP		11	/* property doesn't exist */
 #define	SA_INVALID_NAME		12	/* name of object is invalid */
-#define	SA_INVALID_PROTOCOL	13	/* specified protocol not valid */
 #define	SA_NOT_ALLOWED		14	/* operation not allowed */
 #define	SA_BAD_VALUE		15	/* bad value for property */
 #define	SA_INVALID_SECURITY	16	/* invalid security type */
@@ -58,7 +56,6 @@
 #define	SA_VALUE_CONFLICT	18	/* property value conflict */
 #define	SA_NOT_IMPLEMENTED	19	/* plugin interface not implemented */
 #define	SA_INVALID_PATH		20	/* path is sub-dir of existing share */
-#define	SA_NOT_SUPPORTED	21	/* operation not supported for proto */
 #define	SA_PROP_SHARE_ONLY	22	/* property valid on share only */
 #define	SA_NOT_SHARED		23	/* path is not shared */
 #define	SA_NO_SUCH_RESOURCE	24	/* resource not found */
