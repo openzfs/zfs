c6652731d91eef1377a1fae7acbc25c934511bde
Rich Ercolani <rincebrain@gmail.com>
2022-03-16 20:06:54 -0400

    Tiered early abort, zstd edition.
    It turns out that "do LZ4 and zstd-1 both fail" is a great heuristic
    for "don't even bother trying higher zstd tiers".
    
    By way of illustration:
    $ cat /incompress | mbuffer | zfs recv -o compression=zstd-12 evenfaster/lowcomp_1M_zstd12_normal
    summary: 39.8 GiByte in  3min 40.2sec - average of  185 MiB/s
    $ echo 3 | sudo tee /sys/module/zzstd/parameters/zstd_lz4_pass
    3
    $ cat /incompress | mbuffer -m 4G | zfs recv -o compression=zstd-12 evenfaster/lowcomp_1M_zstd12_patched
    summary: 39.8 GiByte in 48.6sec - average of  839 MiB/s
    $ sudo zfs list -p -o name,used,lused,ratio evenfaster/lowcomp_1M_zstd12_normal evenfaster/lowcomp_1M_zstd12_patched
    NAME                                         USED        LUSED  RATIO
    evenfaster/lowcomp_1M_zstd12_normal   39549931520  42721221632   1.08
    evenfaster/lowcomp_1M_zstd12_patched  39626399744  42721217536   1.07
    $ python3 -c "print(39626399744 - 39549931520)"
    76468224
    $
    
    I'll take 76 MB out of 42 GB for > 4x speedup.
    
    Signed-off-by: Rich Ercolani <rincebrain@gmail.com>

--- module/zfs/zio_compress.c
+++ module/zfs/zio_compress.c
@@ -66,7 +66,7 @@ zio_compress_info_t zio_compress_table[ZIO_COMPRESS_FUNCTIONS] = {
 	{"gzip-9",	9,	gzip_compress,	gzip_decompress, NULL},
 	{"zle",		64,	zle_compress,	zle_decompress, NULL},
 	{"lz4",		0,	lz4_compress_zfs, lz4_decompress_zfs, NULL},
-	{"zstd",	ZIO_ZSTD_LEVEL_DEFAULT,	zfs_zstd_compress,
+	{"zstd",	ZIO_ZSTD_LEVEL_DEFAULT,	zfs_zstd_compress_wrap,
 	    zfs_zstd_decompress, zfs_zstd_decompress_level},
 };
 
