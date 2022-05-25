22b22a03f44d8531395db8a73cf2850eeee97d28
Alexander Motin <mav@FreeBSD.org>
2022-05-25 13:12:52 -0400

    More speculative prefetcher improvements
    - Make prefetch distance adaptive: up to 4MB prefetch doubles for
    every, hit same as before, but after that it grows by 1/8 every time
    the prefetch read does not complete in time to satisfy the demand.
    My tests show that 4MB is sufficient for wide NVMe pool to saturate
    single reader thread at 2.5GB/s, while new 64MB maximum allows the
    same thread to reach 1.5GB/s on wide HDD pool.  Further distance
    increase may increase speed even more, but less dramatic and with
    higher latency.
    
     - Allow early reuse of inactive prefetch streams: streams that never
    saw hits can be reused immediately if there is a demand, while others
    can be reused after 1s of inactivity, starting with the oldest.  After
    2s of inactivity streams are deleted to free resources same as before.
    This allows by several times increase strided read performance on HDD
    pool in presence of simultaneous random reads, previously filling the
    zfetch_max_streams limit for seconds and so blocking most of prefetch.
    
     - Always issue intermediate indirect block reads with SYNC priority.
    Each of those reads if delayed for longer may delay up to 1024 other
    block prefetches, that may be not good for wide pools.
    
    Reviewed-by: Allan Jude <allan@klarasystems.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Alexander Motin <mav@FreeBSD.org>
    Sponsored-By: iXsystems, Inc.
    Closes #13452

--- include/sys/dmu_zfetch.h
+++ include/sys/dmu_zfetch.h
@@ -49,20 +49,18 @@ typedef struct zfetch {
 
 typedef struct zstream {
 	uint64_t	zs_blkid;	/* expect next access at this blkid */
-	uint64_t	zs_pf_blkid1;	/* first block to prefetch */
-	uint64_t	zs_pf_blkid;	/* block to prefetch up to */
-
-	/*
-	 * We will next prefetch the L1 indirect block of this level-0
-	 * block id.
-	 */
-	uint64_t	zs_ipf_blkid1;	/* first block to prefetch */
-	uint64_t	zs_ipf_blkid;	/* block to prefetch up to */
+	unsigned int	zs_pf_dist;	/* data prefetch distance in bytes */
+	unsigned int	zs_ipf_dist;	/* L1 prefetch distance in bytes */
+	uint64_t	zs_pf_start;	/* first data block to prefetch */
+	uint64_t	zs_pf_end;	/* data block to prefetch up to */
+	uint64_t	zs_ipf_start;	/* first data block to prefetch L1 */
+	uint64_t	zs_ipf_end;	/* data block to prefetch L1 up to */
 
 	list_node_t	zs_node;	/* link for zf_stream */
 	hrtime_t	zs_atime;	/* time last prefetch issued */
 	zfetch_t	*zs_fetch;	/* parent fetch */
 	boolean_t	zs_missed;	/* stream saw cache misses */
+	boolean_t	zs_more;	/* need more distant prefetch */
 	zfs_refcount_t	zs_callers;	/* number of pending callers */
 	/*
 	 * Number of stream references: dnode, callers and pending blocks.
