e3fa430bc622f08a6dc00ed111bae3c57315b41a
Ameer Hamza <ahamza@ixsystems.com>
2022-07-19 21:31:41 +0500

    Add support for per dataset zil stats and use wmsum counters
    Zil kstats are reported in an inclusive way, i.e., same counters are
    shared to capture all the activities happening in zil. Added support
    to report zil stats for every datset individually by combining them
    with already exposed dataset kstats.
    
    Wmsum uses per cpu counters and provide less overhead as compared
    to atomic operations. Updated zil kstats to replace wmsum counters
    to avoid atomic operations.
    
    Signed-off-by: Ameer Hamza <ahamza@ixsystems.com>

--- module/os/freebsd/zfs/zvol_os.c
+++ module/os/freebsd/zfs/zvol_os.c
@@ -1189,7 +1189,7 @@ zvol_ensure_zilog(zvol_state_t *zv)
 		}
 		if (zv->zv_zilog == NULL) {
 			zv->zv_zilog = zil_open(zv->zv_objset,
-			    zvol_get_data);
+			    zvol_get_data, &zv->zv_kstat.dk_zil_sums);
 			zv->zv_flags |= ZVOL_WRITTEN_TO;
 			/* replay / destroy done in zvol_os_create_minor() */
 			VERIFY0(zv->zv_zilog->zl_header->zh_flags &
@@ -1422,8 +1422,12 @@ zvol_os_create_minor(const char *name)
 	zv->zv_volsize = volsize;
 	zv->zv_objset = os;
 
+	ASSERT3P(zv->zv_kstat.dk_kstats, ==, NULL);
+	error = dataset_kstats_create(&zv->zv_kstat, zv->zv_objset);
+	if (error)
+		goto out_dmu_objset_disown;
 	ASSERT3P(zv->zv_zilog, ==, NULL);
-	zv->zv_zilog = zil_open(os, zvol_get_data);
+	zv->zv_zilog = zil_open(os, zvol_get_data, &zv->zv_kstat.dk_zil_sums);
 	if (spa_writeable(dmu_objset_spa(os))) {
 		if (zil_replay_disable)
 			zil_destroy(zv->zv_zilog, B_FALSE);
@@ -1432,8 +1436,6 @@ zvol_os_create_minor(const char *name)
 	}
 	zil_close(zv->zv_zilog);
 	zv->zv_zilog = NULL;
-	ASSERT3P(zv->zv_kstat.dk_kstats, ==, NULL);
-	dataset_kstats_create(&zv->zv_kstat, zv->zv_objset);
 
 	/* TODO: prefetch for geom tasting */
 
