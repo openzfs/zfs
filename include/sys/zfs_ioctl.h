2592dad4d17d4863ac25341da200e0131ab849d4
Christian Schwarz <christian.schwarz@nutanix.com>
2022-08-24 17:04:19 +0000

    DMU_BACKUP_FEATURE: indicate that bit 28 and 29 are reserved
    Bit 28 is used by an internal Nutanix feature which might be upstreamed in the future.
    
    Bit 29 is the last unused bit. It is reserved to indicate a to-be-designed
    extension to the stream format which will accomodate more feature flags.
    
    refs https://github.com/openzfs/zfs/issues/13795
    
    Signed-off-by: Christian Schwarz <christian.schwarz@nutanix.com>

--- include/sys/zfs_ioctl.h
+++ include/sys/zfs_ioctl.h
@@ -124,6 +124,13 @@ typedef enum drr_headertype {
  * default use of "zfs send" won't encounter the bug mentioned above.
  */
 #define	DMU_BACKUP_FEATURE_SWITCH_TO_LARGE_BLOCKS (1 << 27)
+/* flag #28 is reserved for a Nutanix feature */
+/*
+ * flag #29 is the last unused bit. It is reserved to indicate a to-be-designed
+ * extension to the stream format which will accomodate more feature flags.
+ * If you need to add another feature flag, please reach out to the OpenZFS
+ * community, e.g., on GitHub or Slack.
+ */
 
 /*
  * Mask of all supported backup features
