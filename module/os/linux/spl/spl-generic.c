bf4c1af9454d6f9049c382e6a5b0baace5a8ecd9
Richard Yao <richard.yao@alumni.stonybrook.edu>
2022-09-05 23:19:31 -0400

    Linux SPL: __alloc_percpu() can fail
    Upon inspection of our code, I noticed that we assume that
    __alloc_percpu() cannot fail, and while it probably never has failed in
    practice, technically, it can fail, so we should handle that.
    
    Signed-off-by: Richard Yao <richard.yao@alumni.stonybrook.edu>

--- module/os/linux/spl/spl-generic.c
+++ module/os/linux/spl/spl-generic.c
@@ -705,7 +705,7 @@ spl_kvmem_init(void)
  * initialize each of the per-cpu seeds so that the sequences generated on each
  * CPU are guaranteed to never overlap in practice.
  */
-static void __init
+static int __init
 spl_random_init(void)
 {
 	uint64_t s[2];
@@ -713,6 +713,9 @@ spl_random_init(void)
 
 	spl_pseudo_entropy = __alloc_percpu(2 * sizeof (uint64_t),
 	    sizeof (uint64_t));
+
+	if (!spl_pseudo_entropy)
+		return (-ENOMEM);
 
 	get_random_bytes(s, sizeof (s));
 
@@ -757,7 +760,8 @@ spl_init(void)
 {
 	int rc = 0;
 
-	spl_random_init();
+	if ((rc = spl_random_init())
+		goto out0;
 
 	if ((rc = spl_kvmem_init()))
 		goto out1;
@@ -800,6 +804,8 @@ out3:
 out2:
 	spl_kvmem_fini();
 out1:
+	spl_random_fini();
+out0:
 	return (rc);
 }
 
