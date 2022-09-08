dff541f698d616ed9f9b1ad3afa44e450efdad7a
pkubaj <pkubaj@anongoth.pl>
2022-09-08 17:27:25 +0000

    Fix build on FreeBSD/powerpc64*
    There's no VSX handler on FreeBSD for now.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Piotr Kubaj <pkubaj@FreeBSD.org>
    Closes #13848

--- module/icp/algs/blake3/blake3_x86-64.c
+++ module/icp/algs/blake3/blake3_x86-64.c
@@ -74,7 +74,7 @@ static boolean_t blake3_is_sse2_supported(void)
 {
 #if defined(__x86_64)
 	return (kfpu_allowed() && zfs_sse2_available());
-#elif defined(__PPC64__)
+#elif defined(__PPC64__) && defined(__linux__)
 	return (kfpu_allowed() && zfs_vsx_available());
 #else
 	return (kfpu_allowed());
@@ -140,7 +140,7 @@ static boolean_t blake3_is_sse41_supported(void)
 {
 #if defined(__x86_64)
 	return (kfpu_allowed() && zfs_sse4_1_available());
-#elif defined(__PPC64__)
+#elif defined(__PPC64__) && defined(__linux__)
 	return (kfpu_allowed() && zfs_vsx_available());
 #else
 	return (kfpu_allowed());
