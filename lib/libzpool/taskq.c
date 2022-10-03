d62bafee9fc38f8b9653c531e1861e83dcba4618
Richard Yao <richard.yao@alumni.stonybrook.edu>
2022-10-03 16:41:58 -0400

    Fix memory leak found by GCC static analyzer
    GCC 12.1.1_p20220625's -fanalyzer found and reported this.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Damian Szuberski <szuberskidamian@gmail.com>
    Signed-off-by: Richard Yao <richard.yao@alumni.stonybrook.edu>
    Closes #13975

--- lib/libzpool/taskq.c
+++ lib/libzpool/taskq.c
@@ -319,7 +319,9 @@ taskq_destroy(taskq_t *tq)
 	tq->tq_minalloc = 0;
 	while (tq->tq_nalloc != 0) {
 		ASSERT(tq->tq_freelist != NULL);
-		task_free(tq, task_alloc(tq, KM_SLEEP));
+		taskq_ent_t *tqent_nexttq = tq->tq_freelist->tqent_next;
+		task_free(tq, tq->tq_freelist);
+		tq->tq_freelist = tqent_nexttq;
 	}
 
 	mutex_exit(&tq->tq_lock);
