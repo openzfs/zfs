ee93cbc9d477d8ce0a6ebffe9a582dbe83144577
Alexander Lobakin <alobakin@pm.me>
2022-10-16 16:53:22 +0200

    icp: properly fix all RETs in x86_64 Asm code
    Commit 43569ee37420 ("Fix objtool: missing int3 after ret warning")
    addressed replacing all `ret`s in x86 asm code to a macro in the
    Linux kernel in order to enable SLS. That was done by copying the
    upstream macro definitions and fixed objtool complaints.
    Since then, several more mitigations were introduced, including
    Rethunk. It requires to have a jump to one of the thunks in order
    to work, so the RET macro was changed again. And, as ZFS code
    didn't use the mainline defition, but copied it, this is currently
    missing.
    
    Objtool reminds about it time to time (Clang 16, CONFIG_RETHUNK=y):
    
    fs/zfs/lua/zlua.o: warning: objtool: setjmp+0x25: 'naked' return
     found in RETHUNK build
    fs/zfs/lua/zlua.o: warning: objtool: longjmp+0x27: 'naked' return
     found in RETHUNK build
    
    Do it the following way:
    * if we're building under Linux, unconditionally include
      <linux/linkage.h> in the related files. It is available in x86
      sources since even pre-2.6 times, so doesn't need any conftests;
    * then, if RET macro is available, it will be used directly, so that
      we will always have the version actual to the kernel we build;
    * if there's no such macro, we define it as a simple `ret`, as it
      was on pre-SLS times.
    
    This ensures we always have the up-to-date definition with no need
    to update it manually, and at the same time is safe for the whole
    variety of kernels ZFS module supports.
    Then, there's a couple more "naked" rets left in the code, they're
    just defined as:
    
    	.byte 0xf3,0xc3
    
    In fact, this is just:
    
    	rep ret
    
    `rep ret` instead of just `ret` seems to mitigate performance issues
    on some old AMD processors and most likely makes no sense as of
    today.
    Anyways, address those rets, so that they will be protected with
    Rethunk and SLS. Include <sys/asm_linkage.h> here which now always
    has RET definition and replace those constructs with just RET.
    This wipes the last couple of places with unpatched rets objtool's
    been complaining about.
    
    Reviewed-by: Attila Fülöp <attila@fueloep.org>
    Reviewed-by: Tino Reichardt <milky-zfs@mcmilk.de>
    Reviewed-by: Richard Yao <richard.yao@alumni.stonybrook.edu>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Alexander Lobakin <alobakin@pm.me>
    Closes #14035

--- module/icp/include/sys/ia32/asm_linkage.h
+++ module/icp/include/sys/ia32/asm_linkage.h
@@ -30,9 +30,11 @@
 #include <sys/stack.h>
 #include <sys/trap.h>
 
-#if defined(__linux__) && defined(CONFIG_SLS)
-#define	RET	ret; int3
-#else
+#if defined(_KERNEL) && defined(__linux__)
+#include <linux/linkage.h>
+#endif
+
+#ifndef RET
 #define	RET	ret
 #endif
 
@@ -204,6 +206,7 @@ sym1	= sym2
  * insert the calls to mcount for profiling. ENTRY_NP is identical, but
  * never calls mcount.
  */
+#undef ENTRY
 #define	ENTRY(x) \
 	.text; \
 	.align	ASM_ENTRY_ALIGN; \
