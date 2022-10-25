a446ffaf6ea256463626b4b5b62fddbf8a495a4f
Paul Dagnelie <pcd@delphix.com>
2022-10-25 15:20:50 -0700

    Add ability to recompress send streams with new compression algorithm
    Signed-off-by: Paul Dagnelie <pcd@delphix.com>

--- cmd/zstream/zstream.h
+++ cmd/zstream/zstream.h
@@ -30,6 +30,7 @@ extern void *safe_malloc(size_t size);
 extern int zstream_do_redup(int, char *[]);
 extern int zstream_do_dump(int, char *[]);
 extern int zstream_do_decompress(int argc, char *argv[]);
+extern int zstream_do_recompress(int argc, char *argv[]);
 extern int zstream_do_token(int, char *[]);
 extern void zstream_usage(void);
 
