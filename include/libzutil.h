aac1f9ff432a4781ae2d7bd86d5fd31a056be289
Ethan Coe-Renner <coerenner1@llnl.gov>
2022-12-22 13:32:01 -0800

    Colorize zpool iostat output
    - Bold header
    - Color space suffixes in iostat by order of magnitude
        - K is green
        - M is yellow
        - G is red
        - T is blue
        - P is magenta
        - E is cyan
    - 0 space is colored gray
    
    Signed-off-by: Ethan Coe-Renner <coerenner1@llnl.gov>

--- include/libzutil.h
+++ include/libzutil.h
@@ -173,6 +173,10 @@ struct zfs_cmd;
 #define	ANSI_GREEN	"\033[0;32m"
 #define	ANSI_YELLOW	"\033[0;33m"
 #define	ANSI_BLUE	"\033[0;34m"
+#define	ANSI_MAGENTA	"\033[0;34m"
+#define	ANSI_CYAN	"\033[0;34m"
+
+#define	ANSI_GRAY	"\033[0;37m"
 #define	ANSI_RESET	"\033[0m"
 #define	ANSI_BOLD	"\033[1m"
 
