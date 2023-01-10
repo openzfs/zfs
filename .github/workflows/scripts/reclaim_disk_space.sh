abdbdf81ea0a1cd0c3ffb1274c0c7fa11af9fe80
Brian Behlendorf <behlendorf1@llnl.gov>
2023-01-10 14:29:52 -0800

    CI: remove unused packages/snaps
    Removing portions of packages/snaps directly with rm can result in
    unexpected errors when running `apt update`.  Free up the additional
    space by removing (most) packages with the proper tools.
    
    This change frees up slightly less space than before, but it is
    expected to still be sufficient.
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

--- .github/workflows/scripts/reclaim_disk_space.sh
+++ .github/workflows/scripts/reclaim_disk_space.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/sh -x
 
 set -eu
 
 # remove 4GiB of images
 sudo systemd-run docker system prune --force --all --volumes
+
+# remove unused packages
+sudo apt remove -q --purge firefox
 
 # remove unused software
 sudo systemd-run rm -rf \
@@ -11,10 +14,6 @@ sudo systemd-run rm -rf \
   /opt/* \
   /usr/local/* \
   /usr/share/az* \
-  /usr/share/dotnet \
   /usr/share/gradle* \
   /usr/share/miniconda \
-  /usr/share/swift \
-  /var/lib/gems \
-  /var/lib/mysql \
-  /var/lib/snapd
+  /usr/share/swift
