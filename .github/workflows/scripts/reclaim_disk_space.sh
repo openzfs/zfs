4f6e0ca117aa596d74e9ff133fe4b780a9f313ee
Brian Behlendorf <behlendorf1@llnl.gov>
2023-03-02 16:18:27 -0800

    Update workflows
    Update the GitHub actions workflows using a subset of the changes
    from the master branch, commit 620a977f22.  Cherry-picking each
    relevant commit would have resulted in a large number of conflicts
    so this change only applies a minimal set of useful updates.
    
    - Added build-dependencies.txt and checkstyle-dependencies.txt
    - Added reclaim_disk_space.sh script
    - Minor changes to build steps
    - Reduced ztest run time
    - checkbashisms, mandoc, and cppcheck were not included to
      avoid additional backports
    - Add exceptions for shellcheck
    
    Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>

---
+++ .github/workflows/scripts/reclaim_disk_space.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eu
+
+# remove 4GiB of images
+sudo systemd-run docker system prune --force --all --volumes
+
+# remove unused software
+sudo systemd-run --wait rm -rf \
+  "$AGENT_TOOLSDIRECTORY" \
+  /opt/* \
+  /usr/local/* \
+  /usr/share/az* \
+  /usr/share/dotnet \
+  /usr/share/gradle* \
+  /usr/share/miniconda \
+  /usr/share/swift \
+  /var/lib/gems \
+  /var/lib/mysql \
+  /var/lib/snapd
+
+# trim the cleaned space
+sudo fstrim /
