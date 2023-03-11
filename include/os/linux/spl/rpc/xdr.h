47b994049fd105f54a887e19ec0669e9084b7cf2
Richard Yao <richard.yao@alumni.stonybrook.edu>
2023-03-11 13:17:59 -0500

    Silence clang static analyzer warnings about stored stack addresses
    Clang's static analyzer complains that nvs_xdr() and nvs_native()
    functions return pointers to stack memory. That is technically true, but
    the pointers are stored in stack memory from the caller's stack frame,
    are not read by the caller and are deallocated when the caller returns,
    so this is harmless. We set the pointers to NULL to silence the
    warnings.
    
    Reviewed-by: Tino Reichardt <milky-zfs@mcmilk.de>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Richard Yao <richard.yao@alumni.stonybrook.edu>
    Closes #14612

--- include/os/linux/spl/rpc/xdr.h
+++ include/os/linux/spl/rpc/xdr.h
@@ -75,9 +75,6 @@ struct xdr_bytesrec {
 void xdrmem_create(XDR *xdrs, const caddr_t addr, const uint_t size,
     const enum xdr_op op);
 
-/* Currently not needed. If needed later, we'll add it to struct xdr_ops */
-#define	xdr_destroy(xdrs) ((void) 0)
-
 #define	xdr_control(xdrs, req, info) \
 	(xdrs)->x_ops->xdr_control((xdrs), (req), (info))
 
