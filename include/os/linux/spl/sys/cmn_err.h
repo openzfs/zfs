bfce569f014ae6f74f8e75136f2e3d6d7e69f733
Attila Fülöp <attila@fueloep.org>
2023-03-15 00:34:11 +0100

    spl: cmn_err_once() should be usable in brace-less if else statements
    Commit 11913870 (#14567) added cmn_err_once() by #define'ing a
    compound statement but failed to consider usage in a single
    statement brace-less if else.
    
    Fix the problem by using the common "do {} while (0)" construct.
    
    Signed-off-by: Attila Fülöp <attila@fueloep.org>

--- include/os/linux/spl/sys/cmn_err.h
+++ include/os/linux/spl/sys/cmn_err.h
@@ -47,19 +47,19 @@ extern void vpanic(const char *, va_list)
 #define	fm_panic	panic
 
 #define	cmn_err_once(ce, ...)				\
-{							\
+do {							\
 	static volatile uint32_t printed = 0;		\
 	if (atomic_cas_32(&printed, 0, 1) == 0) {	\
 		cmn_err(ce, __VA_ARGS__);		\
 	}						\
-}
+} while (0)
 
 #define	vcmn_err_once(ce, fmt, ap)			\
-{							\
+do {							\
 	static volatile uint32_t printed = 0;		\
 	if (atomic_cas_32(&printed, 0, 1) == 0) {	\
 		vcmn_err(ce, fmt, ap);			\
 	}						\
-}
+} while (0)
 
 #endif /* SPL_CMN_ERR_H */
