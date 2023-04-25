6d59d5df9808902a3cb6064605c753ec2ab8d2d7
Han Gao <rabenda.cn@gmail.com>
2023-04-26 07:05:45 +0800

    Add loongarch64 support
    Add loongarch64 definitions & lua module setjmp asm
    
    LoongArch is a new RISC ISA, which is a bit like MIPS or RISC-V.
    
    Reviewed-by: Richard Yao <richard.yao@alumni.stonybrook.edu>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Han Gao <gaohan@uniontech.com>
    Signed-off-by: WANG Xuerui <xen0n@gentoo.org>
    Closes #13422

--- module/lua/ldo.c
+++ module/lua/ldo.c
@@ -84,6 +84,8 @@ static intptr_t stack_remaining(void) {
 #define JMP_BUF_CNT	18
 #elif defined(__riscv)
 #define JMP_BUF_CNT     64
+#elif defined(__loongarch_lp64)
+#define JMP_BUF_CNT     64
 #else
 #define	JMP_BUF_CNT	1
 #endif
