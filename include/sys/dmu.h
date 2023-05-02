d0d91f185efd9149d8faceb89a9a0e5e54093fc8
Pawel Jakub Dawidek <pawel@dawidek.net>
2023-05-02 15:46:14 -0700

    Don't use dmu_buf_is_dirty() for unassigned transaction.
    The dmu_buf_is_dirty() call doesn't make sense here for two reasons:
    1. txg is 0 for unassigned tx, so it was a no-op.
    2. It is equivalent of checking if we have dirty records and we are doing
       this few lines earlier.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Pawel Jakub Dawidek <pawel@dawidek.net>
    Closes #14825

--- include/sys/dmu.h
+++ include/sys/dmu.h
@@ -1065,7 +1065,7 @@ int dmu_offset_next(objset_t *os, uint64_t object, boolean_t hole,
     uint64_t *off);
 
 int dmu_read_l0_bps(objset_t *os, uint64_t object, uint64_t offset,
-    uint64_t length, dmu_tx_t *tx, struct blkptr *bps, size_t *nbpsp);
+    uint64_t length, struct blkptr *bps, size_t *nbpsp);
 int dmu_brt_clone(objset_t *os, uint64_t object, uint64_t offset,
     uint64_t length, dmu_tx_t *tx, const struct blkptr *bps, size_t nbps,
     boolean_t replay);
