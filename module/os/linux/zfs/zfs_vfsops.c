2768dc04cc45b2506a5f2f3b7c5da17eb42ec3c3
Rob Norris <rob.norris@klarasystems.com>
2023-06-25 20:50:19 +1000

    linux: implement filesystem-side copy/clone functions for EL7
    Redhat have backported copy_file_range and clone_file_range to the EL7
    kernel using an "extended file operations" wrapper structure. This
    connects all that up to let cloning work there too.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Kay Pedersen <mail@mkwg.de>
    Signed-off-by: Rob Norris <rob.norris@klarasystems.com>
    Sponsored-By: OpenDrives Inc.
    Sponsored-By: Klara Inc.
    Closes #15050

--- module/os/linux/zfs/zfs_vfsops.c
+++ module/os/linux/zfs/zfs_vfsops.c
@@ -2092,6 +2092,9 @@ zfs_init(void)
 	zfs_znode_init();
 	dmu_objset_register_type(DMU_OST_ZFS, zpl_get_file_info);
 	register_filesystem(&zpl_fs_type);
+#ifdef HAVE_VFS_FILE_OPERATIONS_EXTEND
+	register_fo_extend(&zpl_file_operations);
+#endif
 }
 
 void
@@ -2102,6 +2105,9 @@ zfs_fini(void)
 	 */
 	taskq_wait(system_delay_taskq);
 	taskq_wait(system_taskq);
+#ifdef HAVE_VFS_FILE_OPERATIONS_EXTEND
+	unregister_fo_extend(&zpl_file_operations);
+#endif
 	unregister_filesystem(&zpl_fs_type);
 	zfs_znode_fini();
 	zfsctl_fini();
