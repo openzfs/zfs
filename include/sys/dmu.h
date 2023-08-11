65488a0b88c72bf8d44b50171408a34dac65ea3d
Alexander Motin <mav@FreeBSD.org>
2023-08-11 12:04:08 -0400

    ZIL: Avoid dbuf_read() before dmu_sync().
    In most cases dmu_sync() works with dirty records directly and does
    not need actual data. The only exception is dmu_sync_late_arrival().
    To save some CPU time use dmu_buf_hold_noread*() in z*_get_data()
    and explicitly call dbuf_read() in dmu_sync_late_arrival(). There
    is also a chance that by that time TXG will already be synced and
    we won't have to do it at all.
    
    Reviewed-by: Brian Atkinson <batkinson@lanl.gov>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by:	Alexander Motin <mav@FreeBSD.org>
    Sponsored by:	iXsystems, Inc.
    Closes #15153

--- include/sys/dmu.h
+++ include/sys/dmu.h
@@ -572,11 +572,15 @@ int dmu_buf_hold(objset_t *os, uint64_t object, uint64_t offset,
 int dmu_buf_hold_array(objset_t *os, uint64_t object, uint64_t offset,
     uint64_t length, int read, const void *tag, int *numbufsp,
     dmu_buf_t ***dbpp);
+int dmu_buf_hold_noread(objset_t *os, uint64_t object, uint64_t offset,
+    const void *tag, dmu_buf_t **dbp);
 int dmu_buf_hold_by_dnode(dnode_t *dn, uint64_t offset,
     const void *tag, dmu_buf_t **dbp, int flags);
 int dmu_buf_hold_array_by_dnode(dnode_t *dn, uint64_t offset,
     uint64_t length, boolean_t read, const void *tag, int *numbufsp,
     dmu_buf_t ***dbpp, uint32_t flags);
+int dmu_buf_hold_noread_by_dnode(dnode_t *dn, uint64_t offset, const void *tag,
+    dmu_buf_t **dbp);
 /*
  * Add a reference to a dmu buffer that has already been held via
  * dmu_buf_hold() in the current context.
