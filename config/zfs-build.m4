openzfs/zfs

commit 612b8dff5bc3d827efb864a199a62bda1a419254

contrib: bash_completion.d: install, fix dist
dist diff:
  -zfs-2.1.99/contrib/bash_completion.d/zfs

install diff:
  +destdir/usr/local/etc/bash_completion.d
  +destdir/usr/local/etc/bash_completion.d/zfs

Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
Closes #13316

contrib/bash_completion.d/Makefile.am
@@ -1,6 +1,7 @@
-noinst_DATA  = %D%/zfs
-EXTRA_DIST  += $(noinst_DATA)
-SUBSTFILES  += $(noinst_DATA)
+bashcompletiondir = $(sysconfdir)/bash_completion.d
 
-SHELLCHECKSCRIPTS += $(noinst_DATA)
-$(call SHELLCHECK_OPTS,$(noinst_DATA)): SHELLCHECK_SHELL = bash
+nodist_bashcompletion_DATA  = %D%/zfs
+SUBSTFILES                 += $(nodist_bashcompletion_DATA)
+
+SHELLCHECKSCRIPTS   += $(nodist_bashcompletion_DATA)
+$(call SHELLCHECK_OPTS,$(nodist_bashcompletion_DATA)): SHELLCHECK_SHELL = bash

rpm/generic/zfs.spec.in
@@ -518,6 +518,8 @@ systemctl --system daemon-reload >/dev/null || true
 %config(noreplace) %{_sysconfdir}/%{name}/vdev_id.conf.*.example
 %attr(440, root, root) %config(noreplace) %{_sysconfdir}/sudoers.d/*
 
+%config(noreplace) %{_sysconfdir}/bash_completion.d/zfs
+
 %files -n libzpool5
 %{_libdir}/libzpool.so.*
 

commit e98fc2fdd1414089961b0b7743628a82f43097b0

contrib: bash_completion.d: make install destination vendor dependent
Certain Linux distributions (Debian/Ubuntu at least) expect
bash-completion snippets to be installed in
/usr/share/bash-completion/completions instead of
/etc/bash_completion.d.

This patch sets the bashcompletiondir variable based on the vendor,
inspired by similar settings for initdir and initconfdir.

It seems that commit 612b8dff5bc3d827efb864a199a62bda1a419254
caused the file to be installed in the first-place (thus the error
when building debian packages only became apparent when testing a
2.2.0-rc4 build)

The change only sets the variable in Makefile context - the
rpm/zfs.spec.in file has the path hardcoded as
%{_sysconfdir}/bash_completion.d/zfs, but since running
```
./configure --sysconfdir=/myetc  ; make rpm
```
also results in all relevant files to be installed in /etc instead of
/myetc I assume this can remain as is.

Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

config/zfs-build.m4
@@ -617,6 +617,17 @@ AC_DEFUN([ZFS_AC_DEFAULT_PACKAGE], [
 		AC_MSG_RESULT([no])
 	fi
 	AC_SUBST(RPM_DEFINE_INITRAMFS)
+
+	AC_MSG_CHECKING([default bash completion directory])
+	case "$VENDOR" in
+		ubuntu)     bashcompletiondir=/usr/share/bash-completion/completions   ;;
+		debian)     bashcompletiondir=/usr/share/bash-completion/completions   ;;
+		freebsd)    bashcompletiondir=$sysconfdir/bash_completion.d;;
+		*)          bashcompletiondir=/etc/bash_completion.d   ;;
+	esac
+	AC_MSG_RESULT([$bashcompletiondir])
+	AC_SUBST(bashcompletiondir)
+
 ])
 
 dnl #

contrib/bash_completion.d/Makefile.am
@@ -1,5 +1,3 @@
-bashcompletiondir = $(sysconfdir)/bash_completion.d
-
 nodist_bashcompletion_DATA  = %D%/zfs
 SUBSTFILES                 += $(nodist_bashcompletion_DATA)
 