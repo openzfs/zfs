8205d5a66b5a4f42b02ee7c4d0d0ebeeaba35090
Rob Norris <rob.norris@klarasystems.com>
2023-09-29 17:03:40 +1000

    zpool: add condense verb and wire up to log-spacemap flush
    The idea is to have a single command that could signal to any background
    cleanup task that it should do its work faster, or care less about not
    getting in the way of user IO, or whatever.
    
    This adds the the `zpool condense` command, the `ZFS_IOC_POOL_CONDENSE`
    ioctl and counters so userspace can get progress. Because the target
    could be anything, there's no particular unit, just a total number of
    items to condense and count of how many done.
    
    It also adds a `log-spacemap` condense target, which calls the "request
    log flush" function.
    
    Sponsored-by: Klara, Inc.
    Sponsored-by: Wasabi Technology, Inc.
    Signed-off-by: Rob Norris <rob.norris@klarasystems.com>

--- include/libzfs.h
+++ include/libzfs.h
@@ -29,6 +29,7 @@
  * Copyright (c) 2017 Open-E, Inc. All Rights Reserved.
  * Copyright (c) 2019 Datto Inc.
  * Copyright (c) 2021, Colm Buckley <colm@tuatha.org>
+ * Copyright (c) 2024, Klara, Inc.
  */
 
 #ifndef	_LIBZFS_H
@@ -297,6 +298,8 @@ _LIBZFS_H int zpool_initialize_wait(zpool_handle_t *, pool_initialize_func_t,
     nvlist_t *);
 _LIBZFS_H int zpool_trim(zpool_handle_t *, pool_trim_func_t, nvlist_t *,
     trimflags_t *);
+_LIBZFS_H int zpool_condense(zpool_handle_t *, pool_condense_func_t,
+    pool_condense_type_t);
 
 _LIBZFS_H int zpool_clear(zpool_handle_t *, const char *, nvlist_t *);
 _LIBZFS_H int zpool_reguid(zpool_handle_t *);
