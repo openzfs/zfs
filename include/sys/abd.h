05a7348a7e7e0be7ae5e729fdc98cb77416d30b8
Alexander Motin <mav@FreeBSD.org>
2023-10-30 17:54:27 -0400

    RAIDZ: Use cache blocking during parity math
    RAIDZ parity is calculated by adding data one column at a time.  It
    works OK for small blocks, but for large blocks results of previous
    addition may already be evicted from CPU caches to main memory, and
    in addition to extra memory write require extra read to get it back.
    
    This patch splits large parity operations into 64KB chunks, that
    should in most cases fit into CPU L2 caches from the last decade.
    I haven't touched more complicated cases of data reconstruction to
    not over complicate the code.  Those should be relatively rare.
    
    My tests on Xeon Gold 6242R CPU with 1MB of L2 cache per core show
    up to 10/20% memory traffic reduction when writing to 4-wide RAIDZ/
    RAIDZ2 blocks of ~4MB and up.  Older CPUs with 256KB of L2 cache
    should see the effect even on smaller blocks.  Wider vdevs may need
    bigger blocks to be affected.
    
    Reviewed-by: Brian Atkinson <batkinson@lanl.gov>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by:	Alexander Motin <mav@FreeBSD.org>
    Sponsored by:	iXsystems, Inc.
    Closes #15448

--- include/sys/abd.h
+++ include/sys/abd.h
@@ -133,11 +133,11 @@ int abd_cmp_buf_off(abd_t *, const void *, size_t, size_t);
 void abd_zero_off(abd_t *, size_t, size_t);
 void abd_verify(abd_t *);
 
-void abd_raidz_gen_iterate(abd_t **cabds, abd_t *dabd,
-	ssize_t csize, ssize_t dsize, const unsigned parity,
+void abd_raidz_gen_iterate(abd_t **cabds, abd_t *dabd, size_t off,
+	size_t csize, size_t dsize, const unsigned parity,
 	void (*func_raidz_gen)(void **, const void *, size_t, size_t));
 void abd_raidz_rec_iterate(abd_t **cabds, abd_t **tabds,
-	ssize_t tsize, const unsigned parity,
+	size_t tsize, const unsigned parity,
 	void (*func_raidz_rec)(void **t, const size_t tsize, void **c,
 	const unsigned *mul),
 	const unsigned *mul);
