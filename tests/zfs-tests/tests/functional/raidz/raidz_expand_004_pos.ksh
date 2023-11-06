don-brady/openzfs

raidz expansion feature
This feature allows disks to be added one at a time to a RAID-Z group,
expanding its capacity incrementally.  This feature is especially useful
for small pools (typically with only one RAID-Z group), where there
isn't sufficient hardware to add capacity by adding a whole new RAID-Z
group (typically doubling the number of disks).

== Initiating expansion ==

A new device (disk) can be attached to an existing RAIDZ vdev, by
running `zpool attach POOL raidzP-N NEW_DEVICE`, e.g. `zpool attach tank
raidz2-0 sda`.  The new device will become part of the RAIDZ group.  A
"raidz expansion" will be initiated, and the new device will contribute
additional space to the RAIDZ group once the expansion completes.

The `feature@raidz_expansion` on-disk feature flag must be `enabled` to
initiate an expansion, and it remains `active` for the life of the pool.
In other words, pools with expanded RAIDZ vdevs can not be imported by
older releases of the ZFS software.

== During expansion ==

The expansion entails reading all allocated space from existing disks in
the RAIDZ group, and rewriting it to the new disks in the RAIDZ group
(including the newly added device).

The expansion progress can be monitored with `zpool status`.

Data redundancy is maintained during (and after) the expansion.  If a
disk fails while the expansion is in progress, the expansion pauses
until the health of the RAIDZ vdev is restored (e.g. by replacing the
failed disk and waiting for reconstruction to complete).

The pool remains accessible during expansion.  Following a reboot or
export/import, the expansion resumes where it left off.

== After expansion ==

When the expansion completes, the additional space is available for use,
and is reflected in the `available` zfs property (as seen in `zfs list`,
`df`, etc).

Expansion does not change the number of failures that can be tolerated
without data loss (e.g. a RAIDZ2 is still a RAIDZ2 even after
expansion).

A RAIDZ vdev can be expanded

tests/zfs-tests/tests/functional/raidz/raidz_expand_004_pos.ksh
@@ -1,0 +1,119 @@
+#!/bin/ksh -p
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2021 by vStack. All rights reserved.
+#
+
+. $STF_SUITE/include/libtest.shlib
+
+#
+# DESCRIPTION:
+#	Check device replacement during raidz expansion.
+#
+# STRATEGY:
+#	1. Create block device files for the test raidz pool
+#	2. For each parity value [1..3]
+#	    - create raidz pool with minimum block device files required
+#	    - create couple of datasets with different recordsize and fill it
+#	    - attach new device to the pool
+#	    - offline and zero vdevs allowed by parity
+#	    - wait some time and start offlined vdevs replacement
+#	    - wait replacement completion and verify pool status
+
+typeset -r devs=10
+typeset -r dev_size_mb=128
+
+typeset -a disks
+
+embedded_slog_min_ms=$(get_tunable EMBEDDED_SLOG_MIN_MS)
+
+function cleanup
+{
+	poolexists "$TESTPOOL" && log_must_busy zpool destroy "$TESTPOOL"
+
+	for i in {0..$devs}; do
+		log_must rm -f "$TEST_BASE_DIR/dev-$i"
+	done
+
+	log_must set_tunable32 PREFETCH_DISABLE $embedded_slog_min_ms
+}
+
+log_onexit cleanup
+
+log_must set_tunable32 EMBEDDED_SLOG_MIN_MS 99999
+
+# Disk files which will be used by pool
+for i in {0..$(($devs))}; do
+	device=$TEST_BASE_DIR/dev-$i
+	log_must truncate -s ${dev_size_mb}M $device
+	disks[${#disks[*]}+1]=$device
+done
+
+for nparity in 1 2 3; do
+	raid=raidz$nparity
+	pool=$TESTPOOL
+	opts="-o cachefile=none"
+
+	log_must zpool create -f $opts $pool $raid ${disks[1..$(($nparity+1))]}
+
+	log_must zfs create -o recordsize=8k $pool/fs
+	log_must fill_fs /$pool/fs 1 128 100 1024 R
+
+	log_must zfs create -o recordsize=128k $pool/fs2
+	log_must fill_fs /$pool/fs2 1 128 100 1024 R
+
+	for disk in ${disks[$(($nparity+2))..$devs]}; do
+		log_must zpool attach $pool ${raid}-0 $disk
+
+		sleep 10
+
+		for (( i=1; i<=$nparity; i=i+1 )); do
+			log_must zpool offline $pool ${disks[$i]}
+			log_must dd if=/dev/zero of=${disks[$i]} \
+			    bs=1024k count=$dev_size_mb conv=notrunc
+		done
+
+		sleep 3
+
+		for (( i=1; i<=$nparity; i=i+1 )); do
+			log_must zpool replace $pool ${disks[$i]}
+		done
+
+		log_must zpool wait -t replace $pool
+		log_must check_pool_status $pool "scan" "with 0 errors"
+
+		log_must zpool wait -t raidz_expand $pool
+
+		log_must zpool clear $pool
+		log_must zpool scrub -w $pool
+
+		# XXX step sometimes FAILED
+		log_must zpool status -v
+		# log_must check_pool_status $pool "scan" "repaired 0B"
+	done
+
+	log_must zpool destroy "$pool"
+done
+
+log_pass "raidz expansion test succeeded."
+

openzfs/zfs

Sponsored-by: vStack

tests/zfs-tests/tests/functional/raidz/raidz_expand_004_pos.ksh
@@ -1,0 +1,121 @@
+#!/bin/ksh -p
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2021 by vStack. All rights reserved.
+#
+
+. $STF_SUITE/include/libtest.shlib
+
+#
+# DESCRIPTION:
+#	Check device replacement during raidz expansion.
+#
+# STRATEGY:
+#	1. Create block device files for the test raidz pool
+#	2. For each parity value [1..3]
+#	    - create raidz pool with minimum block device files required
+#	    - create couple of datasets with different recordsize and fill it
+#	    - attach new device to the pool
+#	    - offline and zero vdevs allowed by parity
+#	    - wait some time and start offlined vdevs replacement
+#	    - wait replacement completion and verify pool status
+
+typeset -r devs=10
+typeset -r dev_size_mb=128
+
+typeset -a disks
+
+embedded_slog_min_ms=$(get_tunable EMBEDDED_SLOG_MIN_MS)
+original_scrub_after_expand=$(get_tunable SCRUB_AFTER_EXPAND)
+
+function cleanup
+{
+	poolexists "$TESTPOOL" && log_must_busy zpool destroy "$TESTPOOL"
+
+	for i in {0..$devs}; do
+		log_must rm -f "$TEST_BASE_DIR/dev-$i"
+	done
+
+	log_must set_tunable32 EMBEDDED_SLOG_MIN_MS $embedded_slog_min_ms
+	log_must set_tunable32 SCRUB_AFTER_EXPAND $original_scrub_after_expand
+}
+
+log_onexit cleanup
+
+log_must set_tunable32 EMBEDDED_SLOG_MIN_MS 99999
+
+# Disk files which will be used by pool
+for i in {0..$(($devs))}; do
+	device=$TEST_BASE_DIR/dev-$i
+	log_must truncate -s ${dev_size_mb}M $device
+	disks[${#disks[*]}+1]=$device
+done
+
+nparity=$((RANDOM%(3) + 1))
+raid=raidz$nparity
+pool=$TESTPOOL
+opts="-o cachefile=none"
+
+log_must set_tunable32 SCRUB_AFTER_EXPAND 0
+
+log_must zpool create -f $opts $pool $raid ${disks[1..$(($nparity+1))]}
+
+log_must zfs create -o recordsize=8k $pool/fs
+log_must fill_fs /$pool/fs 1 128 100 1024 R
+
+log_must zfs create -o recordsize=128k $pool/fs2
+log_must fill_fs /$pool/fs2 1 128 100 1024 R
+
+for disk in ${disks[$(($nparity+2))..$devs]}; do
+	log_must zpool attach $pool ${raid}-0 $disk
+
+	sleep 10
+
+	for (( i=1; i<=$nparity; i=i+1 )); do
+		log_must zpool offline $pool ${disks[$i]}
+		log_must dd if=/dev/zero of=${disks[$i]} \
+		    bs=1024k count=$dev_size_mb conv=notrunc
+	done
+
+	sleep 3
+
+	for (( i=1; i<=$nparity; i=i+1 )); do
+		log_must zpool replace $pool ${disks[$i]}
+	done
+
+	log_must zpool wait -t replace $pool
+	log_must check_pool_status $pool "scan" "with 0 errors"
+
+	log_must zpool wait -t raidz_expand $pool
+
+	log_must zpool clear $pool
+	log_must zpool scrub -w $pool
+
+	log_must zpool status -v
+	log_must check_pool_status $pool "scan" "with 0 errors"
+done
+
+log_must zpool destroy "$pool"
+
+log_pass "raidz expansion test succeeded."
+