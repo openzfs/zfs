93f39ef0870a7ecbe3eda8e737c1cf7c0a902e71
Shengqi Chen <harry-chen@outlook.com>
2023-12-02 14:45:38 +0800

    module/icp/asm-arm/sha2: enable non-SIMD asm kernels on armv5/6
    My merged pull request #15557 fixes compilation of sha2 kernels on arm
    v5/6. However, the compiler guards only allows sha256/512_armv7_impl to
    be used when __ARM_ARCH > 6. This patch enables these ASM kernels on all
    arm architectures. Some compiler guards are adjusted accordingly to
    avoid the unnecessary compilation of SIMD (e.g., neon, armv8ce) kernels
    on old architectures.
    
    Signed-off-by: Shengqi Chen <harry-chen@outlook.com>

--- module/icp/algs/sha2/sha256_impl.c
+++ module/icp/algs/sha2/sha256_impl.c
@@ -118,7 +118,15 @@ const sha256_ops_t sha256_shani_impl = {
 };
 #endif
 
-#elif defined(__aarch64__) || (defined(__arm__) && __ARM_ARCH > 6)
+#elif defined(__aarch64__) || defined(__arm__)
+extern void zfs_sha256_block_armv7(uint32_t s[8], const void *, size_t);
+const sha256_ops_t sha256_armv7_impl = {
+	.is_supported = sha2_is_supported,
+	.transform = zfs_sha256_block_armv7,
+	.name = "armv7"
+};
+
+#if __ARM_ARCH > 6
 static boolean_t sha256_have_neon(void)
 {
 	return (kfpu_allowed() && zfs_neon_available());
@@ -128,13 +136,6 @@ static boolean_t sha256_have_armv8ce(void)
 {
 	return (kfpu_allowed() && zfs_sha256_available());
 }
-
-extern void zfs_sha256_block_armv7(uint32_t s[8], const void *, size_t);
-const sha256_ops_t sha256_armv7_impl = {
-	.is_supported = sha2_is_supported,
-	.transform = zfs_sha256_block_armv7,
-	.name = "armv7"
-};
 
 TF(zfs_sha256_block_neon, tf_sha256_neon);
 const sha256_ops_t sha256_neon_impl = {
@@ -149,6 +150,7 @@ const sha256_ops_t sha256_armv8_impl = {
 	.transform = tf_sha256_armv8ce,
 	.name = "armv8-ce"
 };
+#endif
 
 #elif defined(__PPC64__)
 static boolean_t sha256_have_isa207(void)
@@ -192,10 +194,12 @@ static const sha256_ops_t *const sha256_impls[] = {
 #if defined(__x86_64) && defined(HAVE_SSE4_1)
 	&sha256_shani_impl,
 #endif
-#if defined(__aarch64__) || (defined(__arm__) && __ARM_ARCH > 6)
+#if defined(__aarch64__) || defined(__arm__)
 	&sha256_armv7_impl,
+#if __ARM_ARCH > 6
 	&sha256_neon_impl,
 	&sha256_armv8_impl,
+#endif
 #endif
 #if defined(__PPC64__)
 	&sha256_ppc_impl,
