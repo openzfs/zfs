18269018e20d2cb7e794caf135d355cc9c8a7a43
Warner Losh <imp@bsdimp.com>
2024-07-25 10:58:00 -0600

    freebsd: Only define __deprecated if it's not already defined
    FreeBSD's sys/cdefs.h is about to start defining __deprecated. So, only
    define it away if it's not already defined for older branches, etc.
    
    Sponsored-by: Netflix
    Signed-off-by: Warner Losh <imp@bsdimp.com>

--- include/os/freebsd/linux/compiler.h
+++ include/os/freebsd/linux/compiler.h
@@ -50,7 +50,9 @@
 #define	__cond_lock(x, c)		(c)
 #define	__bitwise
 #define	__devinitdata
+#ifndef __deprecated
 #define	__deprecated
+#endif
 #define	__init
 #define	__initconst
 #define	__devinit
