8156099cf24fb12d113235a2311b6c9257c587c3
Rob Norris <robn@despairlabs.com>
2024-07-31 18:43:39 +1000

    Linux 6.11: add compat macro for page_mapping()
    Since the change to folios it has just been a wrapper anyway. Linux has
    removed their wrapper, so we add one.
    
    Reviewed-by: Tony Hutter <hutter2@llnl.gov>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Rob Norris <robn@despairlabs.com>
    Sponsored-by: https://despairlabs.com/sponsor/
    Closes #16400

--- include/os/linux/kernel/linux/mm_compat.h
+++ include/os/linux/kernel/linux/mm_compat.h
@@ -21,16 +21,23 @@
 
 /*
  * Copyright (c) 2023, 2024, Klara Inc.
+ * Copyright (c) 2024, Rob Norris <robn@despairlabs.com>
  */
 
 #ifndef _ZFS_MM_COMPAT_H
 #define	_ZFS_MM_COMPAT_H
 
 #include <linux/mm.h>
+#include <linux/pagemap.h>
 
 /* 5.4 introduced page_size(). Older kernels can use a trivial macro instead */
 #ifndef HAVE_MM_PAGE_SIZE
 #define	page_size(p) ((unsigned long)(PAGE_SIZE << compound_order(p)))
+#endif
+
+/* 6.11 removed page_mapping(). A simple wrapper around folio_mapping() works */
+#ifndef HAVE_MM_PAGE_MAPPING
+#define	page_mapping(p) folio_mapping(page_folio(p))
 #endif
 
 #endif /* _ZFS_MM_COMPAT_H */
