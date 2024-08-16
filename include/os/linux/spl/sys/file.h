d1a07741f8add5328ee405830a573975c8373c47
Rob Norris <robn@despairlabs.com>
2024-08-16 16:57:45 +1000

    config: remove HAVE_INODE_LOCK_SHARED
    Sponsored-by: https://despairlabs.com/sponsor/
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Tony Hutter <hutter2@llnl.gov>
    Reviewed-by: Tino Reichardt <milky-zfs@mcmilk.de>
    Signed-off-by: Rob Norris <robn@despairlabs.com>
    Closes #16479

--- include/os/linux/spl/sys/file.h
+++ include/os/linux/spl/sys/file.h
@@ -28,7 +28,6 @@
 #define	FKIOCTL			0x80000000
 #define	ED_CASE_CONFLICT	0x10
 
-#ifdef HAVE_INODE_LOCK_SHARED
 #define	spl_inode_lock(ip)		inode_lock(ip)
 #define	spl_inode_unlock(ip)		inode_unlock(ip)
 #define	spl_inode_lock_shared(ip)	inode_lock_shared(ip)
@@ -37,15 +36,5 @@
 #define	spl_inode_trylock_shared(ip)	inode_trylock_shared(ip)
 #define	spl_inode_is_locked(ip)		inode_is_locked(ip)
 #define	spl_inode_lock_nested(ip, s)	inode_lock_nested(ip, s)
-#else
-#define	spl_inode_lock(ip)		mutex_lock(&(ip)->i_mutex)
-#define	spl_inode_unlock(ip)		mutex_unlock(&(ip)->i_mutex)
-#define	spl_inode_lock_shared(ip)	mutex_lock(&(ip)->i_mutex)
-#define	spl_inode_unlock_shared(ip)	mutex_unlock(&(ip)->i_mutex)
-#define	spl_inode_trylock(ip)		mutex_trylock(&(ip)->i_mutex)
-#define	spl_inode_trylock_shared(ip)	mutex_trylock(&(ip)->i_mutex)
-#define	spl_inode_is_locked(ip)		mutex_is_locked(&(ip)->i_mutex)
-#define	spl_inode_lock_nested(ip, s)	mutex_lock_nested(&(ip)->i_mutex, s)
-#endif
 
 #endif /* SPL_FILE_H */
