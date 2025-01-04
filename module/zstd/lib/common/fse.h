bda6ed8c5c36f498be387e2990ba688865512249
Rob Norris <robn@despairlabs.com>
2025-01-04 19:25:02 +1100

    SPDX: license tags: BSD-3-Clause OR GPL-2.0-only
    Sponsored-by: https://despairlabs.com/sponsor/
    Signed-off-by: Rob Norris <robn@despairlabs.com>

--- module/zstd/lib/common/fse.h
+++ module/zstd/lib/common/fse.h
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0-only
 /* ******************************************************************
  * FSE : Finite State Entropy codec
  * Public Prototypes declaration
