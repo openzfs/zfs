pjd/openzfs

Hierarchical bandwidth and operations limits.
Introduce six new properties: iolimit_{bw,op}_{read,write,total}.

The iolimit_bw_* properties limit the read, write, or combined bandwidth,
respectively, that a dataset and its descendants can consume.
Limits are applied to both file systems and ZFS volumes.

The configured limits are hierarchical, just like quotas; i.e., even if
a higher limit is configured on the child dataset, the parent's lower
limit will be enforced.

The limits are applied at the VFS level, not at the disk level.
The dataset is charged for each operation even if no disk access is
required (e.g., due to caching, compression, deduplication,
or NOP writes) or if the operation will cause more traffic (due to
the copies property, mirroring, or RAIDZ).

Read bandwidth consumption is based on:

- read-like syscalls, eg., aio_read(2), pread(2), preadv(2), read(2),
  readv(2), sendfile(2)

- syscalls like getdents(2) and getdirentries(2)

- reading via mmaped files

- zfs send

Write bandwidth consumption is based on:

- write-like syscalls, eg., aio_write(2), pwrite(2), pwritev(2),
  write(2), writev(2)

- writing via mmaped files

- zfs receive

The iolimit_op_* properties limit the read, write, or both metadata
operations, respectively, that dataset and its descendants can generate.

Read operations consumption is based on:

- read-like syscalls where the number of operations is equal to the
  number of blocks being read (never less than 1)

- reading via mmaped files, where the number of operations is equal
  to the number of pages being read (never less than 1)

- syscalls accessing metadata: readlink(2), stat(2)

Write operations consumption is based on:

- write-like syscalls where the number of operations is equal to the
  number of blocks being written (never less than 1)

- writing via mmaped files, where the number of operations is equal
  to the number of pages being written (never less than 1)

- syscalls modifing a directory's content: bind(2

tests/zfs-tests/tests/functional/iolimit/filesystem_bw_combined.ksh
@@ -1,0 +1,104 @@
+#! /bin/ksh -p
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or https://opensource.org/licenses/CDDL-1.0.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2024 The FreeBSD Foundation
+#
+# This software was developed by Pawel Dawidek <pawel@dawidek.net>
+# under sponsorship from the FreeBSD Foundation.
+#
+
+. $STF_SUITE/include/libtest.shlib
+. $STF_SUITE/tests/functional/iolimit/iolimit_common.kshlib
+
+verify_runnable "both"
+
+log_assert "Verify configurations where multiple limit types are set"
+
+iolimit_reset
+
+log_must truncate -s 1G "$TESTDIR/file"
+
+log_must zfs set iolimit_bw_read=4M "$TESTPOOL/$TESTFS"
+log_must zfs set iolimit_bw_write=5M "$TESTPOOL/$TESTFS"
+log_must zfs set iolimit_bw_total=6M "$TESTPOOL/$TESTFS"
+
+log_must ioiolimit_bw_read 12 3 "$TESTDIR/file"
+log_must ioiolimit_bw_write 15 3 "$TESTDIR/file"
+stopwatch_start
+ddio "$TESTDIR/file" "/dev/null" 36 &
+ddio "/dev/zero" "$TESTDIR/file" 36 &
+wait
+stopwatch_check 12
+
+log_must zfs set iolimit_bw_read=none "$TESTPOOL/$TESTFS"
+log_must zfs set iolimit_bw_write=none "$TESTPOOL/$TESTFS"
+log_must zfs set iolimit_bw_total=none "$TESTPOOL/$TESTFS"
+
+rm -f "$TESTDIR/file"
+
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0"
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must truncate -s 1G "$TESTDIR/lvl0/lvl1/lvl2/file0"
+log_must truncate -s 1G "$TESTDIR/lvl0/lvl1/lvl2/file1"
+
+log_must zfs set iolimit_bw_read=4M "$TESTPOOL/$TESTFS/lvl0"
+log_must zfs set iolimit_bw_write=5M "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must zfs set iolimit_bw_total=6M "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must ioiolimit_bw_read 12 3 "$TESTDIR/lvl0/lvl1/lvl2/file0"
+log_must ioiolimit_bw_write 15 3 "$TESTDIR/lvl0/lvl1/lvl2/file1"
+stopwatch_start
+ddio "$TESTDIR/lvl0/lvl1/lvl2/file0" "/dev/null" 36 &
+ddio "/dev/zero" "$TESTDIR/lvl0/lvl1/lvl2/file1" 36 &
+wait
+stopwatch_check 12
+
+log_must zfs set iolimit_bw_read=none "$TESTPOOL/$TESTFS/lvl0"
+log_must zfs set iolimit_bw_write=none "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must zfs set iolimit_bw_total=none "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must zfs set iolimit_bw_total=6M "$TESTPOOL/$TESTFS/lvl0"
+log_must zfs set iolimit_bw_read=4M "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must zfs set iolimit_bw_write=5M "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must ioiolimit_bw_read 12 3 "$TESTDIR/lvl0/lvl1/lvl2/file0"
+log_must ioiolimit_bw_write 15 3 "$TESTDIR/lvl0/lvl1/lvl2/file1"
+stopwatch_start
+ddio "$TESTDIR/lvl0/lvl1/lvl2/file0" "/dev/null" 36 &
+ddio "/dev/zero" "$TESTDIR/lvl0/lvl1/lvl2/file1" 36 &
+wait
+stopwatch_check 12
+
+log_must zfs set iolimit_bw_total=none "$TESTPOOL/$TESTFS/lvl0"
+log_must zfs set iolimit_bw_read=none "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must zfs set iolimit_bw_write=none "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0"
+
+rm -f "$TESTDIR/lvl0/lvl1/lvl2/file0" "$TESTDIR/lvl0/lvl1/lvl2/file1"
+
+log_pass

tests/zfs-tests/tests/functional/iolimit/filesystem_bw_hierarchical_vertical_write.ksh
@@ -1,0 +1,65 @@
+#! /bin/ksh -p
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or https://opensource.org/licenses/CDDL-1.0.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2024 The FreeBSD Foundation
+#
+# This software was developed by Pawel Dawidek <pawel@dawidek.net>
+# under sponsorship from the FreeBSD Foundation.
+#
+
+. $STF_SUITE/include/libtest.shlib
+. $STF_SUITE/tests/functional/iolimit/iolimit_common.kshlib
+
+verify_runnable "both"
+
+log_assert "Verify hierarchical bandwidth write limits configured on multiple levels"
+
+iolimit_reset
+
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0"
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must truncate -s 1G "$TESTDIR/lvl0/lvl1/lvl2/file"
+
+for lvl0 in none 1M 3M 5M; do
+	for lvl1 in none 1M 3M 5M; do
+		for lvl2 in none 1M 3M 5M; do
+			# We need at least one level with 1M limit.
+			if [ $lvl0 != "1M" ] && [ $lvl1 != "1M" ] && [ $lvl2 != "1M" ]; then
+				continue
+			fi
+
+			log_must zfs set iolimit_bw_write=$lvl0 "$TESTPOOL/$TESTFS/lvl0"
+			log_must zfs set iolimit_bw_write=$lvl1 "$TESTPOOL/$TESTFS/lvl0/lvl1"
+			log_must zfs set iolimit_bw_write=$lvl2 "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+			log_must ioiolimit_bw_write 5 5 "$TESTDIR/lvl0/lvl1/lvl2/file"
+		done
+	done
+done
+
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0"
+
+log_pass

tests/zfs-tests/tests/functional/iolimit/volume_bw_hierarchical_vertical_write.ksh
@@ -1,0 +1,63 @@
+#! /bin/ksh -p
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or https://opensource.org/licenses/CDDL-1.0.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2024 The FreeBSD Foundation
+#
+# This software was developed by Pawel Dawidek <pawel@dawidek.net>
+# under sponsorship from the FreeBSD Foundation.
+#
+
+. $STF_SUITE/include/libtest.shlib
+. $STF_SUITE/tests/functional/iolimit/iolimit_common.kshlib
+
+verify_runnable "both"
+
+log_assert "Verify hierarchical bandwidth write limits configured on multiple levels"
+
+iolimit_reset
+
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0"
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must create_volume "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2" 16M
+
+for lvl0 in none 1M 3M 5M; do
+	for lvl1 in none 1M 3M 5M; do
+		for lvl2 in none 1M 3M 5M; do
+			# We need at least one level with 1M limit.
+			if [ $lvl0 != "1M" ] && [ $lvl1 != "1M" ] && [ $lvl2 != "1M" ]; then
+				continue
+			fi
+
+			log_must zfs set iolimit_bw_write=$lvl0 "$TESTPOOL/$TESTFS/lvl0"
+			log_must zfs set iolimit_bw_write=$lvl1 "$TESTPOOL/$TESTFS/lvl0/lvl1"
+			log_must zfs set iolimit_bw_write=$lvl2 "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+			log_must ioiolimit_bw_write 5 5 "/dev/zvol/$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+		done
+	done
+done
+
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0"
+
+log_pass

openzfs/zfs

tests/zfs-tests/tests/functional/iolimit/filesystem_bw_combined.ksh
@@ -1,0 +1,104 @@
+#! /bin/ksh -p
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or https://opensource.org/licenses/CDDL-1.0.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2024 The FreeBSD Foundation
+#
+# This software was developed by Pawel Dawidek <pawel@dawidek.net>
+# under sponsorship from the FreeBSD Foundation.
+#
+
+. $STF_SUITE/include/libtest.shlib
+. $STF_SUITE/tests/functional/iolimit/iolimit_common.kshlib
+
+verify_runnable "both"
+
+log_assert "Verify configurations where multiple limit types are set"
+
+iolimit_reset
+
+log_must truncate -s 1G "$TESTDIR/file"
+
+log_must zfs set iolimit_bw_read=4M "$TESTPOOL/$TESTFS"
+log_must zfs set iolimit_bw_write=5M "$TESTPOOL/$TESTFS"
+log_must zfs set iolimit_bw_total=6M "$TESTPOOL/$TESTFS"
+
+log_must iolimit_bw_read 12 3 "$TESTDIR/file"
+log_must iolimit_bw_write 15 3 "$TESTDIR/file"
+stopwatch_start
+ddio "$TESTDIR/file" "/dev/null" 36 &
+ddio "/dev/zero" "$TESTDIR/file" 36 &
+wait
+stopwatch_check 12
+
+log_must zfs set iolimit_bw_read=none "$TESTPOOL/$TESTFS"
+log_must zfs set iolimit_bw_write=none "$TESTPOOL/$TESTFS"
+log_must zfs set iolimit_bw_total=none "$TESTPOOL/$TESTFS"
+
+rm -f "$TESTDIR/file"
+
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0"
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must truncate -s 1G "$TESTDIR/lvl0/lvl1/lvl2/file0"
+log_must truncate -s 1G "$TESTDIR/lvl0/lvl1/lvl2/file1"
+
+log_must zfs set iolimit_bw_read=4M "$TESTPOOL/$TESTFS/lvl0"
+log_must zfs set iolimit_bw_write=5M "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must zfs set iolimit_bw_total=6M "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must iolimit_bw_read 12 3 "$TESTDIR/lvl0/lvl1/lvl2/file0"
+log_must iolimit_bw_write 15 3 "$TESTDIR/lvl0/lvl1/lvl2/file1"
+stopwatch_start
+ddio "$TESTDIR/lvl0/lvl1/lvl2/file0" "/dev/null" 36 &
+ddio "/dev/zero" "$TESTDIR/lvl0/lvl1/lvl2/file1" 36 &
+wait
+stopwatch_check 12
+
+log_must zfs set iolimit_bw_read=none "$TESTPOOL/$TESTFS/lvl0"
+log_must zfs set iolimit_bw_write=none "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must zfs set iolimit_bw_total=none "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must zfs set iolimit_bw_total=6M "$TESTPOOL/$TESTFS/lvl0"
+log_must zfs set iolimit_bw_read=4M "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must zfs set iolimit_bw_write=5M "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must iolimit_bw_read 12 3 "$TESTDIR/lvl0/lvl1/lvl2/file0"
+log_must iolimit_bw_write 15 3 "$TESTDIR/lvl0/lvl1/lvl2/file1"
+stopwatch_start
+ddio "$TESTDIR/lvl0/lvl1/lvl2/file0" "/dev/null" 36 &
+ddio "/dev/zero" "$TESTDIR/lvl0/lvl1/lvl2/file1" 36 &
+wait
+stopwatch_check 12
+
+log_must zfs set iolimit_bw_total=none "$TESTPOOL/$TESTFS/lvl0"
+log_must zfs set iolimit_bw_read=none "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must zfs set iolimit_bw_write=none "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0"
+
+rm -f "$TESTDIR/lvl0/lvl1/lvl2/file0" "$TESTDIR/lvl0/lvl1/lvl2/file1"
+
+log_pass

tests/zfs-tests/tests/functional/iolimit/filesystem_bw_hierarchical_vertical_write.ksh
@@ -1,0 +1,65 @@
+#! /bin/ksh -p
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or https://opensource.org/licenses/CDDL-1.0.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2024 The FreeBSD Foundation
+#
+# This software was developed by Pawel Dawidek <pawel@dawidek.net>
+# under sponsorship from the FreeBSD Foundation.
+#
+
+. $STF_SUITE/include/libtest.shlib
+. $STF_SUITE/tests/functional/iolimit/iolimit_common.kshlib
+
+verify_runnable "both"
+
+log_assert "Verify hierarchical bandwidth write limits configured on multiple levels"
+
+iolimit_reset
+
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0"
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+
+log_must truncate -s 1G "$TESTDIR/lvl0/lvl1/lvl2/file"
+
+for lvl0 in none 1M 3M 5M; do
+	for lvl1 in none 1M 3M 5M; do
+		for lvl2 in none 1M 3M 5M; do
+			# We need at least one level with 1M limit.
+			if [ $lvl0 != "1M" ] && [ $lvl1 != "1M" ] && [ $lvl2 != "1M" ]; then
+				continue
+			fi
+
+			log_must zfs set iolimit_bw_write=$lvl0 "$TESTPOOL/$TESTFS/lvl0"
+			log_must zfs set iolimit_bw_write=$lvl1 "$TESTPOOL/$TESTFS/lvl0/lvl1"
+			log_must zfs set iolimit_bw_write=$lvl2 "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+			log_must iolimit_bw_write 5 5 "$TESTDIR/lvl0/lvl1/lvl2/file"
+		done
+	done
+done
+
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0"
+
+log_pass

tests/zfs-tests/tests/functional/iolimit/volume_bw_hierarchical_vertical_write.ksh
@@ -1,0 +1,63 @@
+#! /bin/ksh -p
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or https://opensource.org/licenses/CDDL-1.0.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2024 The FreeBSD Foundation
+#
+# This software was developed by Pawel Dawidek <pawel@dawidek.net>
+# under sponsorship from the FreeBSD Foundation.
+#
+
+. $STF_SUITE/include/libtest.shlib
+. $STF_SUITE/tests/functional/iolimit/iolimit_common.kshlib
+
+verify_runnable "both"
+
+log_assert "Verify hierarchical bandwidth write limits configured on multiple levels"
+
+iolimit_reset
+
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0"
+log_must create_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must create_volume "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2" 16M
+
+for lvl0 in none 1M 3M 5M; do
+	for lvl1 in none 1M 3M 5M; do
+		for lvl2 in none 1M 3M 5M; do
+			# We need at least one level with 1M limit.
+			if [ $lvl0 != "1M" ] && [ $lvl1 != "1M" ] && [ $lvl2 != "1M" ]; then
+				continue
+			fi
+
+			log_must zfs set iolimit_bw_write=$lvl0 "$TESTPOOL/$TESTFS/lvl0"
+			log_must zfs set iolimit_bw_write=$lvl1 "$TESTPOOL/$TESTFS/lvl0/lvl1"
+			log_must zfs set iolimit_bw_write=$lvl2 "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+			log_must iolimit_bw_write 5 5 "/dev/zvol/$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+		done
+	done
+done
+
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1/lvl2"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0/lvl1"
+log_must destroy_dataset "$TESTPOOL/$TESTFS/lvl0"
+
+log_pass