87f8bf6b0c4d8570754a52178124505339c55dc9
Martin Matuška <martin@matuska.de>
2025-04-02 00:27:03 +0200

    Multiple printf() size fixes (#17199)
    cmd/zinject/zinject.c:
     - use PRIu64 when printing uint64_t
    
    tests/zfs-tests/cmd/clonefile.c:
     - use an unsigned long long to store result from strtoull()
     - use %jd for printing off_t, %zu for size_t, %zd for ssize_t
    
    tests/zfs-tests/tests/functional/vdev_disk/page_alignment.c:
     - use %zx to print size_t
    
    Discovered when compiling on FreeBSD i386.
    
    Signed-off-by: Martin Matuska <mm@FreeBSD.org>
    
    Reviewed-by: Rob Norris <robn@despairlabs.com>
    Reviewed-by: Alexander Motin <mav@FreeBSD.org>
    Reviewed-by: Allan Jude <allan@klarasystems.com>
    Reviewed-by: @ImAwsumm

--- cmd/zinject/zinject.c
+++ cmd/zinject/zinject.c
@@ -454,7 +454,8 @@ print_data_handler(int id, const char *pool, zinject_record_t *record,
 
 
 	(void) printf("%3d  %-15s  %-6llu  %-6llu  %-8s  %-3d  0x%02x  %-15s  "
-	    "%6lu  %6lu\n", id, pool, (u_longlong_t)record->zi_objset,
+	    "%6" PRIu64 "  %6" PRIu64 "\n", id, pool,
+	    (u_longlong_t)record->zi_objset,
 	    (u_longlong_t)record->zi_object, type_to_name(record->zi_type),
 	    record->zi_level, record->zi_dvas, rangebuf,
 	    record->zi_match_count, record->zi_inject_count);
@@ -491,7 +492,8 @@ print_device_handler(int id, const char *pool, zinject_record_t *record,
 	    (((double)record->zi_freq) / ZI_PERCENTAGE_MAX) * 100.0f;
 
 	(void) printf("%3d  %-15s  %llx  %-5s  %-10s  %8.4f%%  "
-	    "%6lu  %6lu\n", id, pool, (u_longlong_t)record->zi_guid,
+	    "%6" PRIu64 "  %6" PRIu64 "\n", id, pool,
+	    (u_longlong_t)record->zi_guid,
 	    iotype_to_str(record->zi_iotype), err_to_str(record->zi_error),
 	    freq, record->zi_match_count, record->zi_inject_count);
 
@@ -526,7 +528,8 @@ print_delay_handler(int id, const char *pool, zinject_record_t *record,
 	    (((double)record->zi_freq) / ZI_PERCENTAGE_MAX) * 100.0f;
 
 	(void) printf("%3d  %-15s  %llx  %10llu  %5llu  %8.4f%%  "
-	    "%6lu  %6lu\n", id, pool, (u_longlong_t)record->zi_guid,
+	    "%6" PRIu64 "  %6" PRIu64 "\n", id, pool,
+	    (u_longlong_t)record->zi_guid,
 	    (u_longlong_t)NSEC2MSEC(record->zi_timer),
 	    (u_longlong_t)record->zi_nlanes,
 	    freq, record->zi_match_count, record->zi_inject_count);
