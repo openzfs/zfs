2a9a91439277c624c2e673770c2be90ce646a066
Rob Norris <robn@despairlabs.com>
2025-05-17 14:06:44 +1000

    lzc: move lzc_ioctl_fd() into lzc proper
    Name the OS-specific call lzc_ioctl_fd_os(), and make lzc_ioctl_fd()
    wrap it, so we can do more in the wrapper.
    
    Sponsored-by: https://despairlabs.com/sponsor/
    Signed-off-by: Rob Norris <robn@despairlabs.com>

--- lib/libzfs_core/libzfs_core.c
+++ lib/libzfs_core/libzfs_core.c
@@ -96,6 +96,8 @@
 #define	BIG_PIPE_SIZE (64 * 1024) /* From sys/pipe.h */
 #endif
 
+#include "libzfs_core_impl.h"
+
 static int g_fd = -1;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 static int g_refcount;
@@ -169,6 +171,12 @@ libzfs_core_fini(void)
 		g_fd = -1;
 	}
 	(void) pthread_mutex_unlock(&g_lock);
+}
+
+int
+lzc_ioctl_fd(int fd, unsigned long ioc, zfs_cmd_t *zc)
+{
+	return (lzc_ioctl_fd_os(fd, ioc, zc));
 }
 
 static int
