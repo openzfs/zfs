aed3a7eb53f7afe0a7142b2eb8e15dc67d1a700c
Paul Dagnelie <paul.dagnelie@klarasystems.com>
2025-05-19 16:38:27 -0700

    Anyraid implementation
    Signed-off-by: Paul Dagnelie <paul.dagnelie@klarasystems.com>
    Sponsored-by: Eshtek, creators of HexOS
    Sponsored-by: Klara, Inc.

--- include/sys/fs/zfs.h
+++ include/sys/fs/zfs.h
@@ -1015,10 +1015,14 @@ typedef struct zpool_load_policy {
 #define	ZPOOL_CONFIG_DRAID_NGROUPS	"draid_ngroups"
 #define	ZPOOL_CONFIG_DRAID_NCHILDREN	"draid_nchildren"
 
+/* ANYRAID configuration */
+#define	ZPOOL_CONFIG_ANYRAID_PARITY_TYPE	"parity_type"
+
 #define	VDEV_TYPE_ROOT			"root"
 #define	VDEV_TYPE_MIRROR		"mirror"
 #define	VDEV_TYPE_REPLACING		"replacing"
 #define	VDEV_TYPE_RAIDZ			"raidz"
+#define	VDEV_TYPE_ANYRAID		"anyraid"
 #define	VDEV_TYPE_DRAID			"draid"
 #define	VDEV_TYPE_DRAID_SPARE		"dspare"
 #define	VDEV_TYPE_DISK			"disk"
@@ -1029,6 +1033,8 @@ typedef struct zpool_load_policy {
 #define	VDEV_TYPE_LOG			"log"
 #define	VDEV_TYPE_L2CACHE		"l2cache"
 #define	VDEV_TYPE_INDIRECT		"indirect"
+
+#define	VDEV_ANYRAID_MAXPARITY		3
 
 #define	VDEV_RAIDZ_MAXPARITY		3
 
--- lib/libzfs/libzfs_pool.c
+++ lib/libzfs/libzfs_pool.c
@@ -1721,6 +1721,20 @@ zpool_create(libzfs_handle_t *hdl, const char *pool, nvlist_t *nvroot,
 			}
 			ret = zfs_error(hdl, EZFS_BADDEV, errbuf);
 			break;
+			return (zfs_error(hdl, EZFS_BADDEV, errbuf));
+		case ENOLCK:
+			/*
+			 * This occurs when one of the devices is an anyraid
+			 * device that can't hold a single tile.
+			 * Unfortunately, we can't detect which device was the
+			 * problem device since there's no reliable way to
+			 * determine device size from userland.
+			 */
+			zfs_error_aux(hdl, dgettext(TEXT_DOMAIN,
+			    "one or more anyraid devices cannot store "
+			    "any tiles"));
+			ret = zfs_error(hdl, EZFS_BADDEV, errbuf);
+			break;
 
 		case ENOSPC:
 			zfs_error_aux(hdl, dgettext(TEXT_DOMAIN,
@@ -1974,7 +1988,18 @@ zpool_add(zpool_handle_t *zhp, nvlist_t *nvroot, boolean_t check_ashift)
 			}
 			(void) zfs_error(hdl, EZFS_BADDEV, errbuf);
 			break;
-
+		case ENOLCK:
+			/*
+			 * This occurs when one of the devices is an anyraid
+			 * device that can't hold a single tile.
+			 * Unfortunately, we can't detect which device was the
+			 * problem device since there's no reliable way to
+			 * determine device size from userland.
+			 */
+			zfs_error_aux(hdl, dgettext(TEXT_DOMAIN,
+			    "one or more anyraid devices cannot store "
+			    "any tiles"));
+			return (zfs_error(hdl, EZFS_BADDEV, errbuf));
 		case ENOTSUP:
 			zfs_error_aux(hdl, dgettext(TEXT_DOMAIN,
 			    "pool must be upgraded to add these vdevs"));
@@ -3333,7 +3358,8 @@ zpool_vdev_is_interior(const char *name)
 	    strncmp(name,
 	    VDEV_TYPE_REPLACING, strlen(VDEV_TYPE_REPLACING)) == 0 ||
 	    strncmp(name, VDEV_TYPE_ROOT, strlen(VDEV_TYPE_ROOT)) == 0 ||
-	    strncmp(name, VDEV_TYPE_MIRROR, strlen(VDEV_TYPE_MIRROR)) == 0)
+	    strncmp(name, VDEV_TYPE_MIRROR, strlen(VDEV_TYPE_MIRROR)) == 0 ||
+	    strncmp(name, VDEV_TYPE_ANYRAID, strlen(VDEV_TYPE_ANYRAID)) == 0)
 		return (B_TRUE);
 
 	if (strncmp(name, VDEV_TYPE_DRAID, strlen(VDEV_TYPE_DRAID)) == 0 &&
@@ -3952,6 +3978,15 @@ zpool_vdev_attach(zpool_handle_t *zhp, const char *old_disk,
 		    "option '-o ashift=N' to override the optimal size"));
 		(void) zfs_error(hdl, EZFS_BADDEV, errbuf);
 		break;
+
+	case ENOLCK:
+		/*
+		 * This occurs when one of the devices is an anyraid
+		 * device that can't hold a single tile.
+		 */
+		zfs_error_aux(hdl, dgettext(TEXT_DOMAIN,
+		    "new device cannot store any tiles"));
+		return (zfs_error(hdl, EZFS_BADDEV, errbuf));
 
 	case ENAMETOOLONG:
 		/*
@@ -4737,9 +4772,11 @@ zpool_vdev_name(libzfs_handle_t *hdl, zpool_handle_t *zhp, nvlist_t *nv,
 		path = type;
 
 		/*
-		 * If it's a raidz device, we need to stick in the parity level.
+		 * If it's a raidz or anyraid device, we need to stick in the
+		 * parity level.
 		 */
-		if (strcmp(path, VDEV_TYPE_RAIDZ) == 0) {
+		if (strcmp(path, VDEV_TYPE_RAIDZ) == 0 ||
+		    strcmp(path, VDEV_TYPE_ANYRAID) == 0) {
 			value = fnvlist_lookup_uint64(nv, ZPOOL_CONFIG_NPARITY);
 			(void) snprintf(buf, sizeof (buf), "%s%llu", path,
 			    (u_longlong_t)value);
