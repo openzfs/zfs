4090661cc94117c117907f8405d7d317e8c509d4
Fedor Uporov <fuporov.vstack@gmail.com>
2025-05-30 11:44:33 +0300

    Rename zvol kernel module parameters sysctls on FreeBSD side
    Make 'zvol_threads', 'zvol_num_taskqs' and 'zvol_request_sync' names
    compatible with FreeBSD sysctl naming convention. Now the sysctls are
    have a next form:
    
    $ sysctl vfs.zfs.vol.threads
    vfs.zfs.vol.threads: 0
    
    $ sysctl vfs.zfs.vol.num_taskqs
    vfs.zfs.vol.num_taskqs: 0
    
    $ sysctl vfs.zfs.vol.request_sync
    vfs.zfs.vol.request_sync: 0
    
    Sponsored-by: vStack, Inc.
    Signed-off-by: Fedor Uporov <fuporov.vstack@gmail.com>

--- module/zfs/zvol.c
+++ module/zfs/zvol.c
@@ -2156,9 +2156,9 @@ zvol_fini_impl(void)
 
 ZFS_MODULE_PARAM(zfs_vol, zvol_, inhibit_dev, UINT, ZMOD_RW,
 	"Do not create zvol device nodes");
-ZFS_MODULE_PARAM(zfs, , zvol_threads, UINT, ZMOD_RW,
+ZFS_MODULE_PARAM(zfs_vol, zvol_, threads, UINT, ZMOD_RW,
 	"Number of threads for I/O requests. Set to 0 to use all active CPUs");
-ZFS_MODULE_PARAM(zfs, , zvol_num_taskqs, UINT, ZMOD_RW,
+ZFS_MODULE_PARAM(zfs_vol, zvol_, num_taskqs, UINT, ZMOD_RW,
 	"Number of zvol taskqs");
-ZFS_MODULE_PARAM(zfs, , zvol_request_sync, UINT, ZMOD_RW,
+ZFS_MODULE_PARAM(zfs_vol, zvol_, request_sync, UINT, ZMOD_RW,
 	"Synchronously handle bio requests");
