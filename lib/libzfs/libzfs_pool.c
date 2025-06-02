622c746953b168d3098bf6f09731092ff4444086
Paul Dagnelie <paul.dagnelie@klarasystems.com>
2025-06-02 14:22:52 -0700

    Add support for anyraid in vdev properties
    Signed-off-by: Paul Dagnelie <paul.dagnelie@klarasystems.com>
    Sponsored-by: Eshtek, creators of HexOS
    Sponsored-by: Klara, Inc.

--- lib/libzfs/libzfs_pool.c
+++ lib/libzfs/libzfs_pool.c
@@ -3980,6 +3980,22 @@ zpool_vdev_attach(zpool_handle_t *zhp, const char *old_disk,
 		free(newname);
 		return (zfs_error(hdl, EZFS_BADTARGET, errbuf));
 	}
+	uint64_t min_tile_size = 0;
+	if (strncmp(fnvlist_lookup_string(tgt, ZPOOL_CONFIG_TYPE), "any",
+	    3) == 0) {
+		char mts[32];
+		VERIFY0(zpool_get_vdev_prop(zhp, old_disk,
+		    VDEV_PROP_ANYRAID_TILE_SIZE, NULL, mts, 32, NULL, B_TRUE));
+		VERIFY3S(sscanf(mts, "%llu", (u_longlong_t *)&min_tile_size),
+		    ==, 1);
+		/*
+		 * Unfortunately it's difficult to get the definitions that
+		 * would allow us to do this cleanly into userland. We need
+		 * space for a tile (above) plus the mapping (256MiB) plus the
+		 * labels (4.5MiB).
+		 */
+		min_tile_size += 261 * 1024 * 1024;
+	}
 
 	free(newname);
 
@@ -4077,14 +4093,18 @@ zpool_vdev_attach(zpool_handle_t *zhp, const char *old_disk,
 		(void) zfs_error(hdl, EZFS_BADDEV, errbuf);
 		break;
 
-	case ENOLCK:
+	case ENOLCK: {
 		/*
 		 * This occurs when one of the devices is an anyraid
 		 * device that can't hold a single tile.
 		 */
+		char buf[32];
+		ASSERT(min_tile_size != 0);
+		zfs_nicenum(min_tile_size, buf, 32);
 		zfs_error_aux(hdl, dgettext(TEXT_DOMAIN,
-		    "new device cannot store any tiles"));
+		    "new device cannot store any tiles (min size %s)"), buf);
 		return (zfs_error(hdl, EZFS_BADDEV, errbuf));
+	}
 
 	case ENAMETOOLONG:
 		/*
@@ -5806,6 +5826,10 @@ zpool_get_vdev_prop_value(nvlist_t *nvprop, vdev_prop_t prop, char *prop_name,
 		if (nvlist_lookup_nvlist(nvprop, prop_name, &nv) == 0) {
 			src = fnvlist_lookup_uint64(nv, ZPROP_SOURCE);
 			intval = fnvlist_lookup_uint64(nv, ZPROP_VALUE);
+		} else if (prop == VDEV_PROP_ANYRAID_CAP_TILES ||
+		    prop == VDEV_PROP_ANYRAID_NUM_TILES ||
+		    prop == VDEV_PROP_ANYRAID_TILE_SIZE) {
+			return (ENOENT);
 		} else {
 			src = ZPROP_SRC_DEFAULT;
 			intval = vdev_prop_default_numeric(prop);
@@ -5836,6 +5860,9 @@ zpool_get_vdev_prop_value(nvlist_t *nvprop, vdev_prop_t prop, char *prop_name,
 		case VDEV_PROP_BYTES_FREE:
 		case VDEV_PROP_BYTES_CLAIM:
 		case VDEV_PROP_BYTES_TRIM:
+		case VDEV_PROP_ANYRAID_CAP_TILES:
+		case VDEV_PROP_ANYRAID_NUM_TILES:
+		case VDEV_PROP_ANYRAID_TILE_SIZE:
 			if (literal) {
 				(void) snprintf(buf, len, "%llu",
 				    (u_longlong_t)intval);
