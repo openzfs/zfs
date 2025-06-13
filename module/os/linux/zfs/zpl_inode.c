734003591568bb97c91f597efc1020c35131c1dd
Pavel Snajdr <snajpa@snajpa.net>
2025-06-14 01:17:04 +0200

    Linux: inode unlock rework
    Inode should be unlocked after dentry is instantiated, otherwise
    to roll back progress in error paths after zfs_znode_alloc calls
    insert_inode_locked, we should call drop_nlink and iput the inode.
    
    Most of touched functions call zfs_mknode, which itself calls
    zfs_znode_alloc and then there are also zfs_zget and rezget,
    but we also had remove_inode_hash in a few other placces, let's remove
    all the calls altogether.
    
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- module/os/linux/zfs/zpl_inode.c
+++ module/os/linux/zfs/zpl_inode.c
@@ -202,10 +202,11 @@ zpl_create(struct inode *dir, struct dentry *dentry, umode_t mode, bool flag)
 
 		if (error) {
 			(void) zfs_remove(ITOZ(dir), dname(dentry), cr, 0);
-			remove_inode_hash(ZTOI(zp));
+			drop_nlink(ZTOI(zp));
 			iput(ZTOI(zp));
 		} else {
 			d_instantiate(dentry, ZTOI(zp));
+			unlock_new_inode(ZTOI(zp));
 		}
 	}
 
@@ -264,10 +265,11 @@ zpl_mknod(struct inode *dir, struct dentry *dentry, umode_t mode,
 
 		if (error) {
 			(void) zfs_remove(ITOZ(dir), dname(dentry), cr, 0);
-			remove_inode_hash(ZTOI(zp));
+			drop_nlink(ZTOI(zp));
 			iput(ZTOI(zp));
 		} else {
 			d_instantiate(dentry, ZTOI(zp));
+			unlock_new_inode(ZTOI(zp));
 		}
 	}
 
@@ -429,10 +431,11 @@ zpl_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode)
 
 		if (error) {
 			(void) zfs_rmdir(ITOZ(dir), dname(dentry), NULL, cr, 0);
-			remove_inode_hash(ZTOI(zp));
+			drop_nlink(ZTOI(zp));
 			iput(ZTOI(zp));
 		} else {
 			d_instantiate(dentry, ZTOI(zp));
+			unlock_new_inode(ZTOI(zp));
 		}
 	}
 
@@ -715,10 +718,11 @@ zpl_symlink(struct inode *dir, struct dentry *dentry, const char *name)
 		error = zpl_xattr_security_init(ZTOI(zp), dir, &dentry->d_name);
 		if (error) {
 			(void) zfs_remove(ITOZ(dir), dname(dentry), cr, 0);
-			remove_inode_hash(ZTOI(zp));
+			drop_nlink(ZTOI(zp));
 			iput(ZTOI(zp));
 		} else {
 			d_instantiate(dentry, ZTOI(zp));
+			unlock_new_inode(ZTOI(zp));
 		}
 	}
 
