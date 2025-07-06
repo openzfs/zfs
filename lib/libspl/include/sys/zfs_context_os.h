249fb1c2abaf3ed73949508d1d8a516949dba506
Rob Norris <robn@despairlabs.com>
2025-07-06 12:16:54 +1000

    libspl: single zfs_context_os.h
    Sponsored-by: https://despairlabs.com/sponsor/
    Signed-off-by: Rob Norris <robn@despairlabs.com>

--- lib/libspl/include/sys/zfs_context_os.h
+++ lib/libspl/include/sys/zfs_context_os.h
@@ -3,9 +3,8 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
  * or https://opensource.org/licenses/CDDL-1.0.
@@ -20,9 +19,15 @@
  *
  * CDDL HEADER END
  */
+/*
+ * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright 2011 Nexenta Systems, Inc.  All rights reserved.
+ * Copyright (c) 2012, 2018 by Delphix. All rights reserved.
+ * Copyright (c) 2012, Joyent, Inc. All rights reserved.
+ */
 
-#ifndef ZFS_CONTEXT_OS_H
-#define	ZFS_CONTEXT_OS_H
+#ifndef ZFS_CONTEXT_OS_H_
+#define	ZFS_CONTEXT_OS_H_
 
 #define	HAVE_LARGE_STACKS	1
 
