885d929cf8ab37a6e09402e01587cfce31094fa1
Alexander Motin <alexander.motin@TrueNAS.com>
2025-08-13 15:56:25 -0400

    Fix missed assertion update in physical rewrite patch
    Physical rewrite patch changed the meaning of BP_GET_BIRTH(), but
    I missed update one of its occurences, ending up asserting equal
    logical birth times instead of equal physical birth times.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Alexander Motin <alexander.motin@TrueNAS.com>
    Fixes #17565
    Closes #17631

--- module/zfs/dsl_deadlist.c
+++ module/zfs/dsl_deadlist.c
@@ -1049,7 +1049,8 @@ dsl_livelist_iterate(void *arg, const blkptr_t *bp, boolean_t bp_freed,
 		ASSERT3U(BP_GET_PSIZE(bp), ==, BP_GET_PSIZE(&found->le_bp));
 		ASSERT3U(BP_GET_CHECKSUM(bp), ==,
 		    BP_GET_CHECKSUM(&found->le_bp));
-		ASSERT3U(BP_GET_BIRTH(bp), ==, BP_GET_BIRTH(&found->le_bp));
+		ASSERT3U(BP_GET_PHYSICAL_BIRTH(bp), ==,
+		    BP_GET_PHYSICAL_BIRTH(&found->le_bp));
 	}
 	if (bp_freed) {
 		if (found == NULL) {
