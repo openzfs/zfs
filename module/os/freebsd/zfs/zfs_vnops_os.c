9212c0bf58a1e9f3cda3413b9ec616dcdf2a9e6f
Dag-Erling Smørgrav <des@FreeBSD.org>
2025-09-12 15:20:37 +0200

    FreeBSD: Correct _PC_MIN_HOLE_SIZE
    The actual minimum hole size on ZFS is variable, but we always report
    SPA_MINBLOCKSIZE, which is 512.  This may lead applications to believe
    that they can reliably create holes at 512-byte boundaries and waste
    resources trying to punch holes that ZFS ends up filling anyway.
    
    * In the general case, if the vnode is a regular file, return its
      current block size.  If the vnode is a directory, return the dataset
      record size.  If it is neither a regular file nor a directory,
      return EINVAL.
    
    * In the control directory case, always return EINVAL.
    
    Signed-off-by: Dag-Erling Smørgrav <des@FreeBSD.org>

--- module/os/freebsd/zfs/zfs_vnops_os.c
+++ module/os/freebsd/zfs/zfs_vnops_os.c
@@ -4118,6 +4118,8 @@ zfs_pathconf(vnode_t *vp, int cmd, ulong_t *valp, cred_t *cr,
 {
 	znode_t *zp;
 	zfsvfs_t *zfsvfs;
+	uint32_t blksize;
+	u_longlong_t nblocks;
 	int error;
 
 	switch (cmd) {
@@ -4129,8 +4131,17 @@ zfs_pathconf(vnode_t *vp, int cmd, ulong_t *valp, cred_t *cr,
 		*valp = 64;
 		return (0);
 	case _PC_MIN_HOLE_SIZE:
-		*valp = (int)SPA_MINBLOCKSIZE;
-		return (0);
+		if (vp->v_type == VREG) {
+			zp = VTOZ(vp);
+			sa_object_size(zp->z_sa_hdl, &blksize, &nblocks);
+			*valp = (int)blksize;
+			return (0);
+		}
+		if (vp->v_type == VDIR) {
+			*valp = (int)vp->v_mount->mnt_stat.f_iosize;
+			return (0);
+		}
+		return (EINVAL);
 	case _PC_ACL_EXTENDED:
 #if 0		/* POSIX ACLs are not implemented for ZFS on FreeBSD yet. */
 		zp = VTOZ(vp);
