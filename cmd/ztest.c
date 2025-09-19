dff67dd1bf74df1bb2b1a475716935771a0c00c4
Igor Ostapenko <igor.ostapenko@klarasystems.com>
2025-09-19 08:11:03 +0000

    hfe: ztest: Add support of ztest_vdev_attach_detach()
    Sponsored-by: Klara, Inc.
    Sponsored-by: Wasabi Technology, Inc.
    Signed-off-by: Igor Ostapenko <igor.ostapenko@klarasystems.com>

--- cmd/ztest.c
+++ cmd/ztest.c
@@ -4210,9 +4210,11 @@ ztest_vdev_attach_detach(ztest_ds_t *zd, uint64_t id)
 		error = spa_vdev_detach(spa, oldguid, pguid, B_FALSE);
 		if (error != 0 && error != ENODEV && error != EBUSY &&
 		    error != ENOTSUP && error != ZFS_ERR_CHECKPOINT_EXISTS &&
-		    error != ZFS_ERR_DISCARDING_CHECKPOINT)
+		    error != ZFS_ERR_DISCARDING_CHECKPOINT &&
+		    !ZTEST_HFE_ACTIVE()) {
 			fatal(B_FALSE, "detach (%s) returned %d",
 			    oldpath, error);
+		}
 		goto out;
 	}
 
@@ -4327,7 +4329,8 @@ ztest_vdev_attach_detach(ztest_ds_t *zd, uint64_t id)
 	    error == ZFS_ERR_REBUILD_IN_PROGRESS)
 		expected_error = error;
 
-	if (error != expected_error && expected_error != EBUSY) {
+	if (error != expected_error && expected_error != EBUSY &&
+	    !ZTEST_HFE_ACTIVE()) {
 		fatal(B_FALSE, "attach (%s %"PRIu64", %s %"PRIu64", %d) "
 		    "returned %d, expected %d",
 		    oldpath, oldsize, newpath,
