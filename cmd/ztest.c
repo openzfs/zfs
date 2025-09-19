1c082327a9ac6697fb364b56ee8e1df07b56bbb8
Igor Ostapenko <igor.ostapenko@klarasystems.com>
2025-09-19 08:11:03 +0000

    hfe: ztest: Add support of ztest_spa_checkpoint_create_discard()
    Sponsored-by: Klara, Inc.
    Sponsored-by: Wasabi Technology, Inc.
    Signed-off-by: Igor Ostapenko <igor.ostapenko@klarasystems.com>

--- cmd/ztest.c
+++ cmd/ztest.c
@@ -3632,6 +3632,8 @@ ztest_spa_checkpoint(spa_t *spa)
 		ztest_record_enospc(FTAG);
 		break;
 	default:
+		if (ZTEST_HFE_ACTIVE())
+			break;
 		fatal(B_FALSE, "spa_checkpoint(%s) = %d", spa->spa_name, error);
 	}
 }
@@ -3649,6 +3651,8 @@ ztest_spa_discard_checkpoint(spa_t *spa)
 	case ZFS_ERR_NO_CHECKPOINT:
 		break;
 	default:
+		if (ZTEST_HFE_ACTIVE())
+			break;
 		fatal(B_FALSE, "spa_discard_checkpoint(%s) = %d",
 		    spa->spa_name, error);
 	}
