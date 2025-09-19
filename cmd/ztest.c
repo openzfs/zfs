d44e6a3a8af07fc6b53fbae3c566eed6344177af
Igor Ostapenko <igor.ostapenko@klarasystems.com>
2025-09-19 08:11:03 +0000

    hfe: ztest: Add support of ztest_vdev_aux_add_remove()
    Sponsored-by: Klara, Inc.
    Sponsored-by: Wasabi Technology, Inc.
    Signed-off-by: Igor Ostapenko <igor.ostapenko@klarasystems.com>

--- cmd/ztest.c
+++ cmd/ztest.c
@@ -3959,6 +3959,8 @@ ztest_vdev_aux_add_remove(ztest_ds_t *zd, uint64_t id)
 		case 0:
 			break;
 		default:
+			if (ZTEST_HFE_ACTIVE())
+				break;
 			fatal(B_FALSE, "spa_vdev_add(%p) = %d", nvroot, error);
 		}
 		fnvlist_free(nvroot);
@@ -3980,6 +3982,8 @@ ztest_vdev_aux_add_remove(ztest_ds_t *zd, uint64_t id)
 		case ZFS_ERR_DISCARDING_CHECKPOINT:
 			break;
 		default:
+			if (ZTEST_HFE_ACTIVE())
+				break;
 			if (error != ignore_err)
 				fatal(B_FALSE,
 				    "spa_vdev_remove(%"PRIu64") = %d",
