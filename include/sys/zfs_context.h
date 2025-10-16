8700fc669b2cbf9bab6129f775757924e6176ac9
Rob Norris <robn@despairlabs.com>
2025-10-16 13:12:30 +1100

    libspl: move procfs_list definitions from zfs_context.h
    Sponsored-by: https://despairlabs.com/sponsor/
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Rob Norris <robn@despairlabs.com>
    Closes #17861

--- include/sys/zfs_context.h
+++ include/sys/zfs_context.h
@@ -125,6 +125,7 @@ extern "C" {
 #include <sys/thread.h>
 #include <sys/taskq.h>
 #include <sys/tsd.h>
+#include <sys/procfs_list.h>
 #include <sys/zfs_delay.h>
 
 #include <sys/zfs_context_os.h>
@@ -180,41 +181,6 @@ extern "C" {
 
 #ifdef __FreeBSD__
 typedef off_t loff_t;
-#endif
-
-/*
- * procfs list manipulation
- */
-
-typedef struct procfs_list {
-	void		*pl_private;
-	kmutex_t	pl_lock;
-	list_t		pl_list;
-	uint64_t	pl_next_id;
-	size_t		pl_node_offset;
-} procfs_list_t;
-
-#ifndef __cplusplus
-struct seq_file { };
-void seq_printf(struct seq_file *m, const char *fmt, ...);
-
-typedef struct procfs_list_node {
-	list_node_t	pln_link;
-	uint64_t	pln_id;
-} procfs_list_node_t;
-
-void procfs_list_install(const char *module,
-    const char *submodule,
-    const char *name,
-    mode_t mode,
-    procfs_list_t *procfs_list,
-    int (*show)(struct seq_file *f, void *p),
-    int (*show_header)(struct seq_file *f),
-    int (*clear)(procfs_list_t *procfs_list),
-    size_t procfs_list_node_off);
-void procfs_list_uninstall(procfs_list_t *procfs_list);
-void procfs_list_destroy(procfs_list_t *procfs_list);
-void procfs_list_add(procfs_list_t *procfs_list, void *p);
 #endif
 
 /*
