7227dce0e9acecfd09329d0e15024be7bf049a19
Tony Hutter <hutter2@llnl.gov>
2025-11-04 13:51:38 -0800

    CI: Add Fedora 43
    Signed-off-by: Tony Hutter <hutter2@llnl.gov>

--- .github/workflows/scripts/qemu-2-start.sh
+++ .github/workflows/scripts/qemu-2-start.sh
@@ -83,6 +83,11 @@ case "$OS" in
     OSv="fedora-unknown"
     URL="https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
     ;;
+  fedora43)
+    OSNAME="Fedora 43"
+    OSv="fedora-unknown"
+    URL="https://download.fedoraproject.org/pub/fedora/linux/releases/43/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-43-1.6.x86_64.qcow2"
+    ;;
   freebsd13-5r)
     FreeBSD="13.5-RELEASE"
     OSNAME="FreeBSD $FreeBSD"
