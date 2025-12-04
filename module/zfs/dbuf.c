df1c6080fc8816f6cc14ffff861ddcec553261ac
Alan Somers <asomers@gmail.com>
2025-12-03 18:16:08 -0700

    fixup to "Be more careful with locking around db.db_data": style

--- module/zfs/dbuf.c
+++ module/zfs/dbuf.c
@@ -293,7 +293,7 @@ assert_db_data_contents_locked(dmu_buf_impl_t *db, boolean_t writer)
 assert_db_data_contents_locked(const dmu_buf_impl_t *db, boolean_t writer)
 #endif
 {
-	/* 
+	/*
 	 * db_rwlock protects indirect blocks and the data block of the meta
 	 * dnode.
 	 */
