2d5a9b6a4c437181200b182d80d01577a77e0478
Alexander Moch <mail@alexmoch.com>
2025-12-30 19:51:25 +0100

    Zstd: Restore SPDX license identifiers
    When updating Zstandard to version 1.5.7 the SPDX license identifiers
    were lost. This commit restores them.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Alexander Moch <mail@alexmoch.com>
    Closes #18089

--- module/zstd/lib/decompress/zstd_ddict.c
+++ module/zstd/lib/decompress/zstd_ddict.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0-only
 /*
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  * All rights reserved.
--- module/zstd/zstd-in.c
+++ module/zstd/zstd-in.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: BSD-3-Clause
 /**
  * \file zstd.c
  * Single-file Zstandard library.
