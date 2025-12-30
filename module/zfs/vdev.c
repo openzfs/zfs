482ae130140480425263e11266e51952965c7700
Martin Matuska <martin@matuska.de>
2025-12-30 22:56:13 +0100

    FreeBSD: unbreak compilation on i386
    tests/zfs-tests/cmd/mmap_seek.c: use correct printf specifier
    module/zfs/vdev.c: vdev_clear(): correctly cast argument to atomic_add_64()
    
    Signed-off-by: Martin Matuska <mm@FreeBSD.org>

--- module/zfs/vdev.c
+++ module/zfs/vdev.c
@@ -4674,7 +4674,7 @@ vdev_clear(spa_t *spa, vdev_t *vd)
 	vd->vdev_stat.vs_checksum_errors = 0;
 	vd->vdev_stat.vs_dio_verify_errors = 0;
 	vd->vdev_stat.vs_slow_ios = 0;
-	atomic_store_64(&vd->vdev_outlier_count, 0);
+	atomic_store_64((volatile uint64_t *)&vd->vdev_outlier_count, 0);
 	vd->vdev_read_sit_out_expire = 0;
 
 	for (int c = 0; c < vd->vdev_children; c++)
