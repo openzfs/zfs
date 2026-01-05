8489d70c820f610cddc041ea68eabf491ed19e3d
Dimitry Andric <dimitry@andric.com>
2026-01-05 23:15:22 +0100

    Rename several printf attributes declarations to __printf__
    For kernel builds on FreeBSD, we redefine `__printf__` to
    `__freebsd_kprintf__`, to support FreeBSD kernel printf(9) extensions
    with clang.
    
    In OpenZFS various printf related functions are declared with
    `__attribute__((format(printf, X, Y)))`, so these won't work with the
    above redefinition. With clang 21 and higher, this leads to errors
    similar to:
    
        sys/contrib/openzfs/module/zfs/spa_misc.c:414:38: error: passing
        'printf' format string where 'freebsd_kprintf' format string is
        expected [-Werror,-Wformat]
          414 |         (void) vsnprintf(buf, sizeof (buf), fmt, adx);
              |                                             ^
    
    Since attribute names can always be spelled with leading and trailing
    double underscores, rename these instances.
    
    Note that in the FreeBSD base system we usually use `__printflike` from
    `<sys/cdefs.h>`, but that does not apply to OpenZFS.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Alexander Motin <alexander.motin@TrueNAS.com>
    Signed-off-by: Dimitry Andric <dimitry@andric.com>
    Closes #18095

--- include/sys/spa.h
+++ include/sys/spa.h
@@ -1065,9 +1065,9 @@ extern spa_autotrim_t spa_get_autotrim(spa_t *spa);
 
 /* Miscellaneous support routines */
 extern void spa_load_failed(spa_t *spa, const char *fmt, ...)
-    __attribute__((format(printf, 2, 3)));
+    __attribute__((format(__printf__, 2, 3)));
 extern void spa_load_note(spa_t *spa, const char *fmt, ...)
-    __attribute__((format(printf, 2, 3)));
+    __attribute__((format(__printf__, 2, 3)));
 extern void spa_activate_mos_feature(spa_t *spa, const char *feature,
     dmu_tx_t *tx);
 extern void spa_deactivate_mos_feature(spa_t *spa, const char *feature);
