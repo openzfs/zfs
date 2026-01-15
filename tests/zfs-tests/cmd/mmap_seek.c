d69f7c5e9b3b29b3739d60605c9596ae4dd731d4
Martin Matuška <martin@matuska.de>
2026-01-15 02:02:41 +0100

    FreeBSD: unbreak compilation on i386
    tests/zfs-tests/cmd/mmap_seek.c: use correct printf specifier
    module/zfs/vdev.c: vdev_clear(): correctly cast argument to
    atomic_add_64().
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Martin Matuska <mm@FreeBSD.org>
    Closes #18096

--- tests/zfs-tests/cmd/mmap_seek.c
+++ tests/zfs-tests/cmd/mmap_seek.c
@@ -55,7 +55,7 @@ seek_expect(int fd, off_t offset, int whence, off_t expect_offset)
 		return;
 
 	int err = errno;
-	fprintf(stderr, "lseek(fd, %ld, SEEK_%s) = %ld (expected %ld)",
+	fprintf(stderr, "lseek(fd, %jd, SEEK_%s) = %jd (expected %jd)",
 	    offset, (whence == SEEK_DATA ? "DATA" : "HOLE"),
 	    seek_offset, expect_offset);
 	if (err != 0)
