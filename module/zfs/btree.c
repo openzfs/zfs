openzfs/zfs

commit d2f5cb3a50d5eb156eb97c7e9a6ed3ad9cd95af6

Move range_tree, btree, highbit64 to common code
Break out the range_tree, btree, and highbit64/lowbit64 code from kernel
space into shared kernel and userspace code.  This is needed for the
updated `zpool status -vv` error byte range reporting that will be
coming in a future commit.  That commit needs the range_tree code in
kernel and userspace.

Reviewed-by: Rob Norris <robn@despairlabs.com>
Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
Signed-off-by: Tony Hutter <hutter2@llnl.gov>
Closes #18133

module/zfs/btree.c
@@ -21,7 +21,11 @@
 #include	<sys/bitops.h>
 #include	<sys/zfs_context.h>
 
-kmem_cache_t *zfs_btree_leaf_cache;
+#ifndef _KERNEL
+#define	panic(...) PANIC(__VA_ARGS__)
+#endif
+
+kmem_cache_t *zfs_btree_leaf_cache = NULL;
 
 /*
  * Control the extent of the verification that occurs when zfs_btree_verify is
@@ -106,6 +110,8 @@ zfs_btree_poison_node(zfs_btree_t *tree, zfs_btree_hdr_t *hdr)
 		    tree->bt_leaf_size - offsetof(zfs_btree_leaf_t, btl_elems) -
 		    (hdr->bth_first + hdr->bth_count) * size);
 	}
+#else
+	(void) tree; (void) hdr;
 #endif
 }
 
@@ -132,6 +138,8 @@ zfs_btree_poison_node_at(zfs_btree_t *tree, zfs_btree_hdr_t *hdr,
 		(void) memset(leaf->btl_elems +
 		    (hdr->bth_first + idx) * size, 0x0f, count * size);
 	}
+#else
+	(void) tree; (void) hdr; (void) idx; (void) count;
 #endif
 }
 
@@ -158,6 +166,8 @@ zfs_btree_verify_poison_at(zfs_btree_t *tree, zfs_btree_hdr_t *hdr,
 			    * size + i], ==, 0x0f);
 		}
 	}
+#else
+	(void) tree; (void) hdr; (void) idx;
 #endif
 }
 
@@ -196,6 +206,9 @@ void
 zfs_btree_create(zfs_btree_t *tree, int (*compar) (const void *, const void *),
     bt_find_in_buf_f bt_find_in_buf, size_t size)
 {
+	/* Verify zfs_btree_init() was called before zfs_btree_create() */
+	ASSERT(zfs_btree_leaf_cache != NULL);
+
 	zfs_btree_create_custom(tree, compar, bt_find_in_buf, size,
 	    BTREE_LEAF_SIZE);
 }
@@ -2185,6 +2198,8 @@ zfs_btree_verify_poison(zfs_btree_t *tree)
 	if (tree->bt_height == -1)
 		return;
 	zfs_btree_verify_poison_helper(tree, tree->bt_root);
+#else
+	(void) tree;
 #endif
 }
 

cmd/Makefile.am
@@ -36,8 +36,8 @@ zhack_SOURCES = \
 zhack_LDADD = \
 	libzpool.la \
 	libzfs_core.la \
-	libnvpair.la
-
+	libnvpair.la \
+	librange_tree.la
 
 ztest_CFLAGS    = $(AM_CFLAGS) $(KERNEL_CFLAGS)
 ztest_CPPFLAGS  = $(AM_CPPFLAGS) $(LIBZPOOL_CPPFLAGS)

cmd/zdb/Makefile.am
@@ -14,6 +14,8 @@ zdb_LDADD = \
 	libzdb.la \
 	libzpool.la \
 	libzfs_core.la \
-	libnvpair.la
+	libnvpair.la \
+	libbtree.la \
+	librange_tree.la
 
 zdb_LDADD += $(LIBCRYPTO_LIBS)

cmd/zpool/zpool_util.c
@@ -114,29 +114,3 @@ array64_max(uint64_t array[], unsigned int len)
 
 	return (max);
 }
-
-/*
- * Find highest one bit set.
- * Returns bit number + 1 of highest bit that is set, otherwise returns 0.
- */
-int
-highbit64(uint64_t i)
-{
-	if (i == 0)
-		return (0);
-
-	return (NBBY * sizeof (uint64_t) - __builtin_clzll(i));
-}
-
-/*
- * Find lowest one bit set.
- * Returns bit number + 1 of lowest bit that is set, otherwise returns 0.
- */
-int
-lowbit64(uint64_t i)
-{
-	if (i == 0)
-		return (0);
-
-	return (__builtin_ffsll(i));
-}

cmd/zpool/zpool_util.h
@@ -45,8 +45,6 @@ void *safe_realloc(void *, size_t);
 void zpool_no_memory(void);
 uint_t num_logs(nvlist_t *nv);
 uint64_t array64_max(uint64_t array[], unsigned int len);
-int highbit64(uint64_t i);
-int lowbit64(uint64_t i);
 
 /*
  * Misc utility functions

include/sys/btree.h
@@ -191,9 +191,11 @@ void zfs_btree_fini(void);
  * size   - the value of sizeof(struct my_type)
  * lsize  - custom leaf size
  */
-void zfs_btree_create(zfs_btree_t *, int (*) (const void *, const void *),
+void zfs_btree_create(zfs_btree_t *,
+    int (*) (const void *, const void *),
     bt_find_in_buf_f, size_t);
-void zfs_btree_create_custom(zfs_btree_t *, int (*)(const void *, const void *),
+void zfs_btree_create_custom(zfs_btree_t *,
+    int (*)(const void *, const void *),
     bt_find_in_buf_f, size_t, size_t);
 
 /*
@@ -213,7 +215,8 @@ void *zfs_btree_find(zfs_btree_t *, const void *, zfs_btree_index_t *);
  * node   - the node to insert
  * where  - position as returned from zfs_btree_find()
  */
-void zfs_btree_add_idx(zfs_btree_t *, const void *, const zfs_btree_index_t *);
+void zfs_btree_add_idx(zfs_btree_t *, const void *,
+    const zfs_btree_index_t *);
 
 /*
  * Return the first or last valued node in the tree. Will return NULL if the

lib/Makefile.am
@@ -12,19 +12,19 @@
 #
 # CMDS:   zhack/ztest/                zfs/zpool/zed/
 #        raidz_{test,bench}          zinject/zstream
-#                |                          |
-# LIBS:          |                          |              libzfsbootenv*
-#                |--libzdb--zdb             |                    |
-#                |                          |                    |
-#             libzpool                   libzfs* ----------------+
-#             | |    \                  / | | |
-#    libicp --/ |     \                /  | \ |
-#               |      \              /   |  \ \
-#    libzstd ---/       \            /    |   \ \-------\
-#                        \          /     \    \         \
-#                         \        /       \    \-------\ \
-#                          \      /         \            \ |
-#                          libzutil        libzfs_core*  | |
+#                |         \_____          / |
+# LIBS:          |               \        /  |            libzfsbootenv*
+#                |--libzdb--zdb   \      /   |                  |
+#                |           |    |     /    |                  |
+#             libzpool-\     |    |    //-libzfs* --------------+
+#             | |    \  \    |    |   // / | \ \
+#    libicp --/ |     \  \   |    |  // /  |  \ \
+#               |      \ librange_tree /   |   \ \
+#    libzstd ---/       \  libbtree   /    |    \ \--------
+#                        \           /      \    \         \
+#                         \         /        \    \------  |
+#                          \       /          \          \ |
+#                           libzutil       libzfs_core*  | |
 #                            | | | \          |     |    | |
 #                            | | | |          |     |    | |
 #                            | | | |          |     |    | |
@@ -55,8 +55,10 @@ noinst_LTLIBRARIES =
 lib_LTLIBRARIES =
 pkgconfig_DATA =
 include $(srcdir)/%D%/libavl/Makefile.am
+include $(srcdir)/%D%/libbtree/Makefile.am
 include $(srcdir)/%D%/libicp/Makefile.am
 include $(srcdir)/%D%/libnvpair/Makefile.am
+include $(srcdir)/%D%/librange_tree/Makefile.am
 include $(srcdir)/%D%/libspl/Makefile.am
 include $(srcdir)/%D%/libzdb/Makefile.am
 include $(srcdir)/%D%/libzfs_core/Makefile.am

lib/libbtree/Makefile.am
@@ -1,0 +1,6 @@
+# SPDX-License-Identifier: CDDL-1.0
+libbtree_la_CFLAGS  = $(AM_CFLAGS) $(KERNEL_CFLAGS) $(LIBRARY_CFLAGS)
+
+noinst_LTLIBRARIES += libbtree.la
+
+nodist_libbtree_la_SOURCES = module/zfs/btree.c

commit 9553d52bce0655d59d6747b96b96ebcfc77724d7

btree: Fix zdb crashes
At least one codepath calls zfs_btree_fini() twice, leading to a zdb
crash due to a double free.  This can cause slog_replay_fs_002 and
possibly other tests to fail.  This was most likely caused by
d2f5cb3a5.  The fix is to add a check to allow multiple calls to
zfs_btree_fini().

Signed-off-by: Tony Hutter <hutter2@llnl.gov>

module/zfs/btree.c
@@ -181,7 +181,10 @@ zfs_btree_init(void)
 void
 zfs_btree_fini(void)
 {
-	kmem_cache_destroy(zfs_btree_leaf_cache);
+	if (zfs_btree_leaf_cache != NULL) {
+		kmem_cache_destroy(zfs_btree_leaf_cache);
+		zfs_btree_leaf_cache = NULL;
+	}
 }
 
 static void *