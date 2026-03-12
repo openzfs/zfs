753f1e1e21aa99a0917fd1a423668b9abb894375
Alan Somers <asomers@FreeBSD.org>
2026-03-12 16:08:58 -0600

    zstream: add a drop_record subcommand
    It can be used to drop extraneous records in a send stream caused by a
    corrupt dataset, as in issue #18239.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by:	Alan Somers <asomers@gmail.com>
    Sponsored by:	ConnectWise
    Closes #18275

--- cmd/zstream/zstream.h
+++ cmd/zstream/zstream.h
@@ -28,6 +28,7 @@ extern "C" {
 extern int zstream_do_redup(int, char *[]);
 extern int zstream_do_dump(int, char *[]);
 extern int zstream_do_decompress(int argc, char *argv[]);
+extern int zstream_do_drop_record(int argc, char *argv[]);
 extern int zstream_do_recompress(int argc, char *argv[]);
 extern int zstream_do_token(int, char *[]);
 extern void zstream_usage(void);
