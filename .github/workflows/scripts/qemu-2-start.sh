046dfbcc3822304efcdc28e3e303d010c4eb91b8
Christos Longros <98426896+chrislongros@users.noreply.github.com>
2026-03-23 18:34:10 +0100

    ci: update FreeBSD CI images from 14.3 to 14.4
    Update FreeBSD CI targets from 14.3 to 14.4 in both the QEMU
    start script and the workflow configuration.
    
    Reviewed-by: Alexander Motin <alexander.motin@TrueNAS.com>
    Reviewed-by: Tony Hutter <hutter2@llnl.gov>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Christos Longros <chris.longros@gmail.com>
    Closes #18362

--- .github/workflows/scripts/qemu-2-start.sh
+++ .github/workflows/scripts/qemu-2-start.sh
@@ -103,8 +103,8 @@ case "$OS" in
     URLxz="$FREEBSD_REL/$FreeBSD/amd64/Latest/FreeBSD-$FreeBSD-amd64-BASIC-CI.raw.xz"
     KSRC="$FREEBSD_REL/../amd64/$FreeBSD/src.txz"
     ;;
-  freebsd14-3r)
-    FreeBSD="14.3-RELEASE"
+  freebsd14-4r)
+    FreeBSD="14.4-RELEASE"
     OSNAME="FreeBSD $FreeBSD"
     OSv="freebsd14.0"
     URLxz="$FREEBSD_REL/$FreeBSD/amd64/Latest/FreeBSD-$FreeBSD-amd64-BASIC-CI.raw.xz"
@@ -118,8 +118,8 @@ case "$OS" in
     KSRC="$FREEBSD_SNAP/../amd64/$FreeBSD/src.txz"
     NIC="rtl8139"
     ;;
-  freebsd14-3s)
-    FreeBSD="14.3-STABLE"
+  freebsd14-4s)
+    FreeBSD="14.4-STABLE"
     OSNAME="FreeBSD $FreeBSD"
     OSv="freebsd14.0"
     URLxz="$FREEBSD_SNAP/$FreeBSD/amd64/Latest/FreeBSD-$FreeBSD-amd64-BASIC-CI-ufs.raw.xz"
