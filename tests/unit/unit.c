9742b3154aabaa54e71ab8a2e9a49eaccb590994
Rob Norris <rob.norris@truenas.com>
2026-05-02 17:37:12 +1000

    unit: a unit testing framework
    This commit establishes a unit test framework for OpenZFS, and
    integrates it into the build.
    
    It includes:
    - the "munit" unit test framework (munit.c, munit.h)
    - some light extensions to munit and glue for OpenZFS (unit.c, unit.h)
    - make targets for running tests and generating coverage reports
    - a document explaining the what, how and why
    
    This is a first step; I expect we will extend all of this as we use it
    more places and gain experience with it.
    
    Sponsored-by: TrueNAS
    Signed-off-by: Rob Norris <rob.norris@truenas.com>

---
+++ tests/unit/unit.c
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: CDDL-1.0
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source.  A copy of the CDDL is also available via the Internet at
+ * http://www.illumos.org/license/CDDL.
+ */
+
+/*
+ * Copyright (c) 2026, TrueNAS.
+ */
+
+/* Core stubs, applicable to all test suites. */
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#include <sys/types.h>
+#include <sys/cmn_err.h>
+#include <sys/zfs_debug.h>
+
+#include "munit.h"
+
+/*
+ * SET_ERROR() expands to __set_error() in debug builds. It's an
+ * under-the-hood tracing aid in production; a no-op is fine.
+ */
+void
+__set_error(const char *file, const char *func, int line, int err)
+{
+	(void) file; (void) func; (void) line; (void) err;
+}
+
+/* Plumb logging and debug into munit for convenience. */
+
+/* dprintf() checks zfs_flags and calls __dprintf() in debug builds. */
+int zfs_dbgmsg_enable = 1;
+int zfs_flags = ZFS_DEBUG_DPRINTF;
+
+/* Log dprintf() to MUNIT_LOG_DEBUG. */
+void
+__dprintf(boolean_t dprint, const char *file, const char *func,
+    int line, const char *fmt, ...)
+{
+	char buf[1024];
+
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(buf, sizeof (buf), fmt, ap);
+	va_end(ap);
+
+	munit_logf_ex(MUNIT_LOG_DEBUG, NULL, 0, "%s%s:%d [%s]: %s",
+	    dprint ? "dprintf: " : "", file, line, func, buf);
+}
+
+/* Log cmn_err() to MUNIT_LOG_INFO or WARNING, abort test on CE_PANIC. */
+void
+cmn_err(int ce, const char *fmt, ...)
+{
+	if (ce == CE_IGNORE)
+		return;
+
+	char buf[1024];
+
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(buf, sizeof (buf), fmt, ap);
+	va_end(ap);
+
+	switch (ce) {
+	case CE_WARN:
+		munit_logf_ex(MUNIT_LOG_WARNING, NULL, 0, "%s", buf);
+		break;
+	case CE_PANIC:
+		munit_errorf_ex(NULL, 0, "PANIC: %s", buf);
+		break;
+	default:
+		munit_logf_ex(MUNIT_LOG_INFO, NULL, 0, "%s", buf);
+		break;
+	}
+}
