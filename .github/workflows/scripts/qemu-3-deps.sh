44e9646ea636d27d40b0e7d998bc93a21a4ed6c5
Tony Hutter <hutter2@llnl.gov>
2026-05-13 20:24:17 -0700

    CI: Fix 99.99 META version
    We have an option in zfs-qemu-packages to test against a specific kernel
    version.  However, qemu-3-deps.sh was incorrectly hard coded to look
    at $2 for a kernel version argument (which could come in $2 or $3
    depending on if --poweroff was also passed).  This caused the CI
    to incorrectly edit META with a max supported kernel version of 99.99
    when we didn't want that.
    
    Fix this by looking at all the arguments for something that looks
    like a kernel version and set that as the kernel max in META.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Tony Hutter <hutter2@llnl.gov>
    Closes #18526
    Closes #18531

--- .github/workflows/scripts/qemu-3-deps.sh
+++ .github/workflows/scripts/qemu-3-deps.sh
@@ -2,9 +2,12 @@
 # 3) Wait for VM to boot from previous step and launch dependencies
 #    script on it.
 #
-# $1: OS name (like 'fedora41')
-# $2: (optional) Experimental kernel version to install on fedora,
-#     like "6.14".
+# qemu-3-deps.sh [--poweroff] OS_NAME [FEDORA_VERSION]
+#
+# --poweroff: Power off the VM after installing dependencies
+# OS_NAME: OS name (like 'fedora41')
+# FEDORA_VERSION: (optional) Experimental Fedora kernel version, like "6.14" to
+#     install instead of Fedora defaults.
 ######################################################################
 
 .github/workflows/scripts/qemu-wait-for-vm.sh vm0
@@ -15,8 +18,13 @@
 # we need to update the kernel version in zfs's META file to allow the
 # build to happen.  We update our local copy of META here, since we know
 # it will be rsync'd up in the next step.
-if [ -n "${2:-}" ] ; then
-  sed -i -E 's/Linux-Maximum: .+/Linux-Maximum: 99.99/g' META
+#
+# Look to see if the last argument looks like a kernel version.
+ver="${@: -1}"
+if [[ $ver =~ ^[0-9]+\.[0-9]+ ]] ; then
+  # We got a kernel version, update META to say we support it so we
+  # can test against it.
+  sed -i -E 's/Linux-Maximum: .+/Linux-Maximum: '$ver'/g' META
 fi
 
 scp .github/workflows/scripts/qemu-3-deps-vm.sh zfs@vm0:qemu-3-deps-vm.sh
