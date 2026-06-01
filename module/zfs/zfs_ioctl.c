63a0ff6cd2cdd72505b765c85ee14e0286a02a70
Tony Hutter <hutter2@llnl.gov>
2026-06-01 14:55:20 -0700

    nvpair: Check for un-terminated strings in packed nvlist
    Add additional checks to verify a packed string or string array nvpair
    is terminated.  Or more specifically, verify doing a strlen() on the
    prospective string does not overrun the packed nvlist buffer.
    
    Also add additional checks in the libzfs_input_checks test case to
    verify un-terminated strings, and add in a nvlist ioctl payload
    fuzz test for good measure.
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Tony Hutter <hutter2@llnl.gov>
    Closes #18604

--- module/zfs/zfs_ioctl.c
+++ module/zfs/zfs_ioctl.c
@@ -3884,7 +3884,6 @@ static int
 zfs_ioc_log_history(const char *unused, nvlist_t *innvl, nvlist_t *outnvl)
 {
 	(void) unused, (void) outnvl;
-	const char *message;
 	char *poolname;
 	spa_t *spa;
 	int error;
@@ -3905,7 +3904,7 @@ zfs_ioc_log_history(const char *unused, nvlist_t *innvl, nvlist_t *outnvl)
 	if (error != 0)
 		return (error);
 
-	message = fnvlist_lookup_string(innvl, "message");
+	const char *message = fnvlist_lookup_string(innvl, "message");
 
 	if (spa_version(spa) < SPA_VERSION_ZPOOL_HISTORY) {
 		spa_close(spa, FTAG);
