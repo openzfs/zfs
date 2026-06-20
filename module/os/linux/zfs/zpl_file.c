b1df5661fbc1e687fb7c3ce4b37d4f3fc3a682fd
tiehexue <tiehexue@hotmail.com>
2026-06-21 02:06:38 +0800

    do more checks as zfs_setup_direct
    zfs_setup_direct is not called in async
    write path as we did in async read
    path, because pin pages need wait "a long
    time in write path which will cause
    FOLL_LONGTERM hitting kernel limit.
    
    Signed-off-by: tiehexue <tiehexue@hotmail.com>

--- module/os/linux/zfs/zpl_file.c
+++ module/os/linux/zfs/zpl_file.c
@@ -302,7 +302,9 @@ zpl_iter_read(struct kiocb *kiocb, struct iov_iter *to)
 	 * benefit.
 	 */
 	if (zfs_async_dio_enabled && !is_sync_kiocb(kiocb) &&
-	    (filp->f_flags & O_DIRECT)) {
+	    ((filp->f_flags & O_DIRECT) ||
+	    ITOZSB(filp->f_mapping->host)->z_os->os_direct ==
+	    ZFS_DIRECT_ALWAYS)) {
 		crhold(cr);
 		cookie = spl_fstrans_mark();
 
@@ -386,7 +388,8 @@ zpl_iter_write(struct kiocb *kiocb, struct iov_iter *from)
 	 * through the ARC and the disk write is already async (txg sync).
 	 */
 	if (zfs_async_dio_enabled && !is_sync_kiocb(kiocb) &&
-	    (filp->f_flags & O_DIRECT)) {
+	    ((filp->f_flags & O_DIRECT) ||
+	    ITOZSB(ip)->z_os->os_direct == ZFS_DIRECT_ALWAYS)) {
 		crhold(cr);
 		cookie = spl_fstrans_mark();
 
