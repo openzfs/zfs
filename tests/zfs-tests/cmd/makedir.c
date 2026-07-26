1c696d09a9e7658cee61041b0461612b723230cb
Rob Norris <rob.norris@truenas.com>
2026-07-26 20:27:45 +1000

    ZTS: makedir helper command
    The system mkdir command may do other things besides the mkdir()
    syscall, which is a problem when we're trying to test the syscall
    specifically. This helper only does the mkdir() syscall.
    
    Sponsored-by: TrueNAS
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Rob Norris <rob.norris@truenas.com>
    Closes #18847:w

---
+++ tests/zfs-tests/cmd/makedir.c
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: CDDL-1.0
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source.  A copy of the CDDL is also available via the Internet at
+ * http://www.illumos.org/license/CDDL.
+ */
+
+/*
+ * Copyright (c) 2026, TrueNAS.
+ */
+
+#include <sys/stat.h>
+
+int
+main(int argc, char **argv)
+{
+	if (argc != 2) {
+		fprintf(stderr, "usage: makedir <path>\n");
+		return (1);
+	}
+
+	if (mkdir(argv[1], 0777) == -1) {
+		perror("mkdir");
+		return (2);
+	}
+
+	return (0);
+}
