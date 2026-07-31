49086325ba7f9651ec540ea446dc1e64c280786a
Richard Yao <richard@ryao.dev>
2026-07-31 16:03:38 -0400

    Add missing checks to zfs_clone_range_replay()
    zfs_clone_range() does a few error checks that we are missing in
    zfs_clone_range_replay().
    
    Reported-by: Grok 4.5 Build Beta
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Alexander Motin <alexander.motin@TrueNAS.com>
    Signed-off-by: Richard Yao <richard@ryao.dev>
    Closes #18867

--- module/zfs/zfs_vnops.c
+++ module/zfs/zfs_vnops.c
@@ -1887,7 +1887,7 @@ zfs_clone_range(znode_t *inzp, uint64_t *inoffp, znode_t *outzp,
 
 			/*
 			 * Block growth may fail for many reasons we can not
-			 * predict here.  If it happen the cloning is doomed.
+			 * predict here. If it happens, the cloning is doomed.
 			 */
 			if (inblksz != outzp->z_blksz) {
 				error = SET_ERROR(EINVAL);
@@ -2047,7 +2047,19 @@ zfs_clone_range_replay(znode_t *zp, uint64_t off, uint64_t len, uint64_t blksz,
 	if (zp->z_blksz < blksz)
 		zfs_grow_blocksize(zp, blksz, tx);
 
-	dmu_brt_clone(zfsvfs->z_os, zp->z_id, off, len, tx, bps, nbps);
+	if (blksz != zp->z_blksz) {
+		error = SET_ERROR(EINVAL);
+		dmu_tx_commit(tx);
+		zfs_exit(zfsvfs, FTAG);
+		return (error);
+	}
+
+	error = dmu_brt_clone(zfsvfs->z_os, zp->z_id, off, len, tx, bps, nbps);
+	if (error != 0) {
+		dmu_tx_commit(tx);
+		zfs_exit(zfsvfs, FTAG);
+		return (error);
+	}
 
 	zfs_tstamp_update_setup(zp, CONTENT_MODIFIED, mtime, ctime);
 
