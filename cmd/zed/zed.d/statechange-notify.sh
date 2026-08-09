4b05f927dc1cad612a2303b1dac3b83e6b25abb6
Rob Norris <robn@despairlabs.com>
2026-08-09 21:20:14 +1000

    license: convert all "fenced" CDDL headers to standard form
    These all used the older, more verbose version of the header. The more
    recent short form is clearer and altogether nicer. Use it!
    
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Rob Norris <robn@despairlabs.com>
    Closes #18904

--- cmd/zed/zed.d/statechange-notify.sh
+++ cmd/zed/zed.d/statechange-notify.sh
@@ -2,15 +2,14 @@
 # SPDX-License-Identifier: CDDL-1.0
 # shellcheck disable=SC2154
 #
-# CDDL HEADER START
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
 #
-# The contents of this file are subject to the terms of the
-# Common Development and Distribution License Version 1.0 (CDDL-1.0).
-# You can obtain a copy of the license from the top-level file
-# "OPENSOLARIS.LICENSE" or at <http://opensource.org/licenses/CDDL-1.0>.
-# You may not use this file except in compliance with the license.
-#
-# CDDL HEADER END
+# A full copy of the text of the CDDL should have accompanied this
+# source.  A copy of the CDDL is also available via the Internet at
+# https://opensource.org/license/CDDL-1.0.
 #
 
 #
