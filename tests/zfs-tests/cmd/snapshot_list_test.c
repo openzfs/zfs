468fd945fab5381a6d76ab69edacaf86de9be736
Wolfgang Hoschek <wolfgang.hoschek@mac.com>
2026-08-23 18:14:48 -0400

    fixup! zfs list perf: improve perf by listing snaps a batch at a time
    make new checkstyle rule happy.
    
    Signed-off-by: Wolfgang Hoschek <wolfgang.hoschek@mac.com>

--- tests/zfs-tests/cmd/snapshot_list_test.c
+++ tests/zfs-tests/cmd/snapshot_list_test.c
@@ -1,5 +1,16 @@
 // SPDX-License-Identifier: CDDL-1.0
 /*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source.  A copy of the CDDL is also available via the Internet at
+ * https://opensource.org/license/CDDL-1.0.
+ */
+
+/*
  * Exercise projected snapshot iteration behavior which is not exposed by the
  * zfs command line.
  */
